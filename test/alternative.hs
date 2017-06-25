594185f18d2860fd8bcc7c3776889b417ef9425d
Harendra Kumar <harendra.kumar@gmail.com>
2017-06-26 03:15:27 +0530

    Add applicative and alternative tests

---
+++ test/alternative.hs
@@ -0,0 +1,30 @@
+import Control.Concurrent (threadDelay, myThreadId)
+import Control.Monad.IO.Class (liftIO)
+import System.Random (randomIO)
+import System.IO
+import Control.Applicative
+
+import Strands
+
+main = wait $ threads 3 $ do
+    liftIO $ hSetBuffering stdout LineBuffering
+    mainThread <- liftIO myThreadId
+    liftIO $ putStrLn $ "Main thread: " ++ show mainThread
+
+    eventA <|> eventB
+
+eventA = do
+    x <- sample (randomIO :: IO Int) 1000000
+    evThread <- liftIO myThreadId
+    liftIO $ putStrLn $ "X Event thread: " ++ show evThread
+    liftIO $ putStrLn $ "x = " ++ (show x)
+    return x
+
+eventB = do
+    y <- sample (randomIO :: IO Int) 1000000
+
+    -- liftIO $ threadDelay 10000000
+    evThread <- liftIO myThreadId
+    liftIO $ putStrLn $ "Y Event thread: " ++ show evThread
+    liftIO $ putStrLn $ "y = " ++ (show y)
+    return y
---
+++ test/applicative.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE FlexibleContexts          #-}
+
+import Control.Applicative ((<$>), (<*>), (<|>))
+import Control.Monad.IO.Class (liftIO)
+import System.IO
+import Strands
+
+main = do
+    hSetBuffering stdout LineBuffering
+    xs <- gather $ threads 5 $ do
+        x <- (,) <$> (event 1 <|> event 2) <*> (event 3 <|> event 4)
+        --x <- (,) <$> choose [1,2] <*> choose [3,4]
+        liftIO $ putStrLn $ show x
+        return x
+    putStrLn $ show xs
+    where
+        event n = async (do putStrLn ("event" ++ show n); return n :: IO Int)
