3a988778f65b87d2e3ef97289df9bd97f93e717d
Harendra Kumar <harendra.kumar@gmail.com>
2017-06-26 18:27:12 +0530

    Remove redundant stuff from Log.hs

--- src/Strands/Log.hs
+++ src/Strands/Log.hs
@@ -1,19 +1,12 @@
 {-# LANGUAGE ConstraintKinds           #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
 
 module Strands.Log
     ( Loggable
-    , Replaying
     , LogEntry (..)
+    , Replaying
     , Log (..)
-    , IDynamic (..)
-    , fromIDyn
-    , toIDyn
     )
 where
-
-import           Unsafe.Coerce          (unsafeCoerce)
 
 ------------------------------------------------------------------------------
 -- Log types
@@ -22,42 +15,11 @@ import           Unsafe.Coerce          (unsafeCoerce)
 -- | Constraint type synonym for a value that can be logged.
 type Loggable a = (Show a, Read a)
 
-------------------------------------------------------------------------------
--- Serialization
-------------------------------------------------------------------------------
-
--- | Dynamic serializable data for logging.
-data IDynamic =
-    IDyns String
-  | forall a. Loggable a => IDynamic a
-
-instance Show IDynamic where
-  show (IDynamic x) = show (show x)
-  show (IDyns    s) = show s
-
-instance Read IDynamic where
-  readsPrec _ str = map (\(x,s) -> (IDyns x,s)) $ readsPrec 0 str
-
-fromIDyn :: Loggable a => IDynamic -> a
-fromIDyn (IDynamic x) = r
-    where r = unsafeCoerce x
-
-fromIDyn (IDyns s) = r `seq` r
-    where r = read s
-
-toIDyn :: Loggable a => a -> IDynamic
-toIDyn x = IDynamic x
-
-------------------------------------------------------------------------------
--- Log
-------------------------------------------------------------------------------
-
 data LogEntry        =
-      Executing        -- we are inside this computation, not yet done
-    | Waiting          -- the computation is done and it returned mzero
-    | Result IDynamic  -- computation is done and we have the result to replay
+      Executing    -- we are inside this computation, not yet done
+    | Maybe String -- computation is done and we have the result to replay
   deriving (Read, Show)
 
 type Replaying = Bool
 
-data Log = Log Replaying [LogEntry] deriving (Show)
+data Log = Log Replaying [LogEntry] deriving (Read, Show)
