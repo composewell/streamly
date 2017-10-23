ff35f644c81a21614898747cf469c3508baa74ca
Harendra Kumar <harendra.kumar@gmail.com>
2017-10-24 05:24:45 +0530

    Add Num, Floating, Fractional instances for zip types

--- src/Asyncly/Prelude.hs
+++ src/Asyncly/Prelude.hs
@@ -35,7 +35,7 @@ module Asyncly.Prelude
     )
 where
 
-import Control.Applicative (Alternative (..))
+import Control.Applicative (Alternative (..), liftA2)
 import Data.Semigroup (Semigroup(..), cycle1)
 import Prelude hiding (drop, take, zipWith)
 import Asyncly.AsyncT
@@ -91,11 +91,11 @@ drop n m = AsyncT $ \ctx stp yld -> do
     else (runAsyncT m) ctx stp yield
 
 ------------------------------------------------------------------------------
--- Zipping Streams
+-- Serially Zipping Streams
 ------------------------------------------------------------------------------
 
 -- | Zip two AsyncT streams serially using a monadic zipping function.
-zipWithM :: MonadAsync m =>
+zipWithM :: Monad m =>
     (a -> b -> AsyncT m c) -> AsyncT m a -> AsyncT m b -> AsyncT m c
 zipWithM f m1 m2 = AsyncT $ \_ stp yld -> do
     let merge a ra =
@@ -103,19 +103,18 @@ zipWithM f m1 m2 = AsyncT $ \_ stp yld -> do
                 yield2 b (Just rb) =
                     (runAsyncT ((f a b) <> (zipWithM f ra rb))) Nothing stp yld
              in (runAsyncT m2) Nothing stp yield2
-    let yield1 a Nothing   = merge a empty
+    let yield1 a Nothing   = merge a mempty
         yield1 a (Just ra) = merge a ra
     (runAsyncT m1) Nothing stp yield1
 
 -- | Zip two AsyncT streams serially using a pure zipping function.
-zipWith :: MonadAsync m
-    => (a -> b -> c) -> AsyncT m a -> AsyncT m b -> AsyncT m c
+zipWith :: Monad m => (a -> b -> c) -> AsyncT m a -> AsyncT m b -> AsyncT m c
 zipWith f m1 m2 = AsyncT $ \_ stp yld -> do
     let merge a ra =
             let yield2 b Nothing   = yld (f a b) Nothing
                 yield2 b (Just rb) = yld (f a b) (Just (zipWith f ra rb))
              in (runAsyncT m2) Nothing stp yield2
-    let yield1 a Nothing   = merge a empty
+    let yield1 a Nothing   = merge a mempty
         yield1 a (Just ra) = merge a ra
     (runAsyncT m1) Nothing stp yield1
 
@@ -127,9 +126,53 @@ zipWith f m1 m2 = AsyncT $ \_ stp yld -> do
 newtype ZipSerial m a = ZipSerial {getZipSerial :: AsyncT m a}
         deriving (Functor)
 
-instance MonadAsync m => Applicative (ZipSerial m) where
+instance Monad m => Applicative (ZipSerial m) where
     pure a = ZipSerial $ cycle1 (pure a)
     (ZipSerial xs) <*> (ZipSerial ys) = ZipSerial (zipWith id xs ys)
+
+instance (Monad m, Num a) => Num (ZipSerial m a) where
+    fromInteger n = pure (fromInteger n)
+
+    negate = fmap negate
+    abs    = fmap abs
+    signum = fmap signum
+
+    (+) = liftA2 (+)
+    (*) = liftA2 (*)
+    (-) = liftA2 (-)
+
+instance (Monad m, Fractional a) => Fractional (ZipSerial m a) where
+    fromRational n = pure (fromRational n)
+
+    recip = fmap recip
+
+    (/) = liftA2 (/)
+
+instance (Monad m, Floating a) => Floating (ZipSerial m a) where
+    pi = pure pi
+
+    exp  = fmap exp
+    sqrt = fmap sqrt
+    log  = fmap log
+    sin  = fmap sin
+    tan  = fmap tan
+    cos  = fmap cos
+    asin = fmap asin
+    atan = fmap atan
+    acos = fmap acos
+    sinh = fmap sinh
+    tanh = fmap tanh
+    cosh = fmap cosh
+    asinh = fmap asinh
+    atanh = fmap atanh
+    acosh = fmap acosh
+
+    (**)    = liftA2 (**)
+    logBase = liftA2 logBase
+
+------------------------------------------------------------------------------
+-- Asyncly Zipping Streams
+------------------------------------------------------------------------------
 
 -- | Zip two AsyncT streams asyncly (i.e. both the streams are generated
 -- concurrently) using a monadic zipping function.
@@ -160,3 +203,43 @@ newtype ZipAsync m a = ZipAsync {getZipAsync :: AsyncT m a}
 instance MonadAsync m => Applicative (ZipAsync m) where
     pure a = ZipAsync $ cycle1 (pure a)
     (ZipAsync xs) <*> (ZipAsync ys) = ZipAsync (zipAsyncWith id xs ys)
+
+instance (MonadAsync m, Num a) => Num (ZipAsync m a) where
+    fromInteger n = pure (fromInteger n)
+
+    negate = fmap negate
+    abs    = fmap abs
+    signum = fmap signum
+
+    (+) = liftA2 (+)
+    (*) = liftA2 (*)
+    (-) = liftA2 (-)
+
+instance (MonadAsync m, Fractional a) => Fractional (ZipAsync m a) where
+    fromRational n = pure (fromRational n)
+
+    recip = fmap recip
+
+    (/) = liftA2 (/)
+
+instance (MonadAsync m, Floating a) => Floating (ZipAsync m a) where
+    pi = pure pi
+
+    exp  = fmap exp
+    sqrt = fmap sqrt
+    log  = fmap log
+    sin  = fmap sin
+    tan  = fmap tan
+    cos  = fmap cos
+    asin = fmap asin
+    atan = fmap atan
+    acos = fmap acos
+    sinh = fmap sinh
+    tanh = fmap tanh
+    cosh = fmap cosh
+    asinh = fmap asinh
+    atanh = fmap atanh
+    acosh = fmap acosh
+
+    (**)    = liftA2 (**)
+    logBase = liftA2 logBase
