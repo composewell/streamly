650f9fe945af12401f3e9751358334e4f1992e3b
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-03-24 16:39:11 +0000

    Add alternate implementation of foldrM

--- src/Streamly/Prelude.hs
+++ src/Streamly/Prelude.hs
@@ -150,13 +150,13 @@ foldr step acc m = go (toStream m)
 
 -- | Right fold with a monadic step function.  See 'toList' for an example use.
 {-# INLINE foldrM #-}
-foldrM :: Streaming t => (a -> m b -> m b) -> m b -> t m a -> m b
+foldrM :: (Streaming t, Monad m) => (a -> b -> m b) -> m b -> t m a -> m b
 foldrM step acc m = go (toStream m)
     where
     go m1 =
         let stop = acc
-            yield a Nothing  = step a acc
-            yield a (Just x) = step a (go x)
+            yield a Nothing  = acc >>= (step a)
+            yield a (Just x) = (go x) >>= (step a)
         in (runStream m1) Nothing stop yield
 
 -- | Scan left. A strict left fold which accumulates the result of its reduction steps inside a stream, from left.
@@ -238,7 +238,7 @@ toHandle h m = go (toStream m)
 -- | Convert a stream into a list in the underlying monad.
 {-# INLINABLE toList #-}
 toList :: (Streaming t, Monad m) => t m a -> m [a]
-toList = foldrM (\a xs -> fmap (a :) xs) (return [])
+toList = foldrM (\a xs -> return (a : xs)) (return [])
 
 -- | Take first 'n' elements from the stream and discard the rest.
 {-# INLINE take #-}
