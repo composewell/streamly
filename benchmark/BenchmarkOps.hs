a445ef9873056025af871e11cd6c5e1a9da2b07d
Harendra Kumar <harendra.kumar@gmail.com>
2018-04-09 20:32:49 +0530

    Add benchmarks for streaming operations

---
+++ benchmark/BenchmarkOps.hs
@@ -0,0 +1,127 @@
+-- |
+-- Module      : BenchmarkOps
+-- Copyright   : (c) 2018 Harendra Kumar
+--
+-- License     : MIT
+-- Maintainer  : harendra.kumar@gmail.com
+
+module BenchmarkOps where
+
+import Prelude
+       (Monad, Int, (+), id, ($), (.), return, fmap, even, (>), (<=),
+        subtract, undefined, Maybe)
+
+import qualified Streamly          as S
+import qualified Streamly.Prelude  as S
+
+value, maxValue :: Int
+value = 1000000
+maxValue = value + 1000
+
+-------------------------------------------------------------------------------
+-- Benchmark ops
+-------------------------------------------------------------------------------
+
+{-# INLINE toNull #-}
+{-# INLINE toList #-}
+{-# INLINE foldl #-}
+{-# INLINE last #-}
+{-# INLINE scan #-}
+{-# INLINE map #-}
+{-# INLINE filterEven #-}
+{-# INLINE mapM #-}
+{-# INLINE filterAllOut #-}
+{-# INLINE filterAllIn #-}
+{-# INLINE takeOne #-}
+{-# INLINE takeAll #-}
+{-# INLINE takeWhileTrue #-}
+{-# INLINE dropAll #-}
+{-# INLINE dropWhileTrue #-}
+{-# INLINE zip #-}
+{-# INLINE concat #-}
+{-# INLINE composeMapM #-}
+{-# INLINE composeAllInFilters #-}
+{-# INLINE composeAllOutFilters #-}
+{-# INLINE composeMapAllInFilter #-}
+toNull, scan, map, filterEven, mapM, filterAllOut,
+    filterAllIn, takeOne, takeAll, takeWhileTrue, dropAll, dropWhileTrue, zip,
+    concat, composeMapM, composeAllInFilters, composeAllOutFilters,
+    composeMapAllInFilter
+    :: Monad m
+    => Stream m Int -> m ()
+
+toList :: Monad m => Stream m Int -> m [Int]
+foldl :: Monad m => Stream m Int -> m Int
+last :: Monad m => Stream m Int -> m (Maybe Int)
+
+-------------------------------------------------------------------------------
+-- Stream generation and elimination
+-------------------------------------------------------------------------------
+
+type Stream m a = S.StreamT m a
+
+source :: Int -> Stream m Int
+source n = S.each [n..n+value]
+
+{-# INLINE runStream #-}
+runStream :: Monad m => Stream m a -> m ()
+runStream = S.runStreamT
+
+-------------------------------------------------------------------------------
+-- Elimination
+-------------------------------------------------------------------------------
+
+toNull = runStream
+toList = S.toList
+foldl  = S.foldl (+) 0 id
+last   = S.last
+
+-------------------------------------------------------------------------------
+-- Transformation
+-------------------------------------------------------------------------------
+
+{-# INLINE transform #-}
+transform :: Monad m => Stream m a -> m ()
+transform = runStream
+
+scan          = transform . S.scan (+) 0 id
+map           = transform . fmap (+1)
+mapM          = transform . S.mapM return
+filterEven    = transform . S.filter even
+filterAllOut  = transform . S.filter (> maxValue)
+filterAllIn   = transform . S.filter (<= maxValue)
+takeOne       = transform . S.take 1
+takeAll       = transform . S.take maxValue
+takeWhileTrue = transform . S.takeWhile (<= maxValue)
+dropAll       = transform . S.drop maxValue
+dropWhileTrue = transform . S.dropWhile (<= maxValue)
+
+-------------------------------------------------------------------------------
+-- Zipping and concat
+-------------------------------------------------------------------------------
+
+zip src       = transform $ (S.zipWith (,) src src)
+concat _n     = return ()
+
+-------------------------------------------------------------------------------
+-- Composition
+-------------------------------------------------------------------------------
+
+{-# INLINE compose #-}
+compose :: Monad m => (Stream m Int -> Stream m Int) -> Stream m Int -> m ()
+compose f = transform . f . f . f . f
+
+composeMapM           = compose (S.mapM return)
+composeAllInFilters   = compose (S.filter (<= maxValue))
+composeAllOutFilters  = compose (S.filter (> maxValue))
+composeMapAllInFilter = compose (S.filter (<= maxValue) . fmap (subtract 1))
+
+composeScaling :: Monad m => Int -> Stream m Int -> m ()
+composeScaling m =
+    case m of
+        1 -> transform . f
+        2 -> transform . f . f
+        3 -> transform . f . f . f
+        4 -> transform . f . f . f . f
+        _ -> undefined
+    where f = S.filter (<= maxValue)
---
+++ benchmark/Benchmarks.hs
@@ -0,0 +1,61 @@
+-- |
+-- Module      : Main
+-- Copyright   : (c) 2018 Harendra Kumar
+--
+-- License     : BSD3
+-- Maintainer  : harendra.kumar@gmail.com
+
+import Control.DeepSeq (NFData)
+import Data.Functor.Identity (Identity, runIdentity)
+import System.Random (randomRIO)
+import qualified BenchmarkOps as Ops
+
+import Gauge
+
+-- We need a monadic bind here to make sure that the function f does not get
+-- completely optimized out by the compiler in some cases.
+benchIO :: (NFData b) => String -> (Ops.Stream m Int -> IO b) -> Benchmark
+benchIO name f = bench name $ nfIO $ randomRIO (1,1000) >>= f . Ops.source
+
+_benchId :: (NFData b) => String -> (Ops.Stream m Int -> Identity b) -> Benchmark
+_benchId name f = bench name $ nf (runIdentity . f) (Ops.source 10)
+
+main :: IO ()
+main = do
+  defaultMain
+    [ bgroup "elimination"
+      [ benchIO "toNull" Ops.toNull
+      , benchIO "toList" Ops.toList
+      , benchIO "fold" Ops.foldl
+      , benchIO "last" Ops.last
+      ]
+    , bgroup "transformation"
+      [ benchIO "scan" Ops.scan
+      , benchIO "map" Ops.map
+      , benchIO "mapM" Ops.mapM
+      , benchIO "concat" Ops.concat
+      ]
+    , bgroup "filtering"
+      [ benchIO "filter-even" Ops.filterEven
+      , benchIO "filter-all-out" Ops.filterAllOut
+      , benchIO "filter-all-in" Ops.filterAllIn
+      , benchIO "take-all" Ops.takeAll
+      , benchIO "takeWhile-true" Ops.takeWhileTrue
+      , benchIO "drop-all" Ops.dropAll
+      , benchIO "dropWhile-true" Ops.dropWhileTrue
+      ]
+    , benchIO "zip" Ops.zip
+    , bgroup "compose"
+      [ benchIO "mapM" Ops.composeMapM
+      , benchIO "map-with-all-in-filter" Ops.composeMapAllInFilter
+      , benchIO "all-in-filters" Ops.composeAllInFilters
+      , benchIO "all-out-filters" Ops.composeAllOutFilters
+      ]
+    , bgroup "compose-scaling"
+      -- Scaling with same operation in sequence
+      [ benchIO "1" $ Ops.composeScaling 1
+      , benchIO "2" $ Ops.composeScaling 2
+      , benchIO "3" $ Ops.composeScaling 3
+      , benchIO "4" $ Ops.composeScaling 4
+      ]
+   ]
