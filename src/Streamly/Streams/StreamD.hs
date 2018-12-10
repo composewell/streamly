e08ff863f6000dbb5eb794f9e975778948b49444
Harendra Kumar <harendra.kumar@gmail.com>
2018-12-10 22:55:08 +0530

    Move the StreamD Type in a separate module

--- src/Streamly/Streams/StreamD.hs
+++ src/Streamly/Streams/StreamD.hs
@@ -199,46 +199,10 @@ import Prelude
                notElem, null, head, tail, zipWith, lookup, foldr1, sequence,
                (!!), scanl, scanl1, concatMap, replicate, enumFromTo)
 
-import Streamly.SVar (MonadAsync, State(..), defState, adaptState)
+import Streamly.SVar (MonadAsync, defState, adaptState)
 
+import Streamly.Streams.StreamDType
 import qualified Streamly.Streams.StreamK as K
-
-------------------------------------------------------------------------------
--- The direct style stream type
-------------------------------------------------------------------------------
-
--- | A stream is a succession of 'Step's. A 'Yield' produces a single value and
--- the next state of the stream. 'Stop' indicates there are no more values in
--- the stream.
-data Step s a = Yield a s | Skip s | Stop
-
-instance Functor (Step s) where
-    {-# INLINE fmap #-}
-    fmap f (Yield x s) = Yield (f x) s
-    fmap _ (Skip s) = Skip s
-    fmap _ Stop = Stop
-
--- gst = global state
--- | A stream consists of a step function that generates the next step given a
--- current state, and the current state.
-data Stream m a =
-    forall s. UnStream (State K.Stream m a -> s -> m (Step s a)) s
-
--- Do not use the UnStream constructor directly, use the pattern synonym
--- instead.  XXX we should perhaps put the type in a separate module and do not
--- export the constructor.
-
-detachStream :: Stream m a -> Stream m a
-detachStream (UnStream step state) = UnStream step' state
-    where step' gst = step (adaptState gst)
-
-pattern Stream :: (State K.Stream m a -> s -> m (Step s a)) -> s -> Stream m a
-pattern Stream step state <- (detachStream -> UnStream step state)
-    where Stream = UnStream
-
-#if __GLASGOW_HASKELL__ >= 802
-{-# COMPLETE Stream #-}
-#endif
 
 ------------------------------------------------------------------------------
 -- Construction
@@ -1212,23 +1176,6 @@ uniq (Stream step state) = Stream step' (Nothing, state)
 -- Transformation by Mapping
 ------------------------------------------------------------------------------
 
--- | Map a monadic function over a 'Stream'
-{-# INLINE_NORMAL mapM #-}
-mapM :: Monad m => (a -> m b) -> Stream m a -> Stream m b
-mapM f (Stream step state) = Stream step' state
-  where
-    {-# INLINE_LATE step' #-}
-    step' gst st = do
-        r <- step (adaptState gst) st
-        case r of
-            Yield x s -> f x >>= \a -> return $ Yield a s
-            Skip s    -> return $ Skip s
-            Stop      -> return Stop
-
-{-# INLINE map #-}
-map :: Monad m => (a -> b) -> Stream m a -> Stream m b
-map f = mapM (return . f)
-
 {-# INLINE_NORMAL sequence #-}
 sequence :: Monad m => Stream m (m a) -> Stream m a
 sequence (Stream step state) = Stream step' state
@@ -1497,11 +1444,3 @@ the (Stream step state) = go state
                       | otherwise -> return Nothing
             Skip s -> go' n s
             Stop   -> return (Just n)
-
-------------------------------------------------------------------------------
--- Instances
-------------------------------------------------------------------------------
-
-instance Monad m => Functor (Stream m) where
-    {-# INLINE fmap #-}
-    fmap = map
---
+++ src/Streamly/Streams/StreamDType.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE BangPatterns              #-}
+{-# LANGUAGE CPP                       #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE PatternSynonyms           #-}
+{-# LANGUAGE ViewPatterns              #-}
+{-# LANGUAGE RankNTypes                #-}
+
+#include "inline.hs"
+
+-- |
+-- Module      : Streamly.Streams.StreamDType
+-- Copyright   : (c) 2018 Harendra Kumar
+--
+-- License     : BSD3
+-- Maintainer  : harendra.kumar@gmail.com
+-- Stability   : experimental
+-- Portability : GHC
+
+module Streamly.Streams.StreamDType
+    (
+    -- * The stream type
+      Step (..)
+#if __GLASGOW_HASKELL__ >= 800
+    , Stream (Stream)
+#else
+    , Stream ()
+    , pattern Stream
+#endif
+    , map
+    , mapM
+    )
+where
+
+import Streamly.SVar (State(..), adaptState)
+import qualified Streamly.Streams.StreamK as K
+import Prelude hiding (map, mapM)
+
+------------------------------------------------------------------------------
+-- The direct style stream type
+------------------------------------------------------------------------------
+
+-- | A stream is a succession of 'Step's. A 'Yield' produces a single value and
+-- the next state of the stream. 'Stop' indicates there are no more values in
+-- the stream.
+data Step s a = Yield a s | Skip s | Stop
+
+instance Functor (Step s) where
+    {-# INLINE fmap #-}
+    fmap f (Yield x s) = Yield (f x) s
+    fmap _ (Skip s) = Skip s
+    fmap _ Stop = Stop
+
+-- gst = global state
+-- | A stream consists of a step function that generates the next step given a
+-- current state, and the current state.
+data Stream m a =
+    forall s. UnStream (State K.Stream m a -> s -> m (Step s a)) s
+
+detachStream :: Stream m a -> Stream m a
+detachStream (UnStream step state) = UnStream step' state
+    where step' gst = step (adaptState gst)
+
+pattern Stream :: (State K.Stream m a -> s -> m (Step s a)) -> s -> Stream m a
+pattern Stream step state <- (detachStream -> UnStream step state)
+    where Stream = UnStream
+
+#if __GLASGOW_HASKELL__ >= 802
+{-# COMPLETE Stream #-}
+#endif
+
+------------------------------------------------------------------------------
+-- Instances
+------------------------------------------------------------------------------
+
+-- | Map a monadic function over a 'Stream'
+{-# INLINE_NORMAL mapM #-}
+mapM :: Monad m => (a -> m b) -> Stream m a -> Stream m b
+mapM f (Stream step state) = Stream step' state
+  where
+    {-# INLINE_LATE step' #-}
+    step' gst st = do
+        r <- step (adaptState gst) st
+        case r of
+            Yield x s -> f x >>= \a -> return $ Yield a s
+            Skip s    -> return $ Skip s
+            Stop      -> return Stop
+
+{-# INLINE map #-}
+map :: Monad m => (a -> b) -> Stream m a -> Stream m b
+map f = mapM (return . f)
+
+instance Monad m => Functor (Stream m) where
+    {-# INLINE fmap #-}
+    fmap = map
