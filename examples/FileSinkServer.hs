75ef6c21c15bd92f565fab801d6fbd3b174783ff
Harendra Kumar <harendra.kumar@gmail.com>
2019-05-30 12:10:37 +0530

    sync server example with changes in the library
    Use the file API instead of handle API

--- examples/FileSinkServer.hs
+++ examples/FileSinkServer.hs
@@ -5,33 +5,28 @@
 -- * lines from concurrent connections are merged into a single srteam
 -- * writes the line stream to an output file
 
-import Control.Monad.IO.Class (liftIO)
-import Data.Char (ord, chr)
-import System.IO (openFile, IOMode(..), hClose, hPutStrLn)
 import System.Environment (getArgs)
 
 import Streamly
+import Streamly.String
+import qualified Streamly.FileSystem.File as File
 import qualified Streamly.Fold as FL
 import qualified Streamly.Mem.Array as A
 import qualified Streamly.Network.Socket as NS
-import qualified Network.Socket as NS
 import qualified Streamly.Prelude as S
 
 main :: IO ()
 main = do
-    h <- openOutput
-    S.mapM_ (hPutLines h)
-        $ S.concatMapBy parallel (withSocket recv)
-        $ fmap fst (NS.recvConnectionsOn 8090)
-    hClose h
+    file <- fmap head getArgs
+    File.append file
+        $ encodeChar8Unchecked
+        $ S.concatMap A.read
+        $ S.concatMapBy parallel recv
+        $ NS.recvConnectionsOn 8090
 
     where
 
-    openOutput = fmap head getArgs >>= \file -> openFile file AppendMode
-
-    withSocket f sk = S.finally (liftIO (NS.close sk)) (f sk)
-    hPutLines h = hPutStrLn h . map (chr . fromIntegral) . A.toList
-    ord' = (fromIntegral . ord)
     recv =
-          FL.splitSuffixBy (== ord' '\n') (A.toArrayN 1024)
-        . NS.read
+          FL.splitSuffixBy' (== '\n') (A.toArrayN 1024)
+        . decodeChar8
+        . NS.fromSocket
