7d9c88b06fb0fe46d008befcc84c50805aaf9be2
Harendra Kumar <harendra.kumar@gmail.com>
2019-09-10 15:36:51 +0530

    Use String instead of Array as word type
    improves performance and memory utilization significantly.

--- examples/WordClassifier.hs
+++ examples/WordClassifier.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE FlexibleInstances #-}
 #if __GLASGOW_HASKELL__ >= 800
 {-# OPTIONS_GHC -Wno-orphans #-}
 #endif
@@ -16,6 +17,7 @@ import qualified Data.Ord as Ord
 import           Foreign.Storable (Storable(..))
 import qualified Streamly.Data.String as Streamly
 import qualified Streamly.FileSystem.Handle as FH
+import qualified Streamly.Fold as FL
 import qualified Streamly.Internal as Streamly
 import qualified Streamly.Memory.Array as A
 import qualified Streamly.Prelude as Streamly
@@ -50,11 +52,11 @@ main =
     in do
         name <- fmap head getArgs
         src <- openFile name ReadMode
-        FH.read src                  -- SerialT IO Word8
-         & Streamly.decodeChar8      -- SerialT IO Char
-         & Streamly.map toLower      -- SerialT IO Char
-         & Streamly.words            -- SerialT IO (Array Char)
-         & Streamly.filterM (Streamly.all isAlpha . A.read)
+        FH.read src                     -- SerialT IO Word8
+         & Streamly.decodeChar8         -- SerialT IO Char
+         & Streamly.map toLower         -- SerialT IO Char
+         & Streamly.foldWords FL.toList -- SerialT IO String
+         & Streamly.filter (all isAlpha)
          & Streamly.foldl' increment Map.empty
          & fmap (List.take 25 . List.sortOn (Ord.Down . snd) . Map.toList)
          >>= traverse_ print
