3c5c8bba0907cd68ca2d67041273535f566ea6b5
adithyaov <adi.obilisetty@gmail.com>
2019-10-27 05:15:43 +0530

    Add rollingHashFirstN and rollingHashLastN to Internal.Data.Fold

--- src/Streamly/Internal/Data/Fold.hs
+++ src/Streamly/Internal/Data/Fold.hs
@@ -56,8 +56,8 @@ module Streamly.Internal.Data.Fold
     , stdDev
     , rollingHash
     , rollingHashWithSalt
-    -- , rollingHashFirstN
-    -- , rollingHashLastN
+    , rollingHashFirstN
+    , rollingHashLastN
 
     -- ** Full Folds (Monoidal)
     , mconcat
@@ -566,6 +566,31 @@ defaultSalt = 0x087fc72c
 rollingHash :: (Monad m, Enum a) => Fold m a Int
 rollingHash = rollingHashWithSalt defaultSalt
 
+{-# INLINABLE rollingHashFirstN #-}
+rollingHashFirstN :: (Monad m, Enum a) => Int -> Fold m a Int
+rollingHashFirstN n = ltake n rollingHash
+
+-- XXX Reduce duplication?
+-- XXX Add tests and benchmarks
+-- XXX Change to strict data structures accordingly
+-- XXX Could potantially made faster
+{-# INLINABLE rollingHashLastN #-}
+rollingHashLastN :: (MonadIO m, Enum a, Storable a) => Int -> Fold m a Int
+rollingHashLastN n = Fold step' initial' done'
+  where
+    step' (rb, rh, i) a = do
+      rh1 <- liftIO $ RB.unsafeInsert rb rh a
+      return (rb, rh1, i + 1)
+    initial' = fmap (\(a, b) -> (a, b, 0)) $ liftIO $ RB.new n 
+    done' (rb, rh, i) = do
+      lst <- reverse <$> foldFunc i rh cons' [] rb
+      runFold rollingHash (fromList lst)
+    foldFunc i
+      | i < n = RB.unsafeFoldRingM 
+      | otherwise = RB.unsafeFoldRingFullM 
+    runFold (Fold step begin done) = foldlMx' step begin done
+    cons' b a = return $ a:b 
+
 ------------------------------------------------------------------------------
 -- Monoidal left folds
 ------------------------------------------------------------------------------
@@ -644,6 +669,7 @@ drainWhile p = ltakeWhile p drain
 
 -- XXX Add tests and benchmarks
 -- XXX Change to strict data structures accordingly
+-- XXX Could potantially made faster
 -- | Take last 'n' elements from the stream and discard the rest.
 {-# INLINABLE lastN #-}
 lastN :: (Storable a, MonadIO m) => Int -> Fold m a b -> Fold m a b
