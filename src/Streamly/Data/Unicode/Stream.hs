10bb2fb448a1d2d5767d3ec35db8f7c3813f9144
Harendra Kumar <harendra.kumar@gmail.com>
2019-11-05 02:12:57 +0530

    update unicode stream doc

--- src/Streamly/Data/Unicode/Stream.hs
+++ src/Streamly/Data/Unicode/Stream.hs
@@ -8,23 +8,43 @@
 -- Stability   : experimental
 -- Portability : GHC
 --
--- A byte stream of Unicode text can be decoded into a stream of 'Char' and
--- processed efficiently using regular stream processing operations. A stream
--- of 'Char' can be encoded into a byte stream using UTF formats and written to
--- IO devices.
+-- = Processing Unicode Strings
 --
--- If you have to store a unicode string in memory you can either write it as a
--- pure stream "SerialT Identity Char" or write the 'Char' stream as "Array
--- Char". If space efficiency is a concern you can UTF8 encode the 'Char'
--- stream and then write it as "Array Word8".  "SerialT Identity Char" and
--- "Array Char" are instances of 'IsString' and 'IsList', therefore,
--- 'OverloadedStrings' and 'OverloadedLists' extensions can be used for
--- convenience.
+-- A 'Char' stream is the canonical representation to process Unicode strings.
+-- It can be processed efficiently using regular stream processing operations.
+-- A byte stream of Unicode text read from an IO device or from an
+-- 'Streamly.Memory.Array.Array' in memory can be decoded into a 'Char' stream
+-- using the decoding routines in this module.  A 'String' (@[Char]@) can be
+-- converted into a 'Char' stream using 'Streamly.Prelude.fromList'.  An @Array
+-- Char@ can be 'Streamly.Prelude.unfold'ed into a stream using the array
+-- 'Streamly.Memory.Array.read' unfold.
 --
--- Some experimental APIs to conveniently process text using "Array Char"
--- represenation can be found in "Streamly.Internal.Unicode.Array".
+-- = Storing Unicode Strings
 --
--- Please note the following:
+-- A stream of 'Char' can be encoded into a byte stream using the encoding
+-- routines in this module and then written to IO devices or to arrays in
+-- memory.
+--
+-- If you have to store a 'Char' stream in memory you can convert it into a
+-- 'String' using 'Streamly.Prelude.toList' or using the
+-- 'Streamly.Data.Fold.toList' fold. The 'String' type can be more efficient
+-- than pinned arrays for short and short lived strings.
+--
+-- For longer or long lived streams you can 'Streamly.Prelude.fold' the 'Char'
+-- stream as @Array Char@ using the array 'Streamly.Memory.Array.write' fold.
+-- The 'Array' type provides a more compact representation and pinned memory
+-- reducing GC overhead. If space efficiency is a concern you can use
+-- 'encodeUtf8' on the 'Char' stream before writing it to an 'Array' providing
+-- an even more compact representation.
+--
+-- = String Literals
+--
+-- @SerialT Identity Char@ and @Array Char@ are instances of 'IsString' and
+-- 'IsList', therefore, 'OverloadedStrings' and 'OverloadedLists' extensions
+-- can be used for convenience when specifying unicode strings literals using
+-- these types.
+--
+-- = Pitfalls
 --
 -- * Case conversion: Some unicode characters translate to more than one code
 -- point on case conversion. The 'toUpper' and 'toLower' functions in @base@
@@ -35,7 +55,15 @@
 -- different unicode representations, therefore, a character stream or
 -- character array cannot be directly compared. A normalized comparison may be
 -- needed to check string equivalence correctly.
+--
+-- = Experimental APIs
+--
+-- Some experimental APIs to conveniently process text using the @Array Char@
+-- represenation directly can be found in "Streamly.Internal.Unicode.Array".
 
+-- XXX an unpinned array representation can be useful to store short and short
+-- lived strings in memory.
+--
 module Streamly.Data.Unicode.Stream
     (
     -- * Construction (Decoding)
