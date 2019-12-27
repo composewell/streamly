9710c7b639628dc68a674fffa33bf6e8ac34f7fc
Harendra Kumar <harendra.kumar@gmail.com>
2019-12-27 22:44:06 +0530

    Change newVar signature, add modifyVar'

--- src/Streamly/Internal/Mutable/Prim/Var.hs
+++ src/Streamly/Internal/Mutable/Prim/Var.hs
@@ -18,8 +18,8 @@
 -- Stability   : experimental
 -- Portability : GHC
 --
--- A mutable variable in a mutable monad (IO/ST) holding a 'Prim' value. This
--- allows fast modification of a value.
+-- A mutable variable in a mutation capable monad (IO/ST) holding a 'Prim'
+-- value. This allows fast modification because of unboxed storage.
 
 module Streamly.Internal.Mutable.Prim.Var
     (
@@ -28,13 +28,13 @@ module Streamly.Internal.Mutable.Prim.Var
     , Prim
 
     -- * Construction
-
     , newVar
 
-    -- * modification
+    -- * Write
     , writeVar
+    , modifyVar'
 
-    -- * modification
+    -- * Read
     , readVar
     )
 where
@@ -43,19 +43,23 @@ import Control.Monad.Primitive (PrimMonad(..), primitive_)
 import Data.Primitive.Types (Prim, sizeOf#, readByteArray#, writeByteArray#)
 import GHC.Exts (MutableByteArray#, newByteArray#)
 
--- XXX use unlifted newtype?
---
 -- | A 'Var' holds a single 'Prim' value.
 data Var m a = Var (MutableByteArray# (PrimState m))
 
+-- The name PrimMonad does not give a clue what it means, an explicit "Mut"
+-- suffix provides a better hint. MonadMut is just a generalization of MonadIO.
+--
+-- | A monad that allows mutable operations using a state token.
 type MonadMut = PrimMonad
 
 -- | Create a new mutable variable.
 {-# INLINE newVar #-}
-newVar :: forall m a. (MonadMut m, Prim a) => m (Var m a)
-newVar = primitive (\s# ->
+newVar :: forall m a. (MonadMut m, Prim a) => a -> m (Var m a)
+newVar x = primitive (\s# ->
       case newByteArray# (sizeOf# (undefined :: a)) s# of
-        (# s'#, arr# #) -> (# s'#, Var arr# #)
+        (# s1#, arr# #) ->
+            case writeByteArray# arr# 0# x s1# of
+                s2# -> (# s2#, Var arr# #)
     )
 
 -- | Write a value to a mutable variable.
@@ -63,6 +67,15 @@ newVar = primitive (\s# ->
 writeVar :: (MonadMut m, Prim a) => Var m a -> a -> m ()
 writeVar (Var arr#) x = primitive_ (writeByteArray# arr# 0# x)
 
+-- | Read a value from a variable.
 {-# INLINE readVar #-}
 readVar :: (MonadMut m, Prim a) => Var m a -> m a
 readVar (Var arr#) = primitive (readByteArray# arr# 0#)
+
+-- | Modify the value of a mutable variable using a function with strict
+-- application.
+{-# INLINE modifyVar' #-}
+modifyVar' :: (MonadMut m, Prim a) => Var m a -> (a -> a) -> m ()
+modifyVar' (Var arr#) g = primitive_ $ \s# ->
+  case readByteArray# arr# 0# s# of
+    (# s'#, a #) -> let a' = g a in a' `seq` writeByteArray# arr# 0# a' s'#
