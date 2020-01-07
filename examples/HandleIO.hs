2c7729883769c01c6cb22526a000113218834dc0
Harendra Kumar <harendra.kumar@gmail.com>
2020-01-07 18:43:54 +0530

    update the examples and add comments

--- examples/HandleIO.hs
+++ examples/HandleIO.hs
@@ -1,55 +1,80 @@
-import qualified Streamly.Prelude as S
-import qualified Streamly.Data.Fold as FL
--- import qualified Streamly.Memory.Array as A
-import qualified Streamly.FileSystem.Handle as FH
-import qualified System.IO as FH
--- import qualified Streamly.FileSystem.FD as FH
--- import qualified Streamly.Data.Unicode.Stream as US
-
-import qualified Streamly.Internal.Data.Fold as FL
-import qualified Streamly.Internal.Memory.ArrayStream as AS
-import qualified Streamly.Internal.FileSystem.Handle as IFH
-
 import Data.Char (ord)
 import System.Environment (getArgs)
 import System.IO (IOMode(..), hSeek, SeekMode(..))
 
+import qualified Streamly.Data.Fold as FL
+import qualified Streamly.FileSystem.Handle as FH
+import qualified System.IO as FH
+import qualified Streamly.Memory.Array as A
+import qualified Streamly.Prelude as S
+-- import qualified Streamly.FileSystem.FD as FH
+
+import qualified Streamly.Internal.Data.Fold as FL
+import qualified Streamly.Internal.Data.Unicode.Stream as US
+import qualified Streamly.Internal.Memory.ArrayStream as AS
+import qualified Streamly.Internal.Prelude as S
+
+-- Read the contents of a file to stdout.
+--
+-- FH.read reads the file in 32KB chunks and converts the chunks into a byte
+-- stream. FH.write takes the byte stream as input, converts it into chunks of
+-- 32KB and writes those chunks to stdout.
+--
+_cat :: FH.Handle -> IO ()
+_cat src = S.fold (FH.write FH.stdout) $ S.unfold FH.read src
+
+-- Chunked version, more efficient than the byte stream version above. Reads
+-- the file in 256KB chunks and writes those chunks to stdout.
 cat :: FH.Handle -> IO ()
 cat src =
       S.fold (FH.writeChunks FH.stdout)
-    $ IFH.toChunksWithBufferOf (256*1024) src
--- byte stream version
--- cat src = S.fold (FH.write FH.stdout) $ FH.read src
+    $ S.unfold FH.readChunksWithBufferOf ((256*1024), src)
 
+-- Copy a source file to a destination file.
+--
+-- FH.read reads the file in 32KB chunks and converts the chunks into a byte
+-- stream. FH.write takes the byte stream as input, converts it into chunks of
+-- 32KB and writes those chunks to the destination file.
+_cp :: FH.Handle -> FH.Handle -> IO ()
+_cp src dst = S.fold (FH.write dst) $ S.unfold FH.read src
+
+-- Chunked version, more efficient than the byte stream version above. Reads
+-- the file in 256KB chunks and writes those chunks to stdout.
 cp :: FH.Handle -> FH.Handle -> IO ()
 cp src dst =
       S.fold (FH.writeChunks dst)
-    $ IFH.toChunksWithBufferOf (256*1024) src
--- byte stream version
--- cp src dst = S.fold (FH.write dst) $ FH.read src
+    $ S.unfold FH.readChunksWithBufferOf ((256*1024), src)
 
 ord' :: Num a => Char -> a
 ord' = (fromIntegral . ord)
 
+-- Count lines like wc -l.
+--
+-- Char stream version. Reads the input as a byte stream, splits it into lines
+-- and counts the lines..
+_wcl :: FH.Handle -> IO ()
+_wcl src = print =<< (S.length
+    $ US.lines FL.drain
+    $ US.decodeLatin1
+    $ S.unfold FH.read src)
+
+-- More efficient chunked version. Reads chunks from the input handles and
+-- splits the chunks directly instead of converting them into byte stream
+-- first.
 wcl :: FH.Handle -> IO ()
 wcl src = print =<< (S.length
     $ AS.splitOn 10
-    $ IFH.toChunks src)
-{-
--- Char stream version
-wcl src = print =<< (S.length
-    $ flip US.lines FL.drain
-    $ US.decodeLatin1
-    $ FH.read src)
--}
+    $ S.unfold FH.readChunks src)
 
-{-
+-- grep -c
+--
+-- count the occurrences of a pattern in a file.
 grepc :: String -> FH.Handle -> IO ()
 grepc pat src = print . (subtract 1) =<< (S.length
-    $ FL.splitOnSeq (A.fromList (map ord' pat)) FL.drain
-    $ FH.read src)
--}
+    $ S.splitOnSeq (A.fromList (map ord' pat)) FL.drain
+    $ S.unfold FH.read src)
 
+-- Compute the average line length in a file.
 avgll :: FH.Handle -> IO ()
 avgll src = print =<< (S.fold avg
     $ S.splitWithSuffix (== ord' '\n') FL.length
@@ -57,6 +82,7 @@ avgll src = print =<< (S.fold avg
     where avg = (/) <$> toDouble FL.sum <*> toDouble FL.length
           toDouble = fmap (fromIntegral :: Int -> Double)
 
+-- histogram of line lengths in a file
 llhisto :: FH.Handle -> IO ()
 llhisto src = print =<< (S.fold (FL.classify FL.length)
     $ S.map bucket
@@ -73,7 +99,7 @@ main = do
 
     rewind >> putStrLn "cat"    >> cat src          -- Unix cat program
     rewind >> putStr "wcl "     >> wcl src          -- Unix wc -l program
- -- rewind >> putStr "grepc "   >> grepc "aaaa" src -- Unix grep -c program
+    rewind >> putStr "grepc "   >> grepc "aaaa" src -- Unix grep -c program
     rewind >> putStr "avgll "   >> avgll src        -- get average line length
     rewind >> putStr "llhisto " >> llhisto src      -- get line length histogram
 
