4562ea97f47109bef367248e6c2134ec118560f8
adithyaov <adi.obilisetty@gmail.com>
2020-01-07 17:08:23 +0530

    Add test for reassembleBy combinator

--- test/Streamly/Test/Internal/Prelude.hs
+++ test/Streamly/Test/Internal/Prelude.hs
@@ -25,6 +25,12 @@ import Streamly.Internal.Data.Time.Units
        (AbsTime, NanoSecond64(..), toRelTime64, diffAbsTime64)
 import Data.Int (Int64)
 
+import Test.Hspec.QuickCheck
+import Test.QuickCheck (Property, forAll, choose)
+import Test.QuickCheck.Monadic (monadicIO, assert)
+
+import Streamly
+
 tenPow8 :: Int64
 tenPow8 = 10^(8 :: Int)
 
@@ -70,9 +76,52 @@ testDropByTime = do
         $ S.repeatM (threadDelay 1000 >> getTime Monotonic)
     checkTakeDropTime (Just t0, mt1)
 
+
+newtype WholeInt = WholeInt Int deriving (Eq, Ord, Show)
+
+instance Num WholeInt where
+    (WholeInt x) + (WholeInt y) = WholeInt (x + y)
+    (WholeInt x) * (WholeInt y) = WholeInt (x * y)
+    (WholeInt x) - (WholeInt y) = if x >= y
+                                     then WholeInt (x - y)
+                                     else error "Invaid subtraction: WholeInt"
+    fromInteger x = if x >= 0
+                       then WholeInt $ fromIntegral x
+                       else error "Cannot convert -ve Int to WholeInt"
+    negate = undefined
+    abs = undefined
+    signum = undefined
+
+instance Bounded WholeInt where
+    minBound = WholeInt 0
+    maxBound = WholeInt maxBound
+
+diff :: WholeInt -> WholeInt -> Int
+diff (WholeInt x) (WholeInt y) = x - y
+
+src :: (IsStream t, Monad m) => Int -> Int -> t m WholeInt
+src value' i = S.unfoldr step (WholeInt (i - 1))
+      where
+        step cnt
+            | cnt > WholeInt value' = Nothing
+            | unb cnt `rem` i == 0 = Just (cnt, cnt + 2 * WholeInt i - 1)
+            | otherwise = Just (cnt, cnt - 1)
+        unb (WholeInt x) = x
+
+
+testReassembleBy :: Property
+testReassembleBy =
+  forAll (choose (2, 5)) $ \i ->
+  forAll (choose (10, 20)) $ \v -> monadicIO $ do
+  l1 <- S.toList $ SI.reassembleBy i diff $ src (v * i) i
+  let l2 = map WholeInt [0..(v * i - 1)]
+  assert $ l1 == l2
+
 main :: IO ()
 main =
     hspec $
     describe "Filtering" $ do
         it "takeByTime" (testTakeByTime `shouldReturn` True)
         it "dropByTime" (testDropByTime `shouldReturn` True)
+    describe "ReassembleBy combinator" $ do
+        prop "testReassembleBy" testReassembleBy
