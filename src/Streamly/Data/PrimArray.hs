f20540c24f99f3a249c128b7deb318e19990d6df
adithyaov <adi.obilisetty@gmail.com>
2020-01-20 14:59:59 +0530

    Expose PrimArray from Internal

---
+++ src/Streamly/Data/PrimArray.hs
@@ -0,0 +1,43 @@
+-- |
+-- Module      : Streamly.Data.PrimArray
+-- Copyright   : (c) 2019 Composewell Technologies
+--
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+module Streamly.Data.PrimArray
+    ( PrimArray
+
+    -- * Construction
+    , A.fromListN
+    , A.fromList
+
+    -- Stream Folds
+    , A.fromStreamN
+    , A.fromStream
+
+    -- MonadicAPIs
+    , A.writeN
+    , A.write
+
+    -- * Elimination
+
+    , A.toStream
+    , A.toStreamRev
+    , A.read
+
+    -- * Random Access
+
+    -- * Folding Arrays
+    , A.streamFold
+    , A.fold
+
+    , A.length
+    )
+where
+
+import Streamly.Internal.Data.PrimArray (PrimArray)
+
+import qualified Streamly.Internal.Data.PrimArray as A
