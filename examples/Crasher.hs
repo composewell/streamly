8c61c14900a1dc69ba2f0b2aec2d80e81444e604
Harendra Kumar <harendra.kumar@gmail.com>
2020-01-27 13:15:16 +0530

    Further simplify the crasher example

--- examples/Crasher.hs
+++ examples/Crasher.hs
@@ -1,18 +1,11 @@
 import Data.Function ((&))
 
-import Streamly
-import qualified Streamly.Memory.Array as A
+import Streamly (async)
 import qualified Streamly.Prelude as S
 import qualified Streamly.Internal.Network.Socket as SK
 import qualified Streamly.Internal.Network.Inet.TCP as TCP
 
 main =
-      S.unfold TCP.acceptOnPort 8090        -- SerialT IO Socket
-    & S.concatMapWith async frames  -- SerialT IO (Array Word8)
+      S.unfold TCP.acceptOnPort 8090            -- SerialT IO Socket
+    & S.concatMapWith async (S.unfold SK.read)  -- SerialT IO (Array Word8)
     & S.drain
-
-    where
-
-    frames sk =
-          S.unfold SK.read sk
-        & S.chunksOf 1 (A.writeN 1)
