5f86fd374b7241cee6682f9ae90bf951a600bb77
Harendra Kumar <harendra.kumar@gmail.com>
2020-02-03 16:42:35 +0530

    Add documentation, format code, use forkIO for baseline

--- benchmark/Concurrent.hs
+++ benchmark/Concurrent.hs
@@ -7,8 +7,7 @@
 -- Maintainer  : streamly@composewell.com
 
 import Control.Concurrent
-import Control.Monad (when)
--- import Data.IORef
+import Control.Monad (when, replicateM)
 
 import Gauge
 import Streamly
@@ -18,7 +17,10 @@ import qualified Streamly.Prelude as S
 -- Append
 -------------------------------------------------------------------------------
 
--- Single work item yielded per thread
+-- | Run @tcount@ number of actions concurrently using the given concurrency
+-- style. Each thread produces a single output after a delay of @d@
+-- microseconds.
+--
 {-# INLINE append #-}
 append :: IsStream t
     => Int -> Int -> Int -> (t IO Int -> SerialT IO Int) -> IO ()
@@ -30,11 +32,22 @@ append buflen tcount d t =
         $ maxThreads (-1)
         $ S.fromFoldableM $ map work [1..tcount]
 
--- Big stream of items yielded per thread
+-- | Run @threads@ concurrently, each producing streams of @elems@ elements
+-- with a delay of @d@ microseconds between successive elements, and merge
+-- their outputs in a single output stream. The individual streams are produced
+-- serially but merged using the provided concurrency style.
+--
 {-# INLINE concated #-}
-concated :: Int -> Int -> Int -> Int -> (forall a. SerialT IO a -> SerialT IO a -> SerialT IO a) -> IO ()
+concated
+    :: Int
+    -> Int
+    -> Int
+    -> Int
+    -> (forall a. SerialT IO a -> SerialT IO a -> SerialT IO a)
+    -> IO ()
 concated buflen threads d elems t =
-    let work = (\i -> (S.replicateM i ((when (d /= 0) (threadDelay d)) >> return i)))
+    let work = \i -> S.replicateM i
+                        ((when (d /= 0) (threadDelay d)) >> return i)
     in S.drain
         $ adapt
         $ maxThreads (-1)
@@ -72,25 +85,19 @@ main = do
 
     [ -- bgroup "append/buf-1-threads-10k-0sec"  (appendGroup 1 10000 0)
     -- , bgroup "append/buf-100-threads-100k-0sec"  (appendGroup 100 100000 0)
-      bgroup "append/buf-10k-threads-10k-5sec"  (appendGroup 10000 10000 5000000)
+      bgroup "stream1x10k/buf10k-threads10k-5sec"  (appendGroup 10000 10000 5000000)
     --  bgroup "concat/buf-1-threads-100k-count-1" (concatGroup 1 100000 0 1)
     --  bgroup "concat/buf-1-threads-1-count-10m" (concatGroup 1 1 0 10000000)
-     , bgroup "concat/buf-100-threads-100-count-500k"  (concatGroup 100 100 0 500000)
-    {-
-    , bgroup "forkIO-5000ms-10k" $
-    let delay = threadDelay 5000000
-        count = 10000 :: Int
-        list = [1..count]
-        work i = delay >> return i
-    in
-    [ bench "discard" $ nfIO $ do
-        mapM_ (\i -> forkIO $ work i >> return ()) list
-        threadDelay 6000000
-    , bench "collect" $ nfIO $ do
-        ref <- newIORef []
-        mapM_ (\i -> forkIO $ work i >>=
-               \j -> atomicModifyIORef ref $ \xs -> (j : xs, ())) list
-        threadDelay 6000000
-    ]
-    -}
+    , bgroup "streams100x500k/buf100-threads100"  (concatGroup 100 100 0 500000)
+
+    , bench "forkIO/threads10k-5sec" $
+        let delay = threadDelay 5000000
+            count = 10000 :: Int
+            list = [1..count]
+            work i = delay >> return i
+        in nfIO $ do
+            ref <- newEmptyMVar
+            mapM_ (\i -> forkIO $ work i >>=
+                   \j -> putMVar ref j) list
+            replicateM 10000 (takeMVar ref)
    ]
