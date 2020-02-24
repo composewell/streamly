2b2d6518d54bdeb5603a1c2aa84ced2db9a42b92
adithyaov <adi.obilisetty@gmail.com>
2020-02-24 18:22:23 +0530

    Add tests for reassembleBy

--- test/Streamly/Test/Internal/Prelude.hs
+++ test/Streamly/Test/Internal/Prelude.hs
@@ -25,6 +25,14 @@ import Streamly.Internal.Data.Time.Units
        (AbsTime, NanoSecond64(..), toRelTime64, diffAbsTime64)
 import Data.Int (Int64)
 
+import Test.Hspec.QuickCheck
+import Test.QuickCheck (Property, forAll, choose)
+import Test.QuickCheck.Monadic (monadicIO, assert)
+
+import Streamly
+
+
+
 tenPow8 :: Int64
 tenPow8 = 10^(8 :: Int)
 
@@ -70,9 +78,45 @@ testDropByTime = do
         $ S.repeatM (threadDelay 1000 >> getTime Monotonic)
     checkTakeDropTime (Just t0, mt1)
 
+newtype WholeInt = WholeInt Int deriving (Eq)
+
+instance Bounded WholeInt where
+    minBound = WholeInt 0
+    maxBound = WholeInt maxBound
+
+{-# INLINE diff #-}
+diff :: WholeInt -> WholeInt -> Int
+diff (WholeInt x) (WholeInt y) = x - y
+
+srcOrd :: Monad m => Int -> Int -> SerialT m WholeInt
+srcOrd value' i = S.unfoldr step (i, 0 :: Int)
+  where
+    step (a, b)
+        | c > value' = Nothing
+        | otherwise =
+            Just
+                ( WholeInt c
+                , ( if a + i == 0
+                        then i
+                        else a - 2
+                  , b + 1))
+      where
+        c = a + b
+
+testReassembleBy :: Property
+testReassembleBy =
+    forAll (choose (1, 5)) $ \i ->
+        forAll (choose (10, 20)) $ \v ->
+            monadicIO $ do
+                l1 <- S.toList $ SI.reassembleBy (i + 1) diff $ srcOrd (v * (i + 1)) i
+                let l2 = map WholeInt [0 .. (v * (i + 1) - 1)]
+                assert $ l1 == l2
+
 main :: IO ()
 main =
-    hspec $
-    describe "Filtering" $ do
-        it "takeByTime" (testTakeByTime `shouldReturn` True)
-        it "dropByTime" (testDropByTime `shouldReturn` True)
+    hspec $ do
+        describe "Filtering" $ do
+            it "takeByTime" (testTakeByTime `shouldReturn` True)
+            it "dropByTime" (testDropByTime `shouldReturn` True)
+        describe "Reordering" $ do
+            prop "reassembleBy" testReassembleBy
