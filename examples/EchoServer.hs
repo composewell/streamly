eb7150874968dd3f12c89b65705e1832f2f06ae6
Harendra Kumar <harendra@composewell.com>
2020-03-02 19:33:08 +0530

    improve Network.Socket doc and example

--- examples/EchoServer.hs
+++ examples/EchoServer.hs
@@ -1,19 +1,22 @@
 -- A concurrent TCP server that echoes everything that it receives.
 
-import Control.Exception (finally)
-import Control.Monad.IO.Class (liftIO)
+import Data.Function ((&))
+
 import Streamly
+import Streamly.Internal.Network.Socket (handleWithM)
 import Streamly.Network.Socket
-import qualified Network.Socket as Net
+
 import qualified Streamly.Network.Inet.TCP as TCP
 import qualified Streamly.Prelude as S
 
 main :: IO ()
-main = S.drain
-    $ parallely $ S.mapM (useWith echo)
-    $ serially $ S.unfold TCP.acceptOnPort 8091
+main =
+      serially (S.unfold TCP.acceptOnPort 8091)
+    & parallely . S.mapM (handleWithM echo)
+    & S.drain
+
     where
+
     echo sk =
-          S.fold (writeChunks sk)
-        $ S.unfold readChunksWithBufferOf (32768, sk)
-    useWith f sk = finally (f sk) (liftIO (Net.close sk))
+          S.unfold readChunksWithBufferOf (32768, sk) -- SerialT IO Socket
+        & S.fold (writeChunks sk)                     -- IO ()
