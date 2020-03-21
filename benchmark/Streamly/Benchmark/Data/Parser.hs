df633139c2f977c87c1cc4d953c84c8d83938e00
Harendra Kumar <harendra@composewell.com>
2020-03-22 02:58:44 +0530

    Move benchmark code for parsers to its own module

---
+++ benchmark/Streamly/Benchmark/Data/Parser.hs
@@ -0,0 +1,103 @@
+-- |
+-- Module      : Streamly.Benchmark.Data.Fold
+-- Copyright   : (c) 2018 Composewell
+--
+-- License     : MIT
+-- Maintainer  : streamly@composewell.com
+
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main
+  (
+    main
+  ) where
+
+import Control.DeepSeq (NFData(..))
+import System.Random (randomRIO)
+import Prelude hiding (any, all, take)
+
+import qualified Streamly as S hiding (runStream)
+import qualified Streamly.Prelude  as S
+import qualified Streamly.Internal.Data.Fold as FL
+import qualified Streamly.Internal.Data.Parse as PR
+import qualified Streamly.Internal.Prelude as IP
+
+import Gauge
+import Streamly hiding (runStream)
+import Streamly.Benchmark.Common
+
+-------------------------------------------------------------------------------
+-- Utilities
+-------------------------------------------------------------------------------
+
+-- XXX these can be moved to the common module
+
+-- We need a monadic bind here to make sure that the function f does not get
+-- completely optimized out by the compiler in some cases.
+
+{-# INLINE sourceUnfoldrM #-}
+sourceUnfoldrM :: (S.IsStream t, S.MonadAsync m) => Int -> Int -> t m Int
+sourceUnfoldrM value n = S.unfoldrM step n
+    where
+    step cnt =
+        if cnt > n + value
+        then return Nothing
+        else return (Just (cnt, cnt + 1))
+
+-- | Takes a fold method, and uses it with a default source.
+{-# INLINE benchIOSink #-}
+benchIOSink
+    :: (IsStream t, NFData b)
+    => Int -> String -> (t IO Int -> IO b) -> Benchmark
+benchIOSink value name f =
+    bench name $ nfIO $ randomRIO (1,1) >>= f . sourceUnfoldrM value
+
+-------------------------------------------------------------------------------
+-- Parsers
+-------------------------------------------------------------------------------
+
+any :: (Monad m, Ord a) => a -> SerialT m a -> m (Either String Bool)
+any value = IP.parse (PR.any (> value))
+
+all :: (Monad m, Ord a) => a -> SerialT m a -> m (Either String Bool)
+all value = IP.parse (PR.all (<= value))
+
+zipAllAny :: (Monad m, Ord a)
+    => a -> SerialT m a -> m (Either String (Bool, Bool))
+zipAllAny value = IP.parse ((,) <$> PR.all (<= value) <*> PR.any (> value))
+
+take :: Monad m => Int -> SerialT m a -> m (Either String ())
+take value = IP.parse (FL.take value FL.drain)
+
+-------------------------------------------------------------------------------
+-- Benchmarks
+-------------------------------------------------------------------------------
+
+o_1_space_serial_parse :: Int -> [Benchmark]
+o_1_space_serial_parse value =
+    [ benchIOSink value "any" $ any value
+    , benchIOSink value "all" $ all value
+    , benchIOSink value "(all,any)" $ zipAllAny value
+    , benchIOSink value "take" $ take value
+    ]
+
+
+-------------------------------------------------------------------------------
+-- Driver
+-------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    (value, cfg, benches) <- parseCLIOpts defaultStreamSize
+    value `seq` runMode (mode cfg) cfg benches (allBenchmarks value)
+
+    where
+
+    allBenchmarks value =
+        [ bgroup "o1"
+            [ bgroup "parser" $ concat
+                [
+                  o_1_space_serial_parse value
+                ]
+            ]
+        ]
