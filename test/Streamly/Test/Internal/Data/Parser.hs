278fb61cba03facddb0b7b96ad058587567001a3
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-05-11 16:01:51 +0530

    added test for die and dieM of parseD

--- test/Streamly/Test/Internal/Data/Parser.hs
+++ test/Streamly/Test/Internal/Data/Parser.hs
@@ -1,11 +1,12 @@
 module Main (main) where
 
-import Streamly.Internal.Data.Parser
+import Streamly.Internal.Data.Parser hiding (die, dieM)
+import Streamly.Internal.Data.Parser.ParserD(die, dieM)
 import qualified Streamly.Internal.Prelude as S
 
 import Test.Hspec(hspec)
 import Test.Hspec.QuickCheck
-import Test.QuickCheck (forAll, chooseInt, Property)
+import Test.QuickCheck (forAll, chooseInt, Property, property)
 
 testYield :: Property
 testYield = 
@@ -21,7 +22,23 @@ testYieldM =
             Right r -> r == x
             Left _ -> False
 
+testDie :: Property
+testDie =
+    property $
+    case S.parseD (die "die test") (S.fromList [0 :: Int]) of
+        Right _ -> False
+        Left _ -> True
+
+testDieM :: Property
+testDieM =
+    property $
+    case S.parseD (dieM (Right "die test")) (S.fromList [0 :: Int]) of
+        Right _ -> False
+        Left _ -> True
+
 main :: IO ()
 main = hspec $ do
     prop "test yield function" testYield
     prop "test yieldM function" testYieldM
+    prop "test die function" testDie
+    prop "test dieM function" testDieM
