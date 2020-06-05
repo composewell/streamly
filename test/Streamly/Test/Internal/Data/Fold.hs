03ee5ef784eff9b3fcf8aa7f909132d7a1525a39
Shruti_Umat <111701027@smail.iitpkd.ac.in>
2020-06-05 23:07:28 +0530

    remove prefix test from test function names
    fix indentation
    
    change repo token for travis build
    
    fix do block indentation

--- test/Streamly/Test/Internal/Data/Fold.hs
+++ test/Streamly/Test/Internal/Data/Fold.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
-import Streamly.Internal.Data.Fold
+import Prelude hiding (maximum, minimum, elem, notElem, null, product, sum, head, last)
+import qualified Streamly.Internal.Data.Fold as F
 import qualified Streamly.Prelude as S
 import qualified Streamly.Data.Fold as FL
 
@@ -22,33 +23,33 @@ intMax = maxBound
 {-# INLINE intMin #-}
 {-# INLINE intMax #-}
 
-testRollingHashFirstN :: Property
-testRollingHashFirstN =
+rollingHashFirstN :: Property
+rollingHashFirstN =
     forAll (choose (0, maxStreamLen)) $ \len ->
         forAll (choose (0, len)) $ \n ->
             forAll (vectorOf len (arbitrary :: Gen Int)) $ \vec -> monadicIO $ do
-                a <- run $ S.fold rollingHash $ S.take n $ S.fromList vec
-                b <- run $ S.fold (rollingHashFirstN n) $ S.fromList vec
+                a <- run $ S.fold F.rollingHash $ S.take n $ S.fromList vec
+                b <- run $ S.fold (F.rollingHashFirstN n) $ S.fromList vec
                 assert $ a == b
 
 
-testHead :: [Int] -> Expectation
-testHead ls = S.fold FL.head (S.fromList ls) `shouldReturn` headl ls
+head :: [Int] -> Expectation
+head ls = S.fold FL.head (S.fromList ls) `shouldReturn` headl ls
             where
             headl [] = Nothing
             headl (x:_) = Just x
 
 
-testLength :: [Int] -> Expectation
-testLength ls = S.fold FL.length (S.fromList ls) `shouldReturn` Prelude.length ls
+length :: [Int] -> Expectation
+length ls = S.fold FL.length (S.fromList ls) `shouldReturn` Prelude.length ls
 
 
-testSum :: [Int] -> Expectation
-testSum ls = S.fold FL.sum (S.fromList ls) `shouldReturn` foldl (+) 0 ls
+sum :: [Int] -> Expectation
+sum ls = S.fold FL.sum (S.fromList ls) `shouldReturn` foldl (+) 0 ls
 
 
-testProduct :: [Int] -> Expectation
-testProduct ls = S.fold FL.product (S.fromList ls) `shouldReturn` foldl (*) 1 ls
+product :: [Int] -> Expectation
+product ls = S.fold FL.product (S.fromList ls) `shouldReturn` foldl (*) 1 ls
 
 
 lesser :: (a -> a -> Ordering) -> a -> a -> a
@@ -60,29 +61,30 @@ greater f x y = if f x y == GT then x else y
 
 
 foldMaybe :: (b -> a -> b) -> b -> [a] -> Maybe b
-foldMaybe f acc ls = case ls of
-                        [] -> Nothing
-                        _ -> Just (foldl f acc ls)
+foldMaybe f acc ls =
+                  case ls of
+                     [] -> Nothing
+                     _ -> Just (foldl f acc ls)
 
 
-testMaximumBy :: (Ord a, Show a) => a -> (a -> a -> Ordering) -> [a] -> Expectation
-testMaximumBy genmin f ls = S.fold (FL.maximumBy f) (S.fromList ls) `shouldReturn` foldMaybe (greater f) genmin ls
+maximumBy :: (Ord a, Show a) => a -> (a -> a -> Ordering) -> [a] -> Expectation
+maximumBy genmin f ls = S.fold (FL.maximumBy f) (S.fromList ls) `shouldReturn` foldMaybe (greater f) genmin ls
 
 
-testMaximum :: (Show a, Ord a) => a -> [a] -> Expectation
-testMaximum genmin ls = S.fold FL.maximum (S.fromList ls) `shouldReturn` foldMaybe (greater compare) genmin ls
+maximum :: (Show a, Ord a) => a -> [a] -> Expectation
+maximum genmin ls = S.fold FL.maximum (S.fromList ls) `shouldReturn` foldMaybe (greater compare) genmin ls
 
 
-testMinimumBy :: (Ord a, Show a) => a -> (a -> a -> Ordering) -> [a] -> Expectation
-testMinimumBy genmax f ls = S.fold (FL.minimumBy f) (S.fromList ls) `shouldReturn` foldMaybe (lesser f) genmax ls
+minimumBy :: (Ord a, Show a) => a -> (a -> a -> Ordering) -> [a] -> Expectation
+minimumBy genmax f ls = S.fold (FL.minimumBy f) (S.fromList ls) `shouldReturn` foldMaybe (lesser f) genmax ls
 
 
-testMinimum :: (Show a, Ord a) => a -> [a] -> Expectation
-testMinimum genmax ls = S.fold FL.minimum (S.fromList ls) `shouldReturn` foldMaybe (lesser compare) genmax ls
+minimum :: (Show a, Ord a) => a -> [a] -> Expectation
+minimum genmax ls = S.fold FL.minimum (S.fromList ls) `shouldReturn` foldMaybe (lesser compare) genmax ls
 
 
-testToList :: [Int] -> Expectation
-testToList ls = S.fold FL.toList (S.fromList ls) `shouldReturn` ls
+toList :: [Int] -> Expectation
+toList ls = S.fold FL.toList (S.fromList ls) `shouldReturn` ls
 
 
 safeLast :: [a] -> Maybe a
@@ -91,8 +93,8 @@ safeLast (x:[]) = Just x
 safeLast (_:xs) = safeLast xs
 
 
-testLast :: [String] -> Expectation
-testLast ls = S.fold FL.last (S.fromList ls) `shouldReturn` safeLast ls
+last :: [String] -> Expectation
+last ls = S.fold FL.last (S.fromList ls) `shouldReturn` safeLast ls
 
 
 nth :: Int -> [a] -> Maybe a
@@ -104,102 +106,110 @@ nth idx (x:xs) = if idx == 0
 nth _ [] = Nothing
 
 
-testIndex :: Int -> [String] -> Expectation
-testIndex idx ls = let
-                     x = S.fold (FL.index idx) (S.fromList ls)
-                   in
-                     x `shouldReturn` (nth idx ls)
+index :: Int -> [String] -> Expectation
+index idx ls = let x = S.fold (FL.index idx) (S.fromList ls)
+                in x `shouldReturn` (nth idx ls)
 
 
-testFind :: (Show a, Eq a) => (a -> Bool) -> [a] -> Expectation
-testFind f ls = do
-                   let x = S.fold (FL.findIndex f) (S.fromList ls)
-                   y <- x
-                   case y of
-                        Nothing  -> S.fold (FL.find f) (S.fromList ls) `shouldReturn` Nothing
-                        Just idx -> S.fold (FL.any f) (S.fromList $ take idx ls) `shouldReturn` False
+find :: (Show a, Eq a) => (a -> Bool) -> [a] -> Expectation
+find f ls = do
+   let x = S.fold (FL.findIndex f) (S.fromList ls)
+   y <- x
+   case y of
+      Nothing  -> S.fold (FL.find f) (S.fromList ls) `shouldReturn` Nothing
+      Just idx -> S.fold (FL.any f) (S.fromList $ take idx ls) `shouldReturn` False
 
 
 neg :: (a -> Bool) -> a -> Bool
 neg f x = if f x == True then False else True
 
-testFindIndex :: (a -> Bool) -> [a] -> Expectation
-testFindIndex f ls = do
-                        let x = S.fold (FL.findIndex f) (S.fromList ls)
-                        y <- x
-                        case y of
-                              Nothing  -> S.fold (FL.all $ neg f) (S.fromList ls) `shouldReturn` True
-                              Just idx -> if idx == 0
-                                          then S.fold (FL.all f) (S.fromList []) `shouldReturn` True
-                                          else S.fold (FL.all f) (S.fromList $ (take idx ls)) `shouldReturn` False
+findIndex :: (a -> Bool) -> [a] -> Expectation
+findIndex f ls = do
+   let x = S.fold (FL.findIndex f) (S.fromList ls)
+   y <- x
+   case y of
+      Nothing  -> S.fold (FL.all $ neg f) (S.fromList ls) `shouldReturn` True
+      Just idx -> if idx == 0
+                  then S.fold (FL.all f) (S.fromList []) `shouldReturn` True
+                  else S.fold (FL.all f) (S.fromList $ (take idx ls)) `shouldReturn` False
 
 predicate :: Int -> Bool
 predicate x = if x * x < 100 then True else False
 
-testElemIndex :: Int -> [Int] -> Expectation
-testElemIndex elm ls = do
-                           let x = S.fold (FL.elemIndex elm) (S.fromList ls)
-                           y <- x
-                           case y of
-                                 Nothing  -> S.fold (FL.any (\z -> if z == elm then True else False)) (S.fromList ls) `shouldReturn` False
-                                 Just idx -> S.fold (FL.any (\z -> if z == elm then True else False)) (S.fromList $ (take idx ls)) `shouldReturn` False
+elemIndex :: Int -> [Int] -> Expectation
+elemIndex elm ls = do
+   let x = S.fold (FL.elemIndex elm) (S.fromList ls)
+   y <- x
+   case y of
+      Nothing  -> S.fold (FL.any (\z -> if z == elm
+                                        then True
+                                        else False)) (S.fromList ls) `shouldReturn` False
+      Just idx -> S.fold (FL.any (\z -> if z == elm
+                                        then True
+                                        else False)) (S.fromList $ (take idx ls)) `shouldReturn` False
 
-testNull :: [Int] -> Expectation
-testNull ls = S.fold FL.null (S.fromList ls) `shouldReturn` case ls of
-                                                                  [] -> True
-                                                                  _ -> False
-testElem :: Int -> [Int] -> Expectation
-testElem elm ls = do
-                     let x = S.fold (FL.elem elm) (S.fromList ls)
-                     y <- x
-                     S.fold (FL.any (\z -> if z == elm then True else False)) (S.fromList ls) `shouldReturn` y
-
-
-testNotElem :: Int -> [Int] -> Expectation
-testNotElem elm ls = do
-                        let x = S.fold (FL.notElem elm) (S.fromList ls)
-                        y <- x
-                        S.fold (FL.any (\z -> if z == elm then True else False)) (S.fromList ls) `shouldReturn` (if y == True then False else True)
-
-
-testAll :: (a -> Bool) -> [a] -> Expectation
-testAll f ls = S.fold (FL.all f) (S.fromList ls) `shouldReturn` Prelude.and (map f ls)
+null :: [Int] -> Expectation
+null ls = S.fold FL.null (S.fromList ls) `shouldReturn` case ls of
+                                                            [] -> True
+                                                            _ -> False
+elem :: Int -> [Int] -> Expectation
+elem elm ls = do
+   let x = S.fold (FL.elem elm) (S.fromList ls)
+   y <- x
+   S.fold (FL.any (\z -> if z == elm
+                         then True
+                         else False)) (S.fromList ls) `shouldReturn` y
 
 
-testAny :: (a -> Bool) -> [a] -> Expectation
-testAny f ls = S.fold (FL.any f) (S.fromList ls) `shouldReturn` Prelude.any f ls
+notElem :: Int -> [Int] -> Expectation
+notElem elm ls = do
+   let x = S.fold (FL.notElem elm) (S.fromList ls)
+   y <- x
+   S.fold (FL.any (\z -> if z == elm
+                         then True
+                         else False)) (S.fromList ls) `shouldReturn` (if y == True
+                                                                      then False
+                                                                      else True)
 
 
-testAnd :: [Bool] -> Expectation
-testAnd ls = S.fold FL.and (S.fromList ls) `shouldReturn` Prelude.and ls
+all :: (a -> Bool) -> [a] -> Expectation
+all f ls = S.fold (FL.all f) (S.fromList ls) `shouldReturn` Prelude.and (map f ls)
 
 
-testOr :: [Bool] -> Expectation
-testOr ls = S.fold FL.or (S.fromList ls) `shouldReturn` Prelude.or ls
+any :: (a -> Bool) -> [a] -> Expectation
+any f ls = S.fold (FL.any f) (S.fromList ls) `shouldReturn` Prelude.any f ls
+
+
+and :: [Bool] -> Expectation
+and ls = S.fold FL.and (S.fromList ls) `shouldReturn` Prelude.and ls
+
+
+or :: [Bool] -> Expectation
+or ls = S.fold FL.or (S.fromList ls) `shouldReturn` Prelude.or ls
 
 
 main :: IO ()
 main = hspec $
-    describe "Fold Tests" $ do
-        prop "testRollingHashFirstN" testRollingHashFirstN
-        prop "testIndex" $ testIndex
-        prop "testHead" testHead
-        prop "testLast" testLast
-        prop "testLength" testLength
-        prop "testSum" testSum
-        prop "testProduct" testProduct
-        prop "testMaximumBy" $ testMaximumBy intMin compare
-        prop "testMaximum" $ testMaximum intMin
-        prop "testMinimumBy" $ testMinimumBy intMax compare
-        prop "testMinimum" $ testMinimum intMax
-        prop "testToList" testToList
-        prop "testFind" $ testFind predicate
-        prop "testFindIndex" $ testFindIndex predicate
-        prop "testElemIndex" $ testElemIndex 10
-        prop "testNull" testNull
-        prop "testElem" $ testElem 10
-        prop "testNotElem" $ testNotElem 10
-        prop "testAll" $ testAll predicate
-        prop "testAny" $ testAny predicate
-        prop "testAnd" testAnd
-        prop "testOr" testOr
+    describe "Fold s" $ do
+        prop "RollingHashFirstN" rollingHashFirstN
+        prop "Index" $ index
+        prop "Head" head
+        prop "Last" last
+        prop "Length" Main.length
+        prop "Sum" sum
+        prop "Product" product
+        prop "MaximumBy" $ maximumBy intMin compare
+        prop "Maximum" $ maximum intMin
+        prop "MinimumBy" $ minimumBy intMax compare
+        prop "Minimum" $ minimum intMax
+        prop "ToList" toList
+        prop "Find" $ find predicate
+        prop "FindIndex" $ findIndex predicate
+        prop "ElemIndex" $ elemIndex 10
+        prop "Null" null
+        prop "Elem" $ elem 10
+        prop "NotElem" $ notElem 10
+        prop "All" $ Main.all predicate
+        prop "Any" $ Main.any predicate
+        prop "And" Main.and
+        prop "Or" Main.or
