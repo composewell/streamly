aa1d7a5c401e52132b2a5ab2c1cfc432bc43de4c
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-06-08 17:49:24 +0530

    split functions into pass case and either (pass or fail) case

--- test/Streamly/Test/Internal/Data/Parser.hs
+++ test/Streamly/Test/Internal/Data/Parser.hs
@@ -8,7 +8,7 @@ import qualified Streamly.Internal.Data.Fold as FL
 
 import Test.Hspec(hspec, describe)
 import Test.Hspec.QuickCheck
-import Test.QuickCheck (forAll, chooseInt, Property, property, listOf, vectorOf, (.&&.))
+import Test.QuickCheck (forAll, chooseInt, Property, property, listOf, vectorOf)
 
 min_value :: Int
 min_value = 0
@@ -91,17 +91,19 @@ peekFail =
         Right _ -> False
         Left _ -> True)
 
-eof :: Property
-eof = 
+eofPass :: Property
+eofPass = 
+    property (case S.parse P.eof (S.fromList []) of
+        Right _ -> True
+        Left _ -> False)
+
+eofFail :: Property
+eofFail = 
     forAll (chooseInt (1, max_length)) $ \list_length ->
         forAll (vectorOf list_length (chooseInt (min_value, max_value))) $ \ls ->
             case S.parse P.eof (S.fromList ls) of
                 Right _ -> False
                 Left _ -> True
-    .&&.
-    property (case S.parse P.eof (S.fromList []) of
-        Right _ -> True
-        Left _ -> False)
 
 satisfy :: Property
 satisfy = 
@@ -126,6 +128,15 @@ take =
                 Right parsed_list -> parsed_list == Prelude.take n ls
                 Left _ -> False
 
+takeEQPass :: Property
+takeEQPass = 
+    forAll (chooseInt (min_value, max_value)) $ \n ->
+        forAll (chooseInt (n, max_value)) $ \list_length ->
+            forAll (vectorOf list_length (chooseInt (min_value, max_value))) $ \ls ->
+                case S.parse (P.takeEQ n FL.toList) (S.fromList ls) of
+                    Right parsed_list -> parsed_list == Prelude.take n ls
+                    Left _ -> False
+
 takeEQ :: Property
 takeEQ =
     forAll (chooseInt (min_value, max_value)) $ \n ->
@@ -137,6 +148,15 @@ takeEQ =
                     Right parsed_list -> (n <= list_length) && (parsed_list == Prelude.take n ls)
                     Left _ -> n > list_length
 
+takeGEPass :: Property
+takeGEPass = 
+    forAll (chooseInt (min_value, max_value)) $ \n ->
+        forAll (chooseInt (n, max_value)) $ \list_length ->
+            forAll (vectorOf list_length (chooseInt (min_value, max_value))) $ \ls ->
+                case S.parse (P.takeGE n FL.toList) (S.fromList ls) of
+                    Right parsed_list -> parsed_list == ls
+                    Left _ -> False
+
 takeGE :: Property
 takeGE =
     forAll (chooseInt (min_value, max_value)) $ \n ->
@@ -147,6 +167,22 @@ takeGE =
                 case S.parse (P.takeGE n FL.toList) (S.fromList ls) of
                     Right parsed_list -> (n <= list_length) && (parsed_list == ls)
                     Left _ -> n > list_length
+
+lookAheadPass :: Property
+lookAheadPass =
+    forAll (chooseInt (min_value + 1, max_value)) $ \n -> 
+        let
+            takeWithoutConsume = P.lookAhead $ P.take n FL.toList
+            parseTwice = do
+                parsed_list_1 <- takeWithoutConsume
+                parsed_list_2 <- takeWithoutConsume
+                return (parsed_list_1, parsed_list_2)
+        in
+            forAll (chooseInt (n, max_value)) $ \list_length ->
+                forAll (vectorOf list_length (chooseInt (min_value, max_value))) $ \ls ->
+                    case S.parse parseTwice (S.fromList ls) of
+                        Right (ls_1, ls_2) -> (ls_1 == ls_2) && (ls_1 == Prelude.take n ls)
+                        Left _ -> False
 
 lookAhead :: Property
 lookAhead =
@@ -217,16 +253,20 @@ sliceSepBy =
 --                 0 : 1 : _ -> False
 --                 _ -> True
 
--- splitWithFail :: Property
--- splitWithFail =
+-- splitWithFailLeft :: Property
+-- splitWithFailLeft =
 --     property (case S.parse (P.splitWith (,) (P.die "die") (P.yield (1 :: Int))) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
---     .&&.
+
+-- splitWithFailRight :: Property
+-- splitWithFailRight =
 --     property (case S.parse (P.splitWith (,) (P.yield (1 :: Int)) (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
---     .&&.
+
+-- splitWithFailBoth :: Property
+-- splitWithFailBoth =
 --     property (case S.parse (P.splitWith (,) (P.die "die") (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
@@ -242,16 +282,20 @@ sliceSepBy =
 --                     Right (ls_1, ls_2) -> (Prelude.take n ls == ls_1) && (ls_1 == ls_2)
 --                     Left _ -> False
 
--- teeWithFail :: Property
--- teeWithFail = 
+-- teeWithFailLeft :: Property
+-- teeWithFailLeft = 
 --     property (case S.parse (P.teeWith (,) (P.die "die") (P.yield (1 :: Int))) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
---     .&&.
+
+-- teeWithFailRight :: Property
+-- teeWithFailRight = 
 --     property (case S.parse (P.teeWith (,) (P.yield (1 :: Int)) (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
---     .&&.
+
+-- teeWithFailBoth :: Property
+-- teeWithFailBoth = 
 --     property (case S.parse (P.teeWith (,) (P.die "die") (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
@@ -280,16 +324,20 @@ sliceSepBy =
 --                 Right short_list -> short_list == Prelude.takeWhile (<= 2500) ls
 --                 Left _ -> False
 
--- shortestFail :: Property
--- shortestFail =
+-- shortestFailLeft :: Property
+-- shortestFailLeft =
 --     property (case S.parse (P.shortest (P.die "die") (P.yield (1 :: Int))) (S.fromList [1 :: Int]) of
 --         Right r -> r == 1
 --         Left _ -> False)
---     .&&.
+
+-- shortestFailRight :: Property
+-- shortestFailRight =
 --     property (case S.parse (P.shortest (P.yield (1 :: Int)) (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right r -> r == 1
 --         Left _ -> False)
---     .&&.
+
+-- shortestFailBoth :: Property
+-- shortestFailBoth =
 --     property (case S.parse (P.shortest (P.die "die") (P.die "die")) (S.fromList [1 :: Int]) of
 --         Right _ -> False
 --         Left _ -> True)
@@ -306,24 +354,34 @@ main = hspec $ do
         prop "test dieM function" dieM
     
     describe "test for element parser" $ do
-        prop "test for peek function" peekPass
-        prop "test for peek function" peekFail
-        prop "test for eof function" eof
+        prop "pass test for peek function" peekPass
+        prop "fail test for peek function" peekFail
+        prop "pass test for eof function" eofPass
+        prop "fail test for eof function" eofFail
         prop "test for satisfy function" satisfy
 
     describe "test for sequence parser" $ do
         prop "test for take function" Main.take
+        prop "pass test for takeEq function" takeEQPass
         prop "test for takeEq function" Main.takeEQ
+        prop "pass test for takeGE function" takeGEPass
         prop "test for takeGE function" Main.takeGE
+        prop "pass test for LookAhead function" lookAheadPass
         prop "test for LookAhead function" lookAhead
         prop "test for takeWhile function" Main.takeWhile
         prop "test for takeWhile1 function" takeWhile1
         prop "test for sliceSepBy function" sliceSepBy
         -- prop "test for sliceSepByMax function" sliceSepByMax
-        -- prop "test for splitWithPass function" splitWithPass
-        -- prop "test for splitWithFail function" splitWithFail
-        -- prop "test for teeWithPass function" teeWithPass
-        -- prop "test for teeWithFail function" teeWithFail
+        -- prop "pass test for splitWith function" splitWithPass
+        -- prop "left fail test for splitWith function" splitWithFailLeft
+        -- prop "right fail test for splitWith function" splitWithFailRight
+        -- prop "both fail test for splitWith function" splitWithFailBoth
+        -- prop "pass test for teeWith function" teeWithPass
+        -- prop "left fail test for teeWith function" teeWithFailLeft
+        -- prop "right fail test for teeWith function" teeWithFailRight
+        -- prop "both fail test for teeWith function" teeWithFailBoth
         -- prop "test for deintercalate function" deintercalate
-        -- prop "test for shortestPass function" shortestPass
-        -- prop "test for shortestFail function" shortestFail
+        -- prop "pass test for shortest function" shortestPass
+        -- prop "left fail test for shortest function" shortestFailLeft
+        -- prop "right fail test for shortest function" shortestFailRight
+        -- prop "both fail test for shortest function" shortestFailBoth
