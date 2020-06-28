54ca0173f94b3c38a5a5ac22a3f8c80fa31c6b21
pranaysashank <pranaysashank@composewell.com>
2020-06-28 21:33:33 +0530

    Fix take, takeEQ, takeGE, sliceSepByMax.
    - `take`, `takeEQ`, `takeGE` now work when the count is <0.
    - `sliceSepByMax` behaviour is now more in line with what the
      documentaton says in `Streamly.Internal.Data.Parser`.

--- src/Streamly/Internal/Data/Parser/ParserD.hs
+++ src/Streamly/Internal/Data/Parser/ParserD.hs
@@ -328,13 +328,15 @@ take n (Fold fstep finitial fextract) = Parser step initial extract
 
     initial = Tuple' 0 <$> finitial
 
-    step (Tuple' i r) a = do
-        res <- fstep r a
-        let i1 = i + 1
-            s1 = Tuple' i1 res
-        if i1 < n
-        then return $ Partial 0 s1
-        else Done 0 <$> fextract res
+    step (Tuple' i r) a
+        | i < n = do
+            res <- fstep r a
+            let i1 = i + 1
+                s1 = Tuple' i1 res
+            if i1 < n
+            then return $ Partial 0 s1
+            else Done 0 <$> fextract res
+        | otherwise = Done 1 <$> fextract r
 
     extract (Tuple' _ r) = fextract r
 
@@ -350,11 +352,15 @@ takeEQ n (Fold fstep finitial fextract) = Parser step initial extract
 
     initial = Tuple' 0 <$> finitial
 
-    step (Tuple' i r) a = do
-        res <- fstep r a
-        let i1 = i + 1
-            s1 = Tuple' i1 res
-        if i1 < n then return (Continue 0 s1) else Done 0 <$> fextract res
+    step (Tuple' i r) a
+      | i < n = do
+          res <- fstep r a
+          let i1 = i + 1
+              s1 = Tuple' i1 res
+          if i1 < n
+          then return (Continue 0 s1)
+          else Done 0 <$> fextract res
+      | otherwise = Done 1 <$> fextract r
 
     extract (Tuple' i r) =
         if n == i
@@ -373,9 +379,10 @@ takeEQ n (Fold fstep finitial fextract) = Parser step initial extract
 --
 {-# INLINE takeGE #-}
 takeGE :: MonadThrow m => Int -> Fold m a b -> Parser m a b
-takeGE n (Fold fstep finitial fextract) = Parser step initial extract
-
+takeGE cnt (Fold fstep finitial fextract) = Parser step initial extract
     where
+
+    n = max cnt 0
 
     initial = Tuple' 0 <$> finitial
 
@@ -501,17 +508,18 @@ sliceSepByMax predicate cnt (Fold fstep finitial fextract) =
     where
 
     initial = Tuple' 0 <$> finitial
+
     step (Tuple' i r) a
-        | not (predicate a) = do
-            res <- fstep r a
-            let i1 = i + 1
-                s1 = Tuple' i1 res
-            if i1 < cnt
-            then return $ Partial 0 s1
-            else do
-                b <- fextract res
-                return $ Done 0 b
+        | not (predicate a) =
+            if i < cnt
+            then do
+                res <- fstep r a
+                let i1 = i + 1
+                    s1 = Tuple' i1 res
+                return $ Partial 0 s1
+            else Done 1 <$> fextract r
         | otherwise = Done 0 <$> fextract r
+
     extract (Tuple' _ r) = fextract r
 
 -- | See 'Streamly.Internal.Data.Parser.wordBy'.
