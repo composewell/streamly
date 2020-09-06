dd58d37daf675d414b3a4497f0254ee5c3032b3f
Ranjeet Ranjan <rnjt.ranjan@gmail.com>
2020-09-06 22:44:39 +0530

    Fixed file handle close and rename module

--- test/Streamly/Test/FileSystem/Event.hs
+++ test/Streamly/Test/FileSystem/Event.hs
@@ -11,16 +11,24 @@
 {-# LANGUAGE CPP #-}
 module Main (main) where
 
-import Control.Monad.IO.Class (MonadIO)
+
 import Data.Function ((&))
-import Data.List.NonEmpty (NonEmpty)
-import Data.Word (Word8)
-import Streamly.Internal.Data.Array.Storable.Foreign (Array)
+
 import System.Environment (getArgs)
 import Streamly (SerialT)
 
+#if defined(CABAL_OS_WINDOWS)
+#else    
+import Control.Monad.IO.Class (MonadIO)    
 import qualified Data.List.NonEmpty as NonEmpty
+import Data.List.NonEmpty (NonEmpty)
 import qualified Streamly.Unicode.Stream as Unicode
+import qualified Streamly.Internal.Data.Array.Storable.Foreign as Array
+import Data.Word (Word8)
+import Streamly.Internal.Data.Array.Storable.Foreign (Array)
+#endif
+
+
 import qualified Streamly.Internal.Data.Stream.IsStream as Stream
 #if defined(CABAL_OS_DARWIN)
 import qualified Streamly.Internal.FileSystem.Event.Darwin as Event
@@ -29,25 +37,22 @@ import qualified Streamly.Internal.FileSystem.Event.Linux as Event
 #elif defined(CABAL_OS_WINDOWS)
 import qualified Streamly.Internal.FileSystem.Event.Windows as Event
 #endif
-import qualified Streamly.Internal.Data.Array.Storable.Foreign as Array
 
 -------------------------------------------------------------------------------
 -- Utilities
 -------------------------------------------------------------------------------
-
+#if defined(CABAL_OS_WINDOWS)
+#else
 toUtf8 :: MonadIO m => String -> m (Array Word8)
 toUtf8 = Array.fromStream . Unicode.encodeUtf8 . Stream.fromList
-
+#endif
 -------------------------------------------------------------------------------
 -- Main
 -------------------------------------------------------------------------------
 #if defined(CABAL_OS_WINDOWS)
 watchPaths :: [FilePath] -> SerialT IO Event.Event
 watchPaths = Event.watchTrees
-#elif defined(CABAL_OS_DARWIN)
-watchPaths :: NonEmpty (Array Word8) -> SerialT IO Event.Event    
-watchPaths = Event.watchTrees
-#elif defined(CABAL_OS_LINUX)
+#else
 watchPaths :: NonEmpty (Array Word8) -> SerialT IO Event.Event    
 watchPaths = Event.watchPaths
 #endif
