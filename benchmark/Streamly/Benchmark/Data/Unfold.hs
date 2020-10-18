54240bf7b6e54e9165e5894a0ba69211c9c913ed
adithyaov <adithya@composewell.com>
2020-10-19 04:22:49 +0530

    Use a cleaner way to benchmark unfolds

--- benchmark/Streamly/Benchmark/Data/Unfold.hs
+++ benchmark/Streamly/Benchmark/Data/Unfold.hs
@@ -11,10 +11,11 @@ module Main (main) where
 
 import Control.DeepSeq (NFData(..))
 import System.Random (randomRIO)
+import Streamly.Internal.Data.Unfold (Unfold)
 
 import qualified Prelude
+import qualified Streamly.Internal.Data.Fold as FL
 import qualified Streamly.Internal.Data.Unfold as UF
-import qualified Streamly.Internal.Data.Stream.IsStream as S
 
 import Streamly.Benchmark.Common
 import Streamly.Benchmark.Data.NestedUnfoldOps
@@ -26,32 +27,77 @@ benchIO :: (NFData b) => String -> (Int -> IO b) -> Benchmark
 benchIO name f = bench name $ nfIO $ randomRIO (1,1) >>= f
 
 -------------------------------------------------------------------------------
+-- Benchmark helpers
+-------------------------------------------------------------------------------
+
+{-# INLINE drainUnfold #-}
+drainUnfold :: Monad m => Unfold m a b -> a -> m ()
+drainUnfold unf seed = UF.fold unf FL.drain seed
+
+{-# INLINE drainGeneration #-}
+drainGeneration :: Monad m => Unfold m a b -> a -> m ()
+drainGeneration = drainUnfold
+
+{-# INLINE drainTransformation #-}
+drainTransformation ::
+       Monad m => Unfold m a b -> (Unfold m a b -> Unfold m c d) -> c -> m ()
+drainTransformation unf f seed = drainUnfold (f unf) seed
+
+{-# INLINE drainTransformationDefault #-}
+drainTransformationDefault ::
+       Monad m => Int -> (Unfold m Int Int -> Unfold m c d) -> c -> m ()
+drainTransformationDefault size =
+    drainTransformation (UF.take size UF.enumerateFromIntegral)
+
+{-# INLINE drainProduct #-}
+drainProduct ::
+       Monad m
+    => Unfold m a b
+    -> Unfold m c d
+    -> (Unfold m a b -> Unfold m c d -> Unfold m e f)
+    -> e
+    -> m ()
+drainProduct unf1 unf2 f seed = drainUnfold (f unf1 unf2) seed
+
+{-# INLINE drainProductDefault #-}
+drainProductDefault ::
+       Monad m
+    => Int
+    -> (Unfold m Int Int -> Unfold m Int Int -> Unfold m e f)
+    -> e
+    -> m ()
+drainProductDefault size = drainProduct src src
+
+    where
+
+    src = UF.take size UF.enumerateFromIntegral
+
+-------------------------------------------------------------------------------
 -- Stream generation
 -------------------------------------------------------------------------------
 
 {-# INLINE fromListM #-}
 fromListM :: Monad m => Int -> Int -> m ()
 fromListM size start =
-    S.drain
-         $ S.unfold UF.fromListM (Prelude.map return [start .. start + size])
+    drainGeneration UF.fromListM (Prelude.map return [start .. start + size])
 
 {-# INLINE replicateM #-}
 replicateM :: Monad m => Int -> Int -> m ()
-replicateM size start = S.drain $ S.unfold (UF.replicateM size) (return start)
+replicateM size start = drainGeneration (UF.replicateM size) (return start)
 
 {-# INLINE repeatM #-}
 repeatM :: Monad m => Int -> Int -> m ()
-repeatM size start = S.drain $ S.take size $ S.unfold UF.repeatM (return start)
+repeatM size start = drainGeneration (UF.take size UF.repeatM) (return start)
 
 {-# INLINE enumerateFromStepIntegral #-}
 enumerateFromStepIntegral :: Monad m => Int -> Int -> m ()
-enumerateFromStepIntegral size start = S.drain $
-    S.take size $ S.unfold UF.enumerateFromStepIntegral (start, 1)
+enumerateFromStepIntegral size start =
+    drainGeneration (UF.take size UF.enumerateFromStepIntegral) (start, 1)
 
 {-# INLINE enumerateFromToIntegral #-}
 enumerateFromToIntegral :: Monad m => Int -> Int -> m ()
 enumerateFromToIntegral size start =
-    S.drain $ S.unfold (UF.enumerateFromToIntegral (size + start)) start
+    drainGeneration (UF.enumerateFromToIntegral (size + start)) start
 
 -------------------------------------------------------------------------------
 -- Stream transformation
@@ -59,55 +105,41 @@ enumerateFromToIntegral size start =
 
 {-# INLINE take #-}
 take :: Monad m => Int -> Int -> m ()
-take size start =
-    S.drain
-        $ S.unfold
-              (UF.take size (UF.enumerateFromToIntegral (size + start)))
-              start
+take size start = drainTransformationDefault size (UF.take size) start
 
 {-# INLINE takeWhileM #-}
 takeWhileM :: Monad m => Int -> Int -> m ()
 takeWhileM size start =
-    S.drain
-        $ S.unfold
-            (UF.takeWhileM
-                 (\b -> return (b <= size + start))
-                 (UF.enumerateFromToIntegral (size + start)))
-            start
+    drainTransformationDefault
+        size
+        (UF.takeWhileM (\b -> return (b <= size + start)))
+        start
 
 {-# INLINE _dropOne #-}
 _dropOne :: Monad m => Int -> Int -> m ()
 _dropOne size start =
-    S.drain
-         $ S.unfold (UF.drop 1 (UF.enumerateFromToIntegral (size + start))) start
+    drainTransformationDefault size (UF.drop 1) start
 
 {-# INLINE dropAll #-}
 dropAll :: Monad m => Int -> Int -> m ()
 dropAll size start =
-    S.drain
-        $ S.unfold
-            (UF.drop (size + 1) (UF.enumerateFromToIntegral (size + start)))
-            start
+    drainTransformationDefault size (UF.drop (size + 1)) start
 
 {-# INLINE dropWhileMTrue #-}
 dropWhileMTrue :: Monad m => Int -> Int -> m ()
 dropWhileMTrue size start =
-    S.drain
-        $ S.unfold
-            (UF.dropWhileM
-                 (\_ -> return True)
-                 (UF.enumerateFromToIntegral (size + start)))
-            start
+    drainTransformationDefault
+        size
+        (UF.dropWhileM (\_ -> return True))
+        start
 
 {-# INLINE dropWhileMFalse #-}
 dropWhileMFalse :: Monad m => Int -> Int -> m ()
 dropWhileMFalse size start =
-    S.drain
-        $ S.unfold
-            (UF.dropWhileM
-                 (\_ -> return False)
-                 (UF.enumerateFromToIntegral (size + start)))
-            start
+    drainTransformationDefault
+        size
+        (UF.dropWhileM (\_ -> return False))
+        start
 
 -------------------------------------------------------------------------------
 -- Stream combination
@@ -116,13 +148,10 @@ dropWhileMFalse size start =
 {-# INLINE zipWithM #-}
 zipWithM :: Monad m => Int -> Int -> m ()
 zipWithM size start =
-    S.drain
-        $ S.unfold
-            (UF.zipWithM
-                 (\a b -> return $ a + b)
-                 (UF.enumerateFromToIntegral (size + start))
-                 (UF.enumerateFromToIntegral (size + start + 1)))
-            (start, start + 1)
+    drainProductDefault
+        size
+        (UF.zipWithM (\a b -> return $ a + b))
+        (start, start + 1)
 
 -------------------------------------------------------------------------------
 -- Benchmarks
