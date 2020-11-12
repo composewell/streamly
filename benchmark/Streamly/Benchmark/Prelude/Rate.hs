4b9920a5fd1d2fb2f38d6d85a28f8ab47cba7824
adithyaov <adithya@composewell.com>
2020-11-12 09:57:04 +0530

    Clean bench:Prelude.Rate
    Abstract over avgRate and change label values in bench groups

--- benchmark/Streamly/Benchmark/Prelude/Rate.hs
+++ benchmark/Streamly/Benchmark/Prelude/Rate.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 -- |
 -- Module      : Main
 -- Copyright   : (c) 2018 Composewell Technologies
@@ -5,7 +7,7 @@
 -- License     : BSD3
 -- Maintainer  : streamly@composewell.com
 
-import Streamly.Prelude (asyncly, aheadly, maxThreads)
+import Streamly.Prelude (asyncly, aheadly, maxThreads, IsStream, MonadAsync)
 import qualified Streamly.Prelude as S
 
 import Streamly.Benchmark.Common
@@ -20,6 +22,19 @@ moduleName = "Prelude.Rate"
 -- Average Rate
 -------------------------------------------------------------------------------
 
+{-# INLINE rateNothing #-}
+rateNothing :: (MonadAsync m, IsStream t) => Int -> Int -> t m Int
+rateNothing value = S.rate Nothing . sourceUnfoldrM value
+
+{-# INLINE avgRate #-}
+avgRate :: (MonadAsync m, IsStream t) => Int -> Double -> Int -> t m Int
+avgRate value rate_ = S.avgRate rate_ . sourceUnfoldrM value
+
+{-# INLINE avgRateThreads1 #-}
+avgRateThreads1 :: (MonadAsync m, IsStream t) => Int -> Double -> Int -> t m Int
+avgRateThreads1 value rate_ =
+    maxThreads 1 . S.avgRate rate_ . sourceUnfoldrM value
+
 -- XXX arbitrarily large rate should be the same as rate Nothing
 o_1_space_async_avgRate :: Int -> [Benchmark]
 o_1_space_async_avgRate value =
@@ -27,18 +42,13 @@ o_1_space_async_avgRate value =
         [ bgroup "avgRate"
             -- benchIO "unfoldr" $ toNull asyncly
             -- benchIOSrc asyncly "unfoldrM" (sourceUnfoldrM value)
-            [ benchIOSrc asyncly "unfoldrM/Nothing"
-                (S.rate Nothing . sourceUnfoldrM value)
-            , benchIOSrc asyncly "unfoldrM/1,000,000"
-                (S.avgRate 1000000 . sourceUnfoldrM value)
-            , benchIOSrc asyncly "unfoldrM/3,000,000"
-                (S.avgRate 3000000 . sourceUnfoldrM value)
-            , benchIOSrc asyncly "unfoldrM/10,000,000/maxThreads1"
-                (maxThreads 1 .  S.avgRate 10000000 . sourceUnfoldrM value)
-            , benchIOSrc asyncly "unfoldrM/10,000,000"
-                (S.avgRate 10000000 . sourceUnfoldrM value)
-            , benchIOSrc asyncly "unfoldrM/20,000,000"
-                (S.avgRate 20000000 . sourceUnfoldrM value)
+            [ benchIOSrc asyncly "Nothing" $ rateNothing value
+            , benchIOSrc asyncly "1M" $ avgRate value 1000000
+            , benchIOSrc asyncly "3M" $ avgRate value 3000000
+            , benchIOSrc asyncly "10M/maxThreads1"
+                  $ avgRateThreads1 value 10000000
+            , benchIOSrc asyncly "10M" $ avgRate value 10000000
+            , benchIOSrc asyncly "20M" $ avgRate value 20000000
             ]
         ]
     ]
@@ -47,8 +57,7 @@ o_1_space_ahead_avgRate :: Int -> [Benchmark]
 o_1_space_ahead_avgRate value =
     [ bgroup "aheadly"
         [ bgroup "avgRate"
-            [ benchIOSrc aheadly "unfoldrM/1,000,000"
-                  (S.avgRate 1000000 . sourceUnfoldrM value)
+            [ benchIOSrc aheadly "1M" $ avgRate value 1000000
             ]
         ]
     ]
