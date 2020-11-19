72ab7654856d70a8eca0a92d58ce96128f25ea18
Harendra Kumar <harendra@composewell.com>
2020-11-19 15:02:44 +0000

    Add scripts to generate hscope db and tags

---
+++ bin/mk-hscope.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+GHC_VERSION=8.8.3
+STREAMLY_VERSION=0.7.2
+
+case `uname` in
+  Darwin) SYSTEM=x86_64-osx;;
+  Linux) SYSTEM=x86_64-linux;;
+  *) echo "Unsupported system"; exit 1 ;;
+esac
+
+pushd $(git rev-parse --show-toplevel)
+git ls-files \
+  | grep '\.l\?hs$' \
+  | xargs hscope -b \
+    -I src/ \
+    -I test \
+    -I dist-newstyle/build/$SYSTEM/ghc-$GHC_VERSION/streamly-$STREAMLY_VERSION/build/src/ \
+    -X BangPatterns \
+    -X CApiFFI \
+    -X CPP \
+    -X ConstraintKinds \
+    -X DeriveDataTypeable \
+    -X DeriveGeneric \
+    -X DeriveTraversable \
+    -X ExistentialQuantification \
+    -X FlexibleContexts \
+    -X FlexibleInstances \
+    -X GeneralizedNewtypeDeriving \
+    -X InstanceSigs \
+    -X KindSignatures \
+    -X LambdaCase \
+    -X MagicHash \
+    -X MultiParamTypeClasses \
+    -X PatternSynonyms \
+    -X RankNTypes \
+    -X RecordWildCards \
+    -X ScopedTypeVariables \
+    -X TupleSections \
+    -X TypeFamilies \
+    -X ViewPatterns \
+    -X NoMonoLocalBinds \
+    -X TemplateHaskell \
+    -X UnboxedTuples \
+    -X UndecidableInstances
+popd
