0fd1eb1aec64193ab9fd095201990c3a791b84b2
Harendra Kumar <harendra@composewell.com>
2021-01-16 19:58:25 +0530

    Move Stream/Enumeration under Stream/IsStream

--- src/Streamly/Internal/Data/Stream/IsStream/Generate.hs
+++ src/Streamly/Internal/Data/Stream/IsStream/Generate.hs
@@ -91,7 +91,7 @@ import Control.Monad.IO.Class (MonadIO(..))
 import Data.Void (Void)
 import Streamly.Internal.Data.Unfold.Types (Unfold)
 import Streamly.Internal.Data.SVar (MonadAsync, Rate (..))
-import Streamly.Internal.Data.Stream.Enumeration
+import Streamly.Internal.Data.Stream.IsStream.Enumeration
     (Enumerable(..), enumerate, enumerateTo)
 import Streamly.Internal.Data.Stream.IsStream.Common
     (absTimesWith, concatM, relTimesWith, timesWith, yield, yieldM, repeatM)
