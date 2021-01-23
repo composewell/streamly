dc63298384c773a5e63fa9364ee5b4be5af0dfa3
Harendra Kumar <harendra@composewell.com>
2021-01-24 02:25:40 +0530

    Print more information in failing tests

--- test/lib/Streamly/Test/Common.hs
+++ test/lib/Streamly/Test/Common.hs
@@ -42,6 +42,8 @@ listEquals eq stream list = do
     when (not $ stream `eq` list) $ liftIO $ putStrLn $
                   "stream " <> show stream
              <> "\nlist   " <> show list
+             <> "\nstream length  " <> show (length stream)
+             <> "\nlist length  " <> show (length list)
              <> "\nstream \\\\ list " <> show (stream \\ list)
              <> "\nlist \\\\ stream " <> show (list \\ stream)
     when (not $ stream `eq` list) $
@@ -49,6 +51,8 @@ listEquals eq stream list = do
             (counterexample $
                   "stream " <> show stream
              <> "\nlist   " <> show list
+             <> "\nstream length  " <> show (length stream)
+             <> "\nlist length  " <> show (length list)
              <> "\nstream \\\\ list " <> show (stream \\ list)
              <> "\nlist \\\\ stream " <> show (list \\ stream)
              )
