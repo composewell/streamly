1ddcfc46345cd4e4c3115a955668a6d5158a64cd
Adithya Kumar <adithya@composewell.com>
2021-04-02 04:38:51 +0530

    Fix zipWith(M) to work concurrently according to the stream type
    https://github.com/composewell/streamly/issues/158

--- src/Streamly/Internal/Data/Stream/StreamK.hs
+++ src/Streamly/Internal/Data/Stream/StreamK.hs
@@ -995,34 +995,40 @@ mapMaybe f m = go m
 -- Serial Zipping
 ------------------------------------------------------------------------------
 
--- | Zip two streams serially using a pure zipping function.
+-- XXX We can probably implement zipWith in terms of zipWithM
+-- | Zip two streams serially using a pure zipping function. The zipping
+-- function is applied concurrently for concurrent streams.
 --
 -- @since 0.1.0
 {-# INLINABLE zipWith #-}
-zipWith :: IsStream t => (a -> b -> c) -> t m a -> t m b -> t m c
+zipWith ::
+       (IsStream t, MonadAsync m) => (a -> b -> c) -> t m a -> t m b -> t m c
 zipWith f = go
     where
     go mx my = mkStream $ \st yld sng stp -> do
         let merge a ra =
                 let single2 b = sng (f a b)
-                    yield2 b rb = yld (f a b) (go ra rb)
+                    runIt x = foldStream st yld sng stp x
+                    yield2 b rb = runIt (return (f a b) `consM` go ra rb)
                  in foldStream (adaptState st) yield2 single2 stp my
         let single1 a = merge a nil
             yield1 = merge
         foldStream (adaptState st) yield1 single1 stp mx
 
--- | Zip two streams serially using a monadic zipping function.
+-- | Zip two streams serially using a monadic zipping function. The zipping
+-- function is applied concurrently for concurrent streams.
 --
 -- @since 0.1.0
 {-# INLINABLE zipWithM #-}
-zipWithM :: (IsStream t, Monad m) => (a -> b -> m c) -> t m a -> t m b -> t m c
+zipWithM ::
+       (IsStream t, MonadAsync m) => (a -> b -> m c) -> t m a -> t m b -> t m c
 zipWithM f m1 m2 = go m1 m2
     where
     go mx my = mkStream $ \st yld sng stp -> do
         let merge a ra =
                 let runIt x = foldStream st yld sng stp x
                     single2 b   = f a b >>= sng
-                    yield2 b rb = f a b >>= \x -> runIt (x `cons` go ra rb)
+                    yield2 b rb = runIt (f a b `consM` go ra rb)
                  in foldStream (adaptState st) yield2 single2 stp my
         let single1 a = merge a nil
             yield1 = merge
