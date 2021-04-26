3339c2082d9f86d6cb68d3ffdbfc4aa0e5fed892
Harendra Kumar <harendra@composewell.com>
2021-04-27 02:37:14 +0530

    Add fold and parse for array streams
    The test and benchmark code is taken from Adithya's original double
    ended lists based implementation.

--- benchmark/Streamly/Benchmark/Data/Array/Stream/Foreign.hs
+++ benchmark/Streamly/Benchmark/Data/Array/Stream/Foreign.hs
@@ -23,18 +23,26 @@ module Main
     main
   ) where
 
+import Control.DeepSeq (NFData(..))
+import Control.Monad (void)
+import Control.Monad.Catch (MonadThrow)
 import Data.Functor.Identity (runIdentity)
 import Data.Word (Word8)
 import System.IO (Handle)
+import System.Random (randomRIO)
 import Prelude hiding ()
 
 import qualified Streamly.Prelude  as Stream
 import qualified Streamly.Internal.Data.Array.Foreign as Array
 import qualified Streamly.Internal.Data.Array.Stream.Foreign as ArrayStream
+import qualified Streamly.Internal.Data.Fold as Fold
+import qualified Streamly.Internal.Data.Parser.ParserD as ParserD
+import qualified Streamly.Internal.Data.Stream.IsStream as Stream (arraysOf)
 import qualified Streamly.Internal.FileSystem.Handle as Handle
 import qualified Streamly.Internal.Unicode.Stream as Unicode
 
 import Gauge hiding (env)
+import Streamly.Prelude (SerialT, MonadAsync, IsStream)
 import Streamly.Benchmark.Common
 import Streamly.Benchmark.Common.Handle
 
@@ -43,6 +51,28 @@ import Foreign.Storable (Storable)
 import Streamly.Internal.Data.Stream.StreamD.Type (Step(..))
 import Test.Inspection
 #endif
+
+-------------------------------------------------------------------------------
+-- Utilities
+-------------------------------------------------------------------------------
+
+-- XXX these can be moved to the common module
+
+{-# INLINE sourceUnfoldrM #-}
+sourceUnfoldrM :: (IsStream t, MonadAsync m) => Int -> Int -> t m Int
+sourceUnfoldrM value n = Stream.unfoldrM step n
+    where
+    step cnt =
+        if cnt > n + value
+        then return Nothing
+        else return (Just (cnt, cnt + 1))
+
+{-# INLINE benchIO #-}
+benchIO
+    :: NFData b
+    => String -> (Int -> t IO a) -> (t IO a -> IO b) -> Benchmark
+benchIO name src sink =
+    bench name $ nfIO $ randomRIO (1,1) >>= sink . src
 
 -------------------------------------------------------------------------------
 -- read chunked using toChunks
@@ -184,6 +214,37 @@ o_1_space_copy_toChunks_group_ungroup env =
     ]
 
 -------------------------------------------------------------------------------
+-- Parsers
+-------------------------------------------------------------------------------
+
+{-# INLINE drainWhile #-}
+drainWhile :: MonadThrow m => (a -> Bool) -> ParserD.Parser m a ()
+drainWhile p = ParserD.takeWhile p Fold.drain
+
+-------------------------------------------------------------------------------
+-- Folds and parsers
+-------------------------------------------------------------------------------
+
+{-# INLINE fold #-}
+fold :: SerialT IO (Array.Array Int) -> IO ()
+fold s = void $ ArrayStream.fold Fold.drain s
+
+{-# INLINE parseArray #-}
+parseArray :: Int -> SerialT IO (Array.Array Int) -> IO ()
+parseArray value s = void $ ArrayStream.parse (drainWhile (< value)) s
+
+o_1_space_serial_array ::
+    Int -> [Array.Array Int] -> [Array.Array Int] -> [Benchmark]
+o_1_space_serial_array bound arraysSmall arraysBig =
+    [ benchIO "fold (of 100)" (\_ -> Stream.fromList arraysSmall) fold
+    , benchIO "fold (single)" (\_ -> Stream.fromList arraysBig) fold
+    , benchIO "parse (of 100)" (\_ -> Stream.fromList arraysSmall)
+        $ parseArray bound
+    , benchIO "parse (single)" (\_ -> Stream.fromList arraysBig)
+        $ parseArray bound
+    ]
+
+-------------------------------------------------------------------------------
 -- Driver
 -------------------------------------------------------------------------------
 
@@ -193,13 +254,20 @@ moduleName = "Data.Array.Stream.Foreign"
 main :: IO ()
 main = do
     env <- mkHandleBenchEnv
-    defaultMain (allBenchmarks env)
+    runWithCLIOptsEnv defaultStreamSize alloc (allBenchmarks env)
 
     where
 
-    allBenchmarks env =
-        [ bgroup (o_1_space_prefix moduleName)
-          ( o_1_space_read_chunked env
-          ++ o_1_space_copy_toChunks_group_ungroup env
-          )
+    alloc value = do
+        small <- Stream.toList $ Stream.arraysOf 100 $ sourceUnfoldrM value 0
+        big <- Stream.toList $ Stream.arraysOf value $ sourceUnfoldrM value 0
+        return (small, big)
+
+    allBenchmarks env arrays value =
+        let (arraysSmall, arraysBig) = arrays
+        in [ bgroup (o_1_space_prefix moduleName) $ Prelude.concat
+          [ o_1_space_read_chunked env
+          , o_1_space_serial_array value arraysSmall arraysBig
+          , o_1_space_copy_toChunks_group_ungroup env
+          ]
         ]
