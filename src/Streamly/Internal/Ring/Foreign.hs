2ac8c2465257e2ddac8bfdf127c0b94d1f0e08f3
Harendra Kumar <harendra@composewell.com>
2021-11-19 01:55:43 +0530

    Add more ring buffer APIs
    Make ring buffer a first class array type in streamly.

--- src/Streamly/Internal/Ring/Foreign.hs
+++ src/Streamly/Internal/Ring/Foreign.hs
@@ -6,18 +6,53 @@
 -- Stability   : experimental
 -- Portability : GHC
 --
+-- A ring array is a circular mutable array.
+
+-- XXX Write benchmarks
+-- XXX Make the implementation similar to mutable array
+-- XXX Rename this module to Data.RingArray.Storable
 
 module Streamly.Internal.Ring.Foreign
-    ( Ring(..)
+    (
+    -- * Type
+    Ring(..)
 
     -- * Construction
     , new
+    , newRing
+    , writeN
+
     , advance
     , moveBy
     , startOf
 
-    -- * Modification
+    -- * Random writes
     , unsafeInsert
+    , slide
+    , putIndex
+    , modifyIndex
+
+    -- * Unfolds
+    , read
+    , readRev
+
+    -- * Random reads
+    , getIndex
+    , getIndexUnsafe
+    , getIndexRev
+
+    -- * Size
+    , length
+    , byteLength
+    -- , capacity
+    , byteCapacity
+    , bytesFree
+
+    -- * Casting
+    , cast
+    , castUnsafe
+    , asBytes
+    , fromArray
 
     -- * Folds
     , unsafeFoldRing
@@ -25,25 +60,39 @@ module Streamly.Internal.Ring.Foreign
     , unsafeFoldRingFullM
     , unsafeFoldRingNM
 
+    -- * Stream of Arrays
+    , ringsOf
+
     -- * Fast Byte Comparisons
     , unsafeEqArray
     , unsafeEqArrayN
     ) where
 
+#include "inline.hs"
+
 import Control.Exception (assert)
+import Control.Monad.IO.Class (MonadIO(..))
+import Data.Word (Word8)
 import Foreign.ForeignPtr (ForeignPtr, withForeignPtr, touchForeignPtr)
 import Foreign.ForeignPtr.Unsafe (unsafeForeignPtrToPtr)
 import Foreign.Ptr (plusPtr, minusPtr, castPtr)
 import Foreign.Storable (Storable(..))
 import GHC.ForeignPtr (mallocPlainForeignPtrAlignedBytes)
 import GHC.Ptr (Ptr(..))
-import Prelude hiding (length, concat)
-
-import Control.Monad.IO.Class (MonadIO(..))
-import Streamly.Internal.Data.Array.Foreign.Mut.Type (memcmp)
+import Streamly.Internal.Data.Array.Foreign.Mut.Type (Array, memcmp)
+import Streamly.Internal.Data.Fold.Type (Fold(..))
+import Streamly.Internal.Data.Stream.Serial (SerialT(..))
+-- import Streamly.Internal.Data.Stream.IsStream.Transform (scan)
+import Streamly.Internal.Data.Unfold.Type (Unfold(..))
 import Streamly.Internal.System.IO (unsafeInlineIO)
 
 import qualified Streamly.Internal.Data.Array.Foreign.Type as A
+
+import Prelude hiding (length, concat, read)
+
+-- $setup
+-- >>> :m
+-- >>> import qualified Streamly.Internal.Ring.Foreign as Ring
 
 -- | A ring buffer is a mutable array of fixed size. Initially the array is
 -- empty, with ringStart pointing at the start of allocated memory. We call the
@@ -62,6 +111,10 @@ data Ring a = Ring
     , ringBound :: {-# UNPACK #-} !(Ptr a)        -- first address beyond allocated memory
     }
 
+-------------------------------------------------------------------------------
+-- Construction
+-------------------------------------------------------------------------------
+
 -- | Get the first address of the ring as a pointer.
 startOf :: Ring a -> Ptr a
 startOf = unsafeForeignPtrToPtr . ringStart
@@ -78,6 +131,17 @@ new count = do
         { ringStart = fptr
         , ringBound = p `plusPtr` size
         }, p)
+
+-- XXX Rename this to "new".
+--
+-- | @newRing count@ allocates an empty array that can hold 'count' items.  The
+-- memory of the array is uninitialized and the allocation is aligned as per
+-- the 'Storable' instance of the type.
+--
+-- /Unimplemented/
+{-# INLINE newRing #-}
+newRing :: Int -> m (Ring a)
+newRing = undefined
 
 -- | Advance the ringHead by 1 item, wrap around if we hit the end of the
 -- array.
@@ -106,6 +170,47 @@ moveBy by Ring {..} ringHead = ringStartPtr `plusPtr` advanceFromHead
     off = assert (offInBytes `mod` elemSize == 0) $ offInBytes `div` elemSize
     advanceFromHead = (off + by `mod` len) * elemSize
 
+-- XXX Move the writeLastN from array module here.
+--
+-- | @writeN n@ is a rolling fold that keeps the last n elements of the stream
+-- in a ring array.
+--
+-- /Unimplemented/
+{-# INLINE writeN #-}
+writeN :: -- (Storable a, MonadIO m) =>
+    Int -> Fold m a (Ring a)
+writeN = undefined
+
+-------------------------------------------------------------------------------
+-- Conversions
+-------------------------------------------------------------------------------
+
+-- | Cast a mutable array to a ring array.
+fromArray :: Array a -> Ring a
+fromArray = undefined
+
+-------------------------------------------------------------------------------
+-- Conversion to/from array
+-------------------------------------------------------------------------------
+
+-- | Modify a given index of a ring array using a modifier function.
+--
+-- /Unimplemented/
+modifyIndex :: -- forall m a b. (MonadIO m, Storable a) =>
+    Ring a -> Int -> (a -> (a, b)) -> m b
+modifyIndex = undefined
+
+-- | /O(1)/ Write the given element at the given index in the ring array.
+-- Performs in-place mutation of the array.
+--
+-- >>> putIndex arr ix val = Ring.modifyIndex arr ix (const (val, ()))
+--
+-- /Unimplemented/
+{-# INLINE putIndex #-}
+putIndex :: -- (MonadIO m, Storable a) =>
+    Ring a -> Int -> a -> m ()
+putIndex = undefined
+
 -- | Insert an item at the head of the ring, when the ring is full this
 -- replaces the oldest item in the ring with the new item. This is unsafe
 -- beause ringHead supplied is not verified to be within the Ring. Also,
@@ -116,6 +221,155 @@ unsafeInsert rb ringHead newVal = do
     poke ringHead newVal
     -- touchForeignPtr (ringStart rb)
     return $ advance rb ringHead
+
+-- | Insert an item at the head of the ring, when the ring is full this
+-- replaces the oldest item in the ring with the new item.
+--
+-- /Unimplemented/
+slide :: -- forall m a. (MonadIO m, Storable a) =>
+    Ring a -> a -> m (Ring a)
+slide = undefined
+
+-------------------------------------------------------------------------------
+-- Random reads
+-------------------------------------------------------------------------------
+
+-- | Return the element at the specified index without checking the bounds.
+--
+-- Unsafe because it does not check the bounds of the ring array.
+{-# INLINE_NORMAL getIndexUnsafe #-}
+getIndexUnsafe :: -- forall m a. (MonadIO m, Storable a) =>
+    Ring a -> Int -> m a
+getIndexUnsafe = undefined
+
+-- | /O(1)/ Lookup the element at the given index. Index starts from 0.
+--
+{-# INLINE getIndex #-}
+getIndex :: -- (MonadIO m, Storable a) =>
+    Ring a -> Int -> m a
+getIndex = undefined
+
+-- | /O(1)/ Lookup the element at the given index from the end of the array.
+-- Index starts from 0.
+--
+-- Slightly faster than computing the forward index and using getIndex.
+--
+{-# INLINE getIndexRev #-}
+getIndexRev :: -- (MonadIO m, Storable a) =>
+    Ring a -> Int -> m a
+getIndexRev = undefined
+
+-------------------------------------------------------------------------------
+-- Size
+-------------------------------------------------------------------------------
+
+-- | /O(1)/ Get the byte length of the array.
+--
+-- /Unimplemented/
+{-# INLINE byteLength #-}
+byteLength :: Ring a -> Int
+byteLength = undefined
+
+-- | /O(1)/ Get the length of the array i.e. the number of elements in the
+-- array.
+--
+-- Note that 'byteLength' is less expensive than this operation, as 'length'
+-- involves a costly division operation.
+--
+-- /Unimplemented/
+{-# INLINE length #-}
+length :: -- forall a. Storable a =>
+    Ring a -> Int
+length = undefined
+
+-- | Get the total capacity of an array. An array may have space reserved
+-- beyond the current used length of the array.
+--
+-- /Pre-release/
+{-# INLINE byteCapacity #-}
+byteCapacity :: Ring a -> Int
+byteCapacity = undefined
+
+-- | The remaining capacity in the array for appending more elements without
+-- reallocation.
+--
+-- /Pre-release/
+{-# INLINE bytesFree #-}
+bytesFree :: Ring a -> Int
+bytesFree = undefined
+
+-------------------------------------------------------------------------------
+-- Unfolds
+-------------------------------------------------------------------------------
+
+-- | Unfold a ring array into a stream.
+--
+-- /Unimplemented/
+{-# INLINE_NORMAL read #-}
+read :: -- forall m a. (MonadIO m, Storable a) =>
+    Unfold m (Ring a) a
+read = undefined
+
+-- | Unfold a ring array into a stream in reverse order.
+--
+-- /Unimplemented/
+{-# INLINE_NORMAL readRev #-}
+readRev :: -- forall m a. (MonadIO m, Storable a) =>
+    Unfold m (Array a) a
+readRev = undefined
+
+-------------------------------------------------------------------------------
+-- Stream of arrays
+-------------------------------------------------------------------------------
+
+-- XXX Move this module to a lower level Ring/Type module and move ringsOf to a
+-- higher level ring module where we can import "scan".
+
+-- | @ringsOf n stream@ groups the input stream into a stream of
+-- ring arrays of size n. Each ring is a sliding window of size n.
+--
+-- /Unimplemented/
+{-# INLINE_NORMAL ringsOf #-}
+ringsOf :: -- forall m a. (MonadIO m, Storable a) =>
+    Int -> SerialT m a -> SerialT m (Array a)
+ringsOf = undefined -- Stream.scan (writeN n)
+
+-------------------------------------------------------------------------------
+-- Casting
+-------------------------------------------------------------------------------
+
+-- | Cast an array having elements of type @a@ into an array having elements of
+-- type @b@. The array size must be a multiple of the size of type @b@.
+--
+-- /Unimplemented/
+--
+castUnsafe :: Ring a -> Ring b
+castUnsafe = undefined
+
+-- | Cast an @Array a@ into an @Array Word8@.
+--
+-- /Unimplemented/
+--
+asBytes :: Ring a -> Ring Word8
+asBytes = castUnsafe
+
+-- | Cast an array having elements of type @a@ into an array having elements of
+-- type @b@. The length of the array should be a multiple of the size of the
+-- target element otherwise 'Nothing' is returned.
+--
+-- /Pre-release/
+--
+cast :: forall a b. Storable b => Ring a -> Maybe (Ring b)
+cast arr =
+    let len = byteLength arr
+        r = len `mod` sizeOf (undefined :: b)
+     in if r /= 0
+        then Nothing
+        else Just $ castUnsafe arr
+
+-------------------------------------------------------------------------------
+-- Equality
+-------------------------------------------------------------------------------
 
 -- XXX remove all usage of unsafeInlineIO
 --
@@ -166,6 +420,11 @@ unsafeEqArray Ring{..} rh A.Array{..} =
             return (r1 && r2)
     in res
 
+-------------------------------------------------------------------------------
+-- Folding
+-------------------------------------------------------------------------------
+
+-- XXX We can unfold it into a stream and fold the stream instead.
 -- XXX use MonadIO
 --
 -- | Fold the buffer starting from ringStart up to the given 'Ptr' using a pure
