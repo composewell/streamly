22e8ad3b996b89893dd596368c299e0200335f3d
Adithya Kumar <adithya@composewell.com>
2022-02-06 03:54:01 +0530

    Change the signature order for Foreign.getIndex

--- benchmark/Streamly/Benchmark/Data/Array/Stream/Foreign.hs
+++ benchmark/Streamly/Benchmark/Data/Array/Stream/Foreign.hs
@@ -85,7 +85,7 @@ toChunksLast inh = do
     larr <- Stream.last s
     return $ case larr of
         Nothing -> Nothing
-        Just arr -> Array.getIndex arr (Array.length arr - 1)
+        Just arr -> Array.getIndex (Array.length arr - 1) arr
 
 #ifdef INSPECTION
 inspect $ hasNoTypeClasses 'toChunksLast
