9b11e1f07b9804297bf9b44b32e264ee8ebd6a96
Adithya Kumar <adithya@composewell.com>
2022-03-07 01:45:49 +0530

    Remove the Monad constraint in mergeBy

--- src/Streamly/Internal/Data/Stream/IsStream/Expand.hs
+++ src/Streamly/Internal/Data/Stream/IsStream/Expand.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-redundant-constraints #-}
-
 -- |
 -- Module      : Streamly.Internal.Data.Stream.IsStream.Expand
 -- Copyright   : (c) 2017 Composewell Technologies
@@ -709,8 +707,7 @@ zipAsyncWith f = zipAsyncWithM (\a b -> return (f a b))
 --
 -- @since 0.6.0
 {-# INLINE mergeBy #-}
-mergeBy ::
-       (IsStream t, Monad m) => (a -> a -> Ordering) -> t m a -> t m a -> t m a
+mergeBy :: IsStream t => (a -> a -> Ordering) -> t m a -> t m a -> t m a
 mergeBy f m1 m2 = fromStream $ K.mergeBy f (toStream m1) (toStream m2)
 
 -- | Like 'mergeBy' but with a monadic comparison function.
