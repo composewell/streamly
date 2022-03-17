6726613bcaf4ac66b8ab2deabb5277cb35176e7c
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-03-17 17:46:55 +0530

    Remove the Sink module

--- benchmark/Streamly/Benchmark/Data/Fold.hs
+++ benchmark/Streamly/Benchmark/Data/Fold.hs
@@ -21,7 +21,6 @@ import Streamly.Internal.Data.Fold (Fold(..))
 import qualified Data.Map.Strict as Map
 import qualified Streamly.Internal.Data.Fold as FL
 import qualified Streamly.Internal.Data.Pipe as Pipe
-import qualified Streamly.Internal.Data.Sink as Sink
 import qualified Streamly.Internal.Data.Stream.IsStream as IP
 import qualified Streamly.Prelude as S
 
@@ -247,7 +246,6 @@ o_1_space_serial_elimination value =
         [ benchIOSink value "drain" (S.fold FL.drain)
         , benchIOSink value "drainBy" (S.fold (FL.drainBy return))
         , benchIOSink value "drainN" (S.fold (FL.drainN value))
-        , benchIOSink value "sink" (S.fold $ Sink.toFold Sink.drain)
         , benchIOSink value "last" (S.fold FL.last)
         , benchIOSink value "length" (S.fold FL.length)
         , benchIOSink value "sum" (S.fold FL.sum)
