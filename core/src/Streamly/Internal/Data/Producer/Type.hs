4e252a890bacecbcf2a5ddb4bd9ca2e0771c2d49
Adithya Kumar <adithya@composewell.com>
2022-06-18 22:21:46 +0530

    Remove duplicate function (lmap2) in Producer module

--- core/src/Streamly/Internal/Data/Producer/Type.hs
+++ core/src/Streamly/Internal/Data/Producer/Type.hs
@@ -23,7 +23,6 @@ module Streamly.Internal.Data.Producer.Type
     -- * Mapping
     , translate
     , lmap
-    , lmap2
 
     -- * Nesting
     , NestedLoop (..)
@@ -112,13 +111,6 @@ translate f g (Producer step inject extract) =
 {-# INLINE_NORMAL lmap #-}
 lmap :: (a -> a) -> Producer m a b -> Producer m a b
 lmap f (Producer step inject extract) = Producer step (inject . f) extract
-
--- | Map the producer input using the provided translation functions
---
--- /Pre-release/
-{-# INLINE_NORMAL lmap2 #-}
-lmap2 :: Functor m => (a -> c) -> (c -> a) -> Producer m c b -> Producer m a b
-lmap2 f g (Producer step inject extract) = Producer step (inject . f) (fmap g . extract)
 
 ------------------------------------------------------------------------------
 -- Functor
