3ea05e509e47ad71e9ace16921fdb1e67bd9383b
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-07-03 11:55:48 +0530

    Refactor test and type module for tree array

--- core/src/Streamly/Internal/Data/Array/Tree/Type.hs
+++ core/src/Streamly/Internal/Data/Array/Tree/Type.hs
@@ -1,14 +1,22 @@
-module Streamly.Internal.Data.Array.Tree.Type (TreeArray (count, depth), newArray, getIndex, snoc, appendN, read) where
+module Streamly.Internal.Data.Array.Tree.Type
+  ( TreeArray (count, depth),
+    newArray,
+    getIndex,
+    snoc,
+    appendN,
+    read,
+  )
+where
+
+#include "inline.hs"
 
 import Control.Monad.IO.Class (MonadIO (..))
-import Debug.Trace (trace)
 import qualified Streamly.Internal.Data.Array.Foreign.Mut as UArray
 import qualified Streamly.Internal.Data.Array.Mut.Type as BArray
 import qualified Streamly.Internal.Data.Fold.Type as FL
 import qualified Streamly.Internal.Data.Producer as Producer
 import Streamly.Internal.Data.Producer.Type (Producer (..))
 import qualified Streamly.Internal.Data.Stream.StreamD as D
-import qualified Streamly.Internal.Data.Stream.StreamK as K
 import Streamly.Internal.Data.Unboxed (Storable)
 import Streamly.Internal.Data.Unfold.Type (Unfold (..))
 import Prelude hiding (read)
@@ -29,33 +37,37 @@ type Index = Int
 -- fixed width each
 data TreeArray a = TreeArray {count :: Count, depth :: Depth, _root :: TreeArrayInternal a}
 
-data TreeArrayInternal a = Branch (BArray.Array (TreeArrayInternal a)) | Leaf (UArray.Array a)
+data TreeArrayInternal a = Branch {branchArray :: BArray.Array (TreeArrayInternal a)} | Leaf {_leafArray :: UArray.Array a}
 
+{-# INLINE newArray #-}
 newArray :: MonadIO m => m (TreeArray a)
 newArray = TreeArray 0 1 <$> newBranch
 
+{-# INLINE newBranch #-}
 newBranch :: MonadIO m => m (TreeArrayInternal a)
 newBranch = Branch <$> BArray.newArrayUninitialized branchWidth
 
+{-# INLINE newLeaf #-}
 newLeaf :: (MonadIO m, Storable a) => m (TreeArrayInternal a)
 newLeaf = Leaf <$> UArray.newArrayUnitialized leafWidth
 
+{-# INLINE snoc #-}
 snoc :: (MonadIO m, Storable a, Show a) => TreeArray a -> a -> m (TreeArray a)
-snoc t@(TreeArray count d b@(Branch branchArray)) v =
+snoc t@(TreeArray count d root) v =
   -- tree is full, add one layer of depth to it.
   if count == branchWidth ^ d * leafWidth
     then
       ( do
-          let copyBranch = Branch $ BArray.getSlice 0 branchWidth branchArray
-          _ <- BArray.putIndex branchArray 0 copyBranch
-          branch <- newBranch
-          _ <- BArray.putIndex branchArray 1 branch
-          _ <- go d 0 v branch
-          return $ TreeArray (count + 1) (d + 1) b
+          newRoot <- newBranch
+          _ <- BArray.putIndex (branchArray newRoot) 0 root
+          childBranch <- newBranch
+          _ <- BArray.putIndex (branchArray newRoot) 1 childBranch
+          _ <- go d 0 v childBranch
+          return $ TreeArray (count + 1) (d + 1) newRoot
       )
     else
       ( do
-          _ <- go d count v b
+          _ <- go d count v root
           return $ t {count = count + 1}
       )
   where
@@ -91,11 +103,11 @@ snoc t@(TreeArray count d b@(Branch branchArray)) v =
               childBranch <- newBranch
               _ <- BArray.putIndex branchArray childIndex childBranch
               go (d - 1) 0 v childBranch
-snoc _ _ = error "TreeArray root should always be a Branch constructor"
 
 -- | Lookup the element at the given index. Index starts from 0.
 --  /O(d)/ where d is the depth of the tree. depth of tree is proportional
 --  to log of number of elements to the base of `childWidth`.
+{-# INLINE getIndex #-}
 getIndex :: (MonadIO m, Storable a) => TreeArray a -> Index -> m a
 getIndex (TreeArray count depth treeArray) index = do
   if index < count then go depth index treeArray else invalidIndex "findAtOffset" index
@@ -103,17 +115,13 @@ getIndex (TreeArray count depth treeArray) index = do
     go :: (MonadIO m, Storable a) => Depth -> Index -> TreeArrayInternal a -> m a
     go _ i (Leaf leafArray) = UArray.getIndex i leafArray
     go d i (Branch branchArray) =
-      if d == 0 then error $ "reached here with index " ++ show index ++ " and depth " ++ show depth
-      else
       let -- branch will always have a depth greater than 0
           fullChildTree = branchWidth ^ (d - 1) * leafWidth
           -- index of next branch to visit
           childIndex = i `quot` fullChildTree
           -- index spilled over from skipping children
           spillOver = i `rem` fullChildTree
-       in do
-          liftIO $ print $ "go with d " ++ show d ++ " i " ++ show i
-          go (d - 1) spillOver =<< BArray.getIndex branchArray childIndex
+       in go (d - 1) spillOver =<< BArray.getIndex branchArray childIndex
 
 {-# INLINE_NORMAL appendN #-}
 appendN ::
@@ -132,13 +140,11 @@ invalidIndex label i =
   error $ label ++ " : invalid array index " ++ show i
 
 -- | Resumable unfold of a tree array.
---
+
 {-# INLINE_NORMAL producer #-}
 producer :: (MonadIO m, Storable a) => Producer m (TreeArray a) a
 producer = Producer step inject extract
-
-    where
-
+  where
     {-# INLINE inject #-}
     inject arr = return (arr, 0)
 
@@ -147,13 +153,13 @@ producer = Producer step inject extract
 
     {-# INLINE_LATE step #-}
     step (arr, i)
-        | i >= count arr = return D.Stop
+      | i >= count arr = return D.Stop
     step (arr, i) = do
-        x <- getIndex arr i
-        return $ D.Yield x (arr, i + 1)
+      x <- getIndex arr i
+      return $ D.Yield x (arr, i + 1)
 
 -- | Unfold an array into a stream.
---
+
 {-# INLINE_NORMAL read #-}
 read :: (MonadIO m, Storable a) => Unfold m (TreeArray a) a
 read = Producer.simplify producer
