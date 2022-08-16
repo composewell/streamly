c4dd0cb22516884b2847488e5b2309b9b82dabb1
Adithya Kumar <adithya@composewell.com>
2022-08-16 15:24:09 +0530

    Add Streamly.Data.Array.Unboxed.Mut

---
+++ core/src/Streamly/Data/Array/Unboxed/Mut.hs
@@ -0,0 +1,59 @@
+-- |
+-- Module      : Streamly.Data.Array.Unboxed.Mut
+-- Copyright   : (c) 2022 Composewell Technologies
+--
+-- License     : BSD3
+-- Maintainer  : streamly@composewell.com
+-- Stability   : released
+-- Portability : GHC
+--
+-- This module provides mutable arrays suitable for long lived data storage,
+-- random access, and mutation.
+--
+
+module Streamly.Data.Array.Unboxed.Mut
+    (
+      Array
+
+    -- * Arrays
+    -- ** Construction
+
+    -- Uninitialized Arrays
+    , newArray
+
+    -- From containers
+    , fromListN
+    , fromList
+    , writeN      -- drop new
+    , write       -- full buffer
+    -- writeLastN
+
+    -- ** Appending elements
+    , snoc
+
+    -- ** Appending streams
+    , appendN
+    , append
+
+    -- ** Inplace mutation
+    , putIndex
+
+    -- ** Random access
+    , getIndex
+
+    -- ** Elimination
+    , toList
+    , read
+    , readRev
+
+    -- ** Casting
+    , cast
+    , asBytes
+
+    -- ** Size
+    , length
+    )
+where
+
+import Prelude hiding (length, read)
+import Streamly.Internal.Data.Array.Unboxed.Mut
