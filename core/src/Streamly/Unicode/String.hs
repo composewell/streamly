5f746ba9cd9e97592aceacd48cac358a3f1ea831
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-09-30 14:07:23 +0530

    Expose Unicode.String

---
+++ core/src/Streamly/Unicode/String.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Streamly.Unicode.String
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : Apache-2.0
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- Convenient template Haskell quasiquoters to format strings.
+
+module Streamly.Unicode.String
+    ( str
+    )
+where
+
+import Streamly.Internal.Unicode.String
