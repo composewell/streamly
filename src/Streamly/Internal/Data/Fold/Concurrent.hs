879c24e774eb4fc85168f9f3cf9232ac0d1e9da6
Harendra Kumar <harendra@composewell.com>
2022-10-10 16:28:19 +0530

    Implement a concurrent fold channel

---
+++ src/Streamly/Internal/Data/Fold/Concurrent.hs
@@ -0,0 +1,68 @@
+-- |
+-- Module      : Streamly.Internal.Data.Fold.Concurrent
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- Eval a fold asynchronously in a separate thread.
+--
+-- Use multiple worker folds to fold a stream and collect the results using
+-- another fold, combine using a monoid. The results can be collected
+-- out-of-order or in-order.
+--
+-- Concurrent append: if one fold's buffer becomes full then use the next one
+-- Concurrent interleave/partition: Round robin to n folds.
+-- Concurrent distribute to multiple folds.
+
+module Streamly.Internal.Data.Fold.Concurrent
+    (
+    -- * Imports
+    -- $setup
+
+    -- * Configuration
+      Config
+    , maxBuffer
+    , inspect
+
+    -- * Combinators
+    -- | Stream combinators using Async channel
+
+    , eval
+    , evalWith
+    )
+where
+
+import Streamly.Internal.Control.Concurrent (MonadAsync)
+import Streamly.Internal.Data.Fold (Fold)
+
+import Streamly.Internal.Data.Fold.Concurrent.Channel
+
+-- $setup
+--
+-- Imports for example snippets in this module.
+--
+-- >>> :m
+-- >>> import Control.Concurrent (threadDelay)
+-- >>> import qualified Streamly.Data.Array.Unboxed as Array
+-- >>> import qualified Streamly.Data.Fold as Fold
+-- >>> import qualified Streamly.Data.Parser as Parser
+-- >>> import qualified Streamly.Data.Stream as Stream
+-- >>> import qualified Streamly.Internal.Data.Fold.Concurrent as Fold
+-- >>> import Prelude hiding (concatMap, concat)
+-- >>> :{
+--  delay n = do
+--      threadDelay (n * 1000000)   -- sleep for n seconds
+--      putStrLn (show n ++ " sec") -- print "n sec"
+--      return n                    -- IO Int
+-- :}
+
+-- | Evaluate a stream asynchronously using a channel and serve the consumer
+-- from the evaluation buffer.
+--
+-- >>> eval = Fold.evalWith id
+--
+{-# INLINE eval #-}
+eval :: MonadAsync m => Fold m a b -> Fold m a b
+eval = evalWith id
---
+++ src/Streamly/Internal/Data/Fold/Concurrent/Channel.hs
@@ -0,0 +1,91 @@
+-- |
+-- Module      : Streamly.Internal.Data.Fold.Concurrent.Channel
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+
+module Streamly.Internal.Data.Fold.Concurrent.Channel
+    (
+    -- * Channel
+      Channel
+
+    -- * Configuration
+    , Config
+    , maxBuffer
+    , inspect
+
+    -- * Fold operations
+    , evalWith
+    )
+where
+
+import Control.Concurrent (takeMVar)
+import Control.Monad (void, when)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.IORef (writeIORef)
+import Streamly.Internal.Control.Concurrent (MonadAsync)
+import Streamly.Internal.Data.Fold (Fold(..), Step (..))
+import Streamly.Internal.Data.Stream.Channel.Worker (sendWithDoorBell)
+import Streamly.Internal.Data.Time.Clock (Clock(Monotonic), getTime)
+
+import Streamly.Internal.Data.Fold.Concurrent.Channel.Type
+import Streamly.Internal.Data.Stream.Channel.Types
+
+-------------------------------------------------------------------------------
+-- Evaluating a Fold
+-------------------------------------------------------------------------------
+
+-- XXX Cleanup the fold if the stream is interrupted. Add a GC hook.
+
+-- | Evaluate the fold asynchronously in a worker thread separate from the
+-- driver thread.
+--
+{-# INLINABLE evalWith #-}
+evalWith :: MonadAsync m => (Config -> Config) -> Fold m a b -> Fold m a b
+evalWith modifier f =
+    Fold step initial extract
+
+    where
+
+    initial = Partial <$> newChannel modifier f
+
+    -- XXX This is not truly asynchronous. If the fold is done we only get to
+    -- know when we send the next input unless the stream ends. We could
+    -- potentially throw an async exception to the driver to inform it
+    -- asynchronously. Alternatively, the stream should not block forever, it
+    -- should keep polling the fold status. We can insert a timer tick in the
+    -- input stream to do that.
+    --
+    -- A polled stream abstraction may be useful, it would consist of normal
+    -- events and tick events, latter are guaranteed to arrive.
+    step chan a = do
+        status <- sendToWorker chan a
+        return $ case status of
+            Nothing -> Partial chan
+            Just b -> Done b
+
+    extract chan = do
+        liftIO $ void
+            $ sendWithDoorBell
+                (outputQueue chan)
+                (outputDoorBell chan)
+                ChildStopChannel
+        status <- checkFoldStatus chan
+        case status of
+            Nothing -> do
+                liftIO
+                    $ withDiagMVar
+                        (svarInspectMode chan)
+                        (dumpSVar chan)
+                        "evalWith: waiting to drain"
+                    $ takeMVar (outputDoorBellFromConsumer chan)
+                -- XXX remove recursion
+                extract chan
+            Just b -> do
+                when (svarInspectMode chan) $ liftIO $ do
+                    t <- getTime Monotonic
+                    writeIORef (svarStopTime (svarStats chan)) (Just t)
+                    printSVar (dumpSVar chan) "SVar Done"
+                return b
