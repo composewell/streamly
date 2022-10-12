10231b6724dcb8202e327fb334521e43c8254f9a
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-10-12 11:48:06 +0530

    Fix up

---
+++ benchmark/Streamly/Benchmark/Data/Stream/ConcurrentOrdered.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
+-- |
+-- Module      : Main
+-- Copyright   : (c) 2018 Composewell Technologies
+--
+-- License     : BSD3
+-- Maintainer  : streamly@composewell.com
+
+import Stream.AsyncCommon (allBenchmarks)
+import Streamly.Benchmark.Common (runWithCLIOpts, defaultStreamSize)
+
+import qualified Streamly.Internal.Data.Stream.Concurrent as Async
+
+moduleName :: String
+moduleName = "Data.Stream.ConcurrentOrdered"
+
+-------------------------------------------------------------------------------
+-- Main
+-------------------------------------------------------------------------------
+
+main :: IO ()
+main =
+    runWithCLIOpts defaultStreamSize (allBenchmarks moduleName Async.ordered)
