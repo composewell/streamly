5a384eae7ca7256b4f15fbbeb33dd655a7f0a6f6
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-10-12 15:01:01 +0530

    Expose concurrent operations via Streamly.Data.Stream.Concurrent

--- src/Streamly/Data/Stream/Concurrent.hs
+++ src/Streamly/Data/Stream/Concurrent.hs
@@ -7,8 +7,54 @@
 -- Portability : GHC
 
 module Streamly.Data.Stream.Concurrent
-  ( MonadAsync
-  )
+    (
+      MonadAsync
+    , Config
+    , defaultConfig
+
+    -- ** Set config
+    , maxThreads
+    , maxBuffer
+    , maxYields
+    , inspectMode
+    , eagerEval
+    , stopWhen
+    , ordered
+
+    , rate
+    , avgRate
+    , minRate
+    , maxRate
+    , constRate
+
+    , mapM
+    , mapMWith
+    , sequence
+    , sequenceWith
+
+    -- XXX experimental binary ops, move to parallel?
+    , append
+    , appendWith
+    , interleave
+    , interleaveWith
+    , ahead
+    , parallel
+    , parallelFst
+    , parallelMin
+
+    , apply
+    , applyWith
+    , concatList
+    , concat
+    , concatWith
+    , concatMap
+    , concatMapInterleave
+    , concatMapWith
+    , concatMapInterleaveWith
+    )
 where
 
-import Streamly.Internal.Control.Concurrent (MonadAsync)
+import Streamly.Internal.Data.Stream.Concurrent
+import Streamly.Internal.Data.Stream.Channel.Types
+    ( defaultConfig, maxYields )
+import Prelude hiding (mapM, sequence, concat, concatMap)
