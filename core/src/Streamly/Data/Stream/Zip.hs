67bded664fd53b7c30316b333ac7f9f523637d84
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-10-13 16:25:17 +0530

    Fix review comments

--- core/src/Streamly/Data/Stream/Zip.hs
+++ core/src/Streamly/Data/Stream/Zip.hs
@@ -10,24 +10,7 @@
 module Streamly.Data.Stream.Zip
     (
       ZipStream (..)
-    , ZipSerialM
-    , ZipSerial
-    , zipWithM
-    , zipWith
     )
 where
 
-import Streamly.Internal.Data.Stream (Stream)
 import Streamly.Internal.Data.Stream.Zip
-
-import Prelude hiding (zipWith)
-
-{-# INLINE zipWithM #-}
-zipWithM :: Monad m =>
-    (a -> b -> m c) -> Stream m a -> Stream m b -> Stream m c
-zipWithM = zipWithMD
-
-{-# INLINE zipWith #-}
-zipWith :: Monad m
-    => (a -> b -> c) -> Stream m a -> Stream m b -> Stream m c
-zipWith = zipWithD
