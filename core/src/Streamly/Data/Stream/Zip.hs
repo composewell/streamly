36410d9ff6053abc58d116b103c00e0cc0fc3d32
Ranjeet Ranjan <ranjeetr@composewell.com>
2022-10-13 19:10:46 +0530

    Expose Streamly.Data.Stream.Zip from streamly-core package (#1945)

---
+++ core/src/Streamly/Data/Stream/Zip.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Streamly.Data.Stream.Zip
+-- Copyright   : (c) 2017 Composewell Technologies
+--
+-- License     : BSD3
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+module Streamly.Data.Stream.Zip
+    (
+      ZipStream (..)
+    )
+where
+
+import Streamly.Internal.Data.Stream.Zip
