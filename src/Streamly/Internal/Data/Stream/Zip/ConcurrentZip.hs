ab256566d25fa801528db258605e94cb6f9dda47
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-10-14 13:24:04 +0530

    Add docspec

--- src/Streamly/Internal/Data/Stream/Zip/ConcurrentZip.hs
+++ src/Streamly/Internal/Data/Stream/Zip/ConcurrentZip.hs
@@ -21,7 +21,6 @@ module Streamly.Internal.Data.Stream.Zip.ConcurrentZip
     )
 where
 
-
 import Streamly.Internal.Data.Stream (Stream)
 import Streamly.Internal.Data.Stream.Concurrent (MonadAsync, zipWith)
 
@@ -31,15 +30,32 @@ import Prelude hiding (map, repeat, zipWith, errorWithoutStackTrace)
 
 #include "Instances.hs"
 
+-- $setup
+-- >>> import qualified Streamly.Internal.Data.Stream as Stream
+-- >>> import qualified Streamly.Data.Fold as Fold
+-- >>> import Control.Concurrent (threadDelay)
+-- >>> :{
+--  delay n = do
+--      threadDelay (n * 1000000)   -- sleep for n seconds
+--      putStrLn (show n ++ " sec") -- print "n sec"
+--      return n                    -- IO Int
+-- :}
+
 newtype ConcurrentZipM m a = ConcurrentZipM {getStream :: Stream m a}
         deriving (Semigroup, Monoid)
 
--- | An IO stream whose applicative instance zips streams concurrently.--
+-- | An IO stream whose applicative instance zips streams concurrently.
+--
+-- >>> s = ConcurrentZipM $ Stream.fromList [1, 2, 3]
+-- >>> x = (,,) <$> s <*> s <*> s
+-- >>> Stream.fold F.toList (getStream x)
+-- [(1,1,1),(2,2,2),(3,3,3)]
 --
 -- @since 0.9.0
 type ConcurrentZip = ConcurrentZipM IO
 
-consMConcurrentZip :: Monad m => m a -> ConcurrentZipM m a -> ConcurrentZipM m a
+consMConcurrentZip :: Monad m =>
+    m a -> ConcurrentZipM m a -> ConcurrentZipM m a
 consMConcurrentZip m (ConcurrentZipM r) = ConcurrentZipM $ Stream.consM m r
 
 instance Monad m => Functor (ConcurrentZipM m) where
