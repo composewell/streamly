ee2fd6976d7788c585cfff6cd89b26af9dce52a9
Harendra Kumar <harendra@composewell.com>
2022-10-15 04:05:28 +0530

    Cleanly separate the Channel and stream modules

--- src/Streamly/Internal/Data/Stream/Concurrent.hs
+++ src/Streamly/Internal/Data/Stream/Concurrent.hs
@@ -86,12 +86,16 @@ module Streamly.Internal.Data.Stream.Concurrent
     )
 where
 
-import Streamly.Internal.Control.Concurrent (MonadAsync)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Streamly.Internal.Control.Concurrent (MonadAsync, askRunInIO)
+import Streamly.Internal.Data.Stream.Channel.Types (concatMapDivK)
 import Streamly.Internal.Data.Stream.Type (Stream)
 
 import qualified Streamly.Internal.Data.Stream as Stream
+import qualified Streamly.Internal.Data.Stream.StreamK.Type as K
 
 import Streamly.Internal.Data.Stream.Concurrent.Channel
+
 import Prelude hiding (mapM, sequence, concat, concatMap)
 
 -- $setup
@@ -114,16 +118,38 @@ import Prelude hiding (mapM, sequence, concat, concatMap)
 -- :}
 
 -------------------------------------------------------------------------------
--- Useful operations
+-- Evaluating a stream
 -------------------------------------------------------------------------------
+
+{-
+{-# INLINE_NORMAL evalWithD #-}
+evalWithD :: MonadAsync m => (Config -> Config) -> D.Stream m a -> D.Stream m a
+evalWithD modifier m = D.Stream step Nothing
+    where
+
+    step _ Nothing = do
+        chan <- newChannel modifier
+        sendFirstWorker chan (D.toStreamK m)
+        -- XXX should use an unfold to make this efficient
+        return $ D.Skip $ Just $ fromChannelD chan
+
+    step gst (Just (D.UnStream step1 st)) = do
+        r <- step1 gst st
+        return $ case r of
+            D.Yield a s -> D.Yield a (Just $ D.Stream step1 s)
+            D.Skip s    -> D.Skip (Just $ D.Stream step1 s)
+            D.Stop      -> D.Stop
+-}
 
 -- | Like 'eval' but can specify a config modifier to change the concurrent
 -- channel parameters.
+--
+-- > evalWith = withChannel (const id)
+--
 {-# INLINE evalWith #-}
 evalWith :: MonadAsync m => (Config -> Config) -> Stream m a -> Stream m a
-evalWith cfg stream =
+evalWith = withChannel (const id)
     -- Stream.fromStreamD $ evalWithD cfg $ Stream.toStreamD stream
-    Stream.fromStreamK $ evalWithK cfg $ Stream.toStreamK stream
 
 -- | Evaluate a stream asynchronously using a channel and serve the consumer
 -- from the evaluation buffer.
@@ -135,8 +161,64 @@ eval :: MonadAsync m => Stream m a -> Stream m a
 eval = evalWith id
 
 -------------------------------------------------------------------------------
--- Concurrent combinators for "Stream" type
+-- appending two streams
 -------------------------------------------------------------------------------
+
+{-# INLINE _appendGeneric #-}
+_appendGeneric :: MonadAsync m =>
+       ((Config -> Config) -> m (Channel m a))
+    -> (Config -> Config)
+    -> K.Stream m a
+    -> K.Stream m a
+    -> K.Stream m a
+_appendGeneric newChan modifier stream1 stream2 = K.concatEffect action
+
+    where
+
+    action = do
+        chan <- newChan modifier
+        let cfg = modifier defaultConfig
+            done = K.nilM (stopChannel chan)
+        case getStopWhen cfg of
+            AllStop -> do
+                toChannelK chan stream2
+                toChannelK chan stream1
+            FirstStops -> do
+                toChannelK chan stream2
+                toChannelK chan (K.serial stream1 done)
+            AnyStops -> do
+                toChannelK chan (K.serial stream2 done)
+                toChannelK chan (K.serial stream1 done)
+        return $ Stream.toStreamK $ fromChannel chan
+
+-- | Create a new channel and add both the streams to it for async evaluation.
+-- The output stream is the result of the evaluation.
+{-# INLINE appendWithK #-}
+appendWithK :: MonadAsync m =>
+    (Config -> Config) -> K.Stream m a -> K.Stream m a -> K.Stream m a
+appendWithK modifier stream1 stream2 =
+{-
+    if getOrdered (modifier defaultConfig)
+    then concatMapWithK modifier id (stream1 `K.cons` K.fromPure stream2)
+    else _appendGeneric Append.newChannel modifier stream1 stream2
+-}
+    concatMapWithK modifier id (stream1 `K.cons` K.fromPure stream2)
+
+-- | Evaluate the first stream in the current thread and add the second stream
+-- to the supplied channel. This is to be used by a worker thread.
+--
+-- This can be used with concatMapWith:
+--
+-- @
+-- concatMap = K.concatMapWith (_appendWithChanK chan) f stream
+-- @
+--
+{-# INLINE _appendWithChanK #-}
+_appendWithChanK :: MonadAsync m =>
+    Channel m a -> K.Stream m a -> K.Stream m a -> K.Stream m a
+_appendWithChanK chan stream1 stream2 =
+    K.before (toChannelK chan stream2) stream1
+
 
 -- | Like 'append' but with a Config modifier.
 {-# INLINE combineWith #-}
@@ -216,6 +298,101 @@ parallelFst = combineWith (eagerEval . stopWhen FirstStops)
 {-# INLINE parallelMin #-}
 parallelMin :: MonadAsync m => Stream m a -> Stream m a -> Stream m a
 parallelMin = combineWith (eagerEval . stopWhen AnyStops)
+
+-------------------------------------------------------------------------------
+-- concat
+-------------------------------------------------------------------------------
+
+-- | A runner function takes a queuing function @q@ and a stream, it splits the
+-- input stream, queuing the tail and using the head to generate a stream.
+-- 'mkEnqueue' takes a runner function and generates the queuing function @q@.
+-- Note that @q@ and the runner are mutually recursive, mkEnqueue ties the knot
+-- between the two.
+{-# INLINE mkEnqueue #-}
+mkEnqueue :: MonadAsync m =>
+    Channel m b
+    -> ((K.Stream m a -> m ()) -> K.Stream m a -> K.Stream m b)
+    -> m (K.Stream m a -> m ())
+mkEnqueue chan runner = do
+    runInIO <- askRunInIO
+    return
+        $ let q stream = do
+                -- Enqueue the outer loop
+                liftIO $ enqueue chan False (runInIO, runner q stream)
+                -- XXX In case of eager dispatch we can just directly dispatch
+                -- a worker with the tail stream here rather than first queuing
+                -- and then dispatching a worker which dequeues the work. The
+                -- older implementation did a direct dispatch here and its perf
+                -- characterstics looked much better.
+                eagerDispatch chan
+           in q
+
+-- XXX Can be renamed to concatMapWithK if we move concatMapWithK to higher
+-- level module. We can keep only Channel based ops in this module.
+
+-- | Takes the head element of the input stream and queues the tail of the
+-- stream to the channel, then maps the supplied function on the head and
+-- evaluates the resulting stream.
+--
+-- This function is designed to be used by worker threads on a channel to
+-- concurrently map and evaluate a stream.
+{-# INLINE concatMapWithChanK #-}
+concatMapWithChanK :: MonadAsync m =>
+    Channel m b -> (a -> K.Stream m b) -> K.Stream m a -> K.Stream m b
+concatMapWithChanK chan f stream =
+   let run q = concatMapDivK q f
+    in K.concatMapEffect (`run` stream) (mkEnqueue chan run)
+    -- K.concatMapWith (_appendWithChanK chan) f stream
+
+{-# INLINE concatMapWithChanKAny #-}
+concatMapWithChanKAny :: MonadAsync m =>
+    Channel m b -> (a -> K.Stream m b) -> K.Stream m a -> K.Stream m b
+concatMapWithChanKAny chan f stream =
+   let done = K.nilM (stopChannel chan)
+       run q = concatMapDivK q (\x -> K.serial (f x) done)
+    in K.concatMapEffect (`run` stream) (mkEnqueue chan run)
+
+{-# INLINE concatMapWithChanKFirst #-}
+concatMapWithChanKFirst :: MonadAsync m =>
+    Channel m b -> (a -> K.Stream m b) -> K.Stream m a -> K.Stream m b
+concatMapWithChanKFirst chan f stream =
+   let done = K.nilM (stopChannel chan)
+       run q = concatMapDivK q f
+    in K.concatEffect $ do
+        res <- K.uncons stream
+        case res of
+            Nothing -> return K.nil
+            Just (h, t) -> do
+                q <- mkEnqueue chan run
+                q t
+                return $ K.serial (f h) done
+
+{-# INLINE concatMapWithChanKGeneric #-}
+concatMapWithChanKGeneric :: MonadAsync m =>
+       (Config -> Config)
+    -> Channel m b
+    -> (a -> K.Stream m b)
+    -> K.Stream m a
+    -> K.Stream m b
+concatMapWithChanKGeneric modifier chan f stream = do
+        let cfg = modifier defaultConfig
+        case getStopWhen cfg of
+            AllStop -> concatMapWithChanK chan f stream
+            FirstStops -> concatMapWithChanKFirst chan f stream
+            AnyStops -> concatMapWithChanKAny chan f stream
+
+-- XXX Add a deep evaluation variant that evaluates individual elements in the
+-- generated streams in parallel.
+
+-- | Allocate a channel and use it to concurrently evaluate the streams
+-- generated by the mapped function.
+--
+{-# INLINE concatMapWithK #-}
+concatMapWithK :: MonadAsync m =>
+    (Config -> Config) -> (a -> K.Stream m b) -> K.Stream m a -> K.Stream m b
+concatMapWithK modifier f =
+    let g = concatMapWithChanKGeneric modifier
+     in withChannelK (`g` f) modifier
 
 -- concatMapWith modifier f stream = concatWith modifier $ fmap f stream
 
