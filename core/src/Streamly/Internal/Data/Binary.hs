3866e42c1d7edb8828d5588484d1ce726cdcee5e
Harendra Kumar <harendra@composewell.com>
2022-12-16 07:47:35 +0530

    Add a type class for binary encoding/decoding

---
+++ core/src/Streamly/Internal/Data/Binary.hs
@@ -0,0 +1,27 @@
+-- |
+-- Module      : Streamly.Internal.Data.Binary
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : pre-release
+-- Portability : GHC
+--
+-- Convert Haskell data types to byte streams and vice-versa.
+
+module Streamly.Internal.Data.Binary
+    ( ToBytes (..)
+    , FromBytes (..)
+    )
+where
+
+import Data.Word (Word8)
+import Streamly.Internal.Data.Stream (Stream)
+import Streamly.Internal.Data.Parser (Parser)
+
+class ToBytes a where
+    -- | Convert a Haskell type to a byte stream.
+    toBytes :: a -> Stream m Word8
+
+class FromBytes a where
+    -- | Decode a byte stream to a Haskell type.
+    fromBytes :: Parser Word8 m a
