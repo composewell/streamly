1221bb6cc8561b5eb890bf7f7e6d15bff9433b13
Harendra Kumar <harendra@composewell.com>
2023-02-02 17:38:07 +0530

    Add Streamly.Internal.Data.Stream module

---
+++ core/src/Streamly/Internal/Data/Stream.hs
@@ -0,0 +1,14 @@
+-- |
+-- Module      : Streamly.Internal.Data.Stream
+-- Copyright   : (c) 2019 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+module Streamly.Internal.Data.Stream
+    ( module Streamly.Internal.Data.Stream.StreamD
+    )
+where
+
+import Streamly.Internal.Data.Stream.StreamD
