2562e3e111c806794ac019139d223b3d9dd45832
Harendra Kumar <harendra@composewell.com>
2023-03-07 23:00:30 +0530

    Add code to check fusion in Stream benchmarks

--- benchmark/Streamly/Benchmark/Data/Stream.hs
+++ benchmark/Streamly/Benchmark/Data/Stream.hs
@@ -9,6 +9,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
 
+#undef FUSION_CHECK
+#ifdef FUSION_CHECK
+{-# OPTIONS_GHC -ddump-simpl -ddump-to-file -dsuppress-all #-}
+#endif
+
 module Main (main) where
 
 import Streamly.Benchmark.Common.Handle (mkHandleBenchEnv)
@@ -28,6 +33,15 @@ import qualified Stream.Transform as Transformation
 
 import Streamly.Benchmark.Common
 
+#ifdef FUSION_CHECK
+import Data.Function ((&))
+import Streamly.Data.Stream (Stream)
+import qualified Streamly.Internal.Data.Stream.StreamD as Stream
+import qualified Streamly.Internal.Data.Fold as Fold
+import qualified Data.Foldable as Foldable
+import Stream.Common
+#endif
+
 moduleName :: String
 #ifdef USE_STREAMK
 moduleName = "Data.Stream.StreamDK"
@@ -44,6 +58,7 @@ moduleName = "Data.Stream"
 --
 main :: IO ()
 main = do
+#ifndef FUSION_CHECK
     env <- mkHandleBenchEnv
     runWithCLIOpts defaultStreamSize (allBenchmarks env)
 
@@ -63,3 +78,37 @@ main = do
         , Lift.benchmarks moduleName size
         , NestedStream.benchmarks moduleName size
         ]
+#else
+    -- Enable FUSION_CHECK macro at the beginning of the file
+    -- Enable one benchmark below, and run the benchmark
+    -- Check the .dump-simpl output
+    let value = 100000
+    let input = sourceUnfoldrM value 1
+
+    -- Stream.foldr (:) [] input >>= print . Prelude.length
+    -- Stream.fold (Fold.foldr' (:) []) input >>= print . Prelude.length
+
+    -- Stream.toList input >>= print . Prelude.length
+    -- Stream.fold Fold.toList input >>= print . Prelude.length
+
+    -- We can automatically assert that these generate identical core
+
+    -- Stream.drain input >>= print
+    -- Stream.fold Fold.drain input >>= print
+
+    -- Stream.last input >>= print
+    -- Stream.fold Fold.latest input >>= print
+
+    -- Stream.length input >>= print
+    -- Stream.fold Fold.length input >>= print
+
+    -- Stream.maximum input >>= print
+    -- Stream.fold Fold.maximum input >>= print
+
+    -- Stream.head input >>= print
+    -- Stream.fold Fold.one input >>= print
+
+    -- composeN 4 (Stream.scanl' (+) 0) input
+    composeN 4 (Stream.scan (Fold.foldl' (+) 0)) input
+    return ()
+#endif
