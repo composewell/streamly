9d124313843999fdd93f9796ebf6584a42c726c2
Harendra Kumar <harendra@composewell.com>
2023-03-07 13:13:00 +0530

    Add more documentation to StreamK module

--- core/src/Streamly/Data/StreamK.hs
+++ core/src/Streamly/Data/StreamK.hs
@@ -9,6 +9,26 @@
 --
 -- Streams using Continuation Passing Style (CPS). See notes in "Data.Stream"
 -- module to know when to use this module.
+--
+-- 'StreamK' operations have more overhead than 'Stream' operations, however,
+-- 'Stream' operations fuse only when the pipeline is statically composed, is
+-- not too large, and does not involve recursion. For such cases, a typical
+-- example is creating a large or infinite stream by appending, 'StreamK'
+-- should be used.
+--
+-- You can use operations from "Streamly.Data.Stream" for StreamK as well by
+-- converting StreamK to Stream, and vice-versa. Most of the time converting to
+-- 'Stream' is better than using native StreamK operations, however in certain
+-- cases using native StreamK operations may work better. Using 'Stream'
+-- operations are especially when multiple contiguous operations can fuse
+-- together.
+--
+-- There are no native exception handling operations in the StreamK module,
+-- please use 'toStream' to convert to 'Stream' type and use exception
+-- handling operations from "Streamly.Data.Stream".
+--
+-- See "Streamly.Internal.Data.Stream.StreamK" for more native StreamK
+-- operations.
 
 -- primitive/open loop operations that can be used recursively e.g. uncons,
 -- foldBreak, parseBreak should not be converted from StreamD for use in
