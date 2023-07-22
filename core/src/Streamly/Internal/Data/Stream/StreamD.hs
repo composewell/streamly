53fd11850fbe21876835a5e228e1705e56e4b33d
Harendra Kumar <harendra@composewell.com>
2023-07-22 22:58:27 +0530

    Add StreamD as a deprecated module

---
+++ core/src/Streamly/Internal/Data/Stream/StreamD.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Streamly.Internal.Data.Stream.StreamD
+-- Copyright   : (c) 2018 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+
+module Streamly.Internal.Data.Stream.StreamD
+{-# DEPRECATED "Please use \"Streamly.Internal.Data.Stream\" instead." #-}
+    (
+      module Streamly.Internal.Data.Stream
+    )
+where
+
+import Streamly.Internal.Data.Stream
