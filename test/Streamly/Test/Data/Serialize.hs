2ee80653f9c17ff48865b84523f75410383135ee
Adithya Kumar <adithya@composewell.com>
2023-08-02 18:59:13 +0530

    Add tests for the Serialize template haskell deriving

--- test/Streamly/Test/Data/Serialize.hs
+++ test/Streamly/Test/Data/Serialize.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 -- |
 -- Module      : Streamly.Test.Data.Serialize
 -- Copyright   : (c) 2022 Composewell technologies
@@ -16,11 +18,43 @@ import Streamly.Internal.Data.Unbox (newBytes)
 
 import Test.Hspec as H
 
+import Streamly.Internal.Data.Serialize.TH (deriveSerialize)
+-- import Streamly.Internal.Data.Serialize (Serialize(..))
+
 import qualified Streamly.Internal.Data.Serialize as Serialize
+
+import Streamly.Test.Data.Serialize.TH (genDatatype)
+
+import Test.Hspec.QuickCheck
+
+--------------------------------------------------------------------------------
+-- Generated types
+--------------------------------------------------------------------------------
+
+$(genDatatype "CustomDatatype" 15)
+$(deriveSerialize ''CustomDatatype)
 
 --------------------------------------------------------------------------------
 -- Test helpers
 --------------------------------------------------------------------------------
+
+roundtrip
+    :: forall a. (Eq a, Show a, Serialize.Serialize a)
+    => a
+    -> IO ()
+roundtrip val = do
+
+    let sz =
+          case Serialize.size :: Serialize.Size a of
+              Serialize.VarSize f -> f val
+              Serialize.ConstSize csz -> csz
+
+    arr <- newBytes sz
+
+    off1 <- Serialize.serialize 0 arr val
+    (off2, val2) <- Serialize.deserialize 0 arr
+    val2 `shouldBe` val
+    off2 `shouldBe` off1
 
 testSerializeList
     :: forall a. (Eq a, Show a, Serialize.Serialize a)
@@ -36,12 +70,7 @@ testSerializeList sizeOfA val = do
 
     sz `shouldBe` sizeOfA
 
-    arr <- newBytes sz
-
-    off1 <- Serialize.serialize 0 arr val
-    (off2, val2) <- Serialize.deserialize 0 arr
-    val2 `shouldBe` val
-    off2 `shouldBe` off1
+    roundtrip val
 
 --------------------------------------------------------------------------------
 -- Tests
@@ -56,6 +85,11 @@ testCases = do
         $ testSerializeList
               (8 + 3 * 8 + 6 * 8)
               ([[1], [1, 2], [1, 2, 3]] :: [[Int]])
+
+    prop "CustomDatatype"
+        $ \(x :: CustomDatatype) -> roundtrip x
+    prop "[CustomDatatype]"
+        $ \(x :: [CustomDatatype]) -> roundtrip x
 
 --------------------------------------------------------------------------------
 -- Main function
