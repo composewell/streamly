fa9fa0960e975a0a9db39544fa7726e3ca2c1a17
Harendra Kumar <harendra@composewell.com>
2023-11-26 14:37:45 +0530

    Consolidate SVar internal modules

---
+++ src/Streamly/Internal/Data/Stream/SVar.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      : Streamly.Internal.Data.Stream.SVar
+-- Copyright   : (c) 2022 Composewell Technologies
+-- License     : BSD-3-Clause
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+module Streamly.Internal.Data.Stream.SVar
+    {-# DEPRECATED "This module will be removed in future." #-}
+    (
+      module Streamly.Internal.Data.Stream.SVar.Eliminate
+    , module Streamly.Internal.Data.Stream.SVar.Generate
+    )
+where
+
+import Streamly.Internal.Data.Stream.SVar.Eliminate
+import Streamly.Internal.Data.Stream.SVar.Generate
