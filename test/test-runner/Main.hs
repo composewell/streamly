5adbe6e48d07053acf1eb2d70df749695485f6ec
Harendra Kumar <harendra@composewell.com>
2024-02-19 08:58:33 +0530

    Increase memory for Serialize tests

--- test/test-runner/Main.hs
+++ test/test-runner/Main.hs
@@ -10,24 +10,29 @@ import qualified Data.Map as Map
 import Targets (targets)
 
 rtsOpts :: Bool -> String -> String -> String
-rtsOpts relaxMem exeName target =
+rtsOpts relaxMem exeName target = mkOpts <> " -t"
 
-    if relaxMem
-    then "-K16M -M1024M"
-    else "-K8M -M64M " <> targetSpecific
 
     where
+
+    mkOpts =
+        if relaxMem
+        then "-K16M -M1024M"
+        else "-K8M -M64M " <> targetSpecific
 
     targetSpecific =
         case target of
             -- XXX Data.Array.* heap requirement increased for GHC-8.10
-            "Data.Array.Unboxed" -> "-M128M"
+            "Data.Array" -> "-M128M"
             "Data.Array.Generic" -> "-M128M"
+            "Data.Stream.Rate" -> "-M512M"
+            "Data.Serialize" -> "-M128M"
+            "Data.Serialize.ENABLE_constructorTagAsString" -> "-M128M"
             "Prelude.Rate" -> "-M512M"
             -- For -O0 case writeChunks test fails, maybe we should have a
             -- separate flag for O0 case?
-            "FileSystem.Handle" -> "-K16M"
+            "FileSystem.Handle" -> "-K16M -M128M"
             _ -> ""
 
 main :: IO ()
-main = mainWith targets (rtsOpts ++ " -t")
+main = mainWith targets rtsOpts
