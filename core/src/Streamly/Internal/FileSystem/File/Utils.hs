80f1cd7d99b9c170e44764cda92e20295aba1a8f
Harendra Kumar <harendra@composewell.com>
2024-11-13 15:18:40 +0530

    Fix bug, cleanup the Posix,Windows File modules
    Use asCWStringUnsafe to create a null terminated C string in windows
    module.
    
    Export openFile functions directly from the platform specific module and
    reverse the dependency on Utils module.

--- core/src/Streamly/Internal/FileSystem/File/Utils.hs
+++ core/src/Streamly/Internal/FileSystem/File/Utils.hs
@@ -1,6 +1,6 @@
 module Streamly.Internal.FileSystem.File.Utils
-    ( openFile
-    , withFile
+    ( withFile
+    , openFile
     ) where
 
 -------------------------------------------------------------------------------
@@ -9,17 +9,11 @@ module Streamly.Internal.FileSystem.File.Utils
 
 import Control.Exception (mask, onException, try)
 import Control.Monad (when)
-import GHC.IO (catchException, unsafePerformIO)
+import GHC.IO (catchException)
 import GHC.IO.Exception (IOException(..))
 import GHC.IO.Handle.Internals (handleFinalizer)
 import Streamly.Internal.FileSystem.Path (Path)
 import System.IO (IOMode(..), Handle, hSetBinaryMode, hClose)
-
-#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
-import qualified Streamly.Internal.FileSystem.Windows.File as Platform
-#else
-import qualified Streamly.Internal.FileSystem.Posix.File as Platform
-#endif
 
 import qualified Streamly.Internal.FileSystem.Path as Path
 
@@ -27,7 +21,6 @@ import qualified Streamly.Internal.FileSystem.Path as Path
 import GHC.IO.Handle.Internals (addHandleFinalizer)
 #else
 import Control.Concurrent.MVar (MVar, addMVarFinalizer)
-import GHC.IO.Handle.Internals (debugIO)
 import GHC.IO.Handle.Types (Handle__, Handle(..))
 #endif
 
@@ -43,7 +36,6 @@ type HandleFinalizer = FilePath -> MVar Handle__ -> IO ()
 -- 'MVar' of a duplex handle. See Handle Finalizers for details.
 addHandleFinalizer :: Handle -> HandleFinalizer -> IO ()
 addHandleFinalizer handle finalizer = do
-  debugIO $ "Registering finalizer: " ++ show filepath
   addMVarFinalizer mv (finalizer filepath mv)
   where
     !(filepath, !mv) = case handle of
@@ -51,43 +43,55 @@ addHandleFinalizer handle finalizer = do
       DuplexHandle fp _ write_m -> (fp, write_m)
 #endif
 
-addFilePathToIOError :: String -> Path -> IOException -> IOException
-addFilePathToIOError fun fp ioe = unsafePerformIO $ do
-  let fp' = Path.toString fp
-  -- XXX Why is this important?
-  -- deepseq will be introduced dependency because of this
-  -- fp'' <- evaluate $ force fp'
-  pure $ ioe{ ioe_location = fun, ioe_filename = Just fp' }
-
-augmentError :: String -> Path -> IO a -> IO a
-augmentError str osfp = flip catchException (ioError . addFilePathToIOError str osfp)
-
-withOpenFile'
-    :: Path
-    -> IOMode -> Bool -> Bool -> Bool
-    -> (Handle -> IO r) -> Bool -> IO r
-withOpenFile' fp iomode binary existing cloExec action close_finally =
+{-# INLINE withOpenFile #-}
+withOpenFile
+    :: Bool
+    -> Bool
+    -> (Path -> IOMode -> IO Handle)
+    -> Path
+    -> IOMode
+    -> (Handle -> IO r)
+    -> IO r
+withOpenFile binary close_finally f fp iomode action =
     mask $ \restore -> do
-        hndl <- case (existing, cloExec) of
-                  (True, False) -> Platform.openExistingFile fp iomode
-                  (False, False) -> Platform.openFile fp iomode
-                  (True, True) -> Platform.openExistingFileWithCloseOnExec fp iomode
-                  (False, True) -> Platform.openFileWithCloseOnExec fp iomode
-        addHandleFinalizer hndl handleFinalizer
-        when binary $ hSetBinaryMode hndl True
-        r <- restore (action hndl) `onException` hClose hndl
-        when close_finally $ hClose hndl
+        h <- f fp iomode
+        -- XXX In case of withFile it will be closed anyway, so do we even need
+        -- this?
+        addHandleFinalizer h handleFinalizer
+        when binary $ hSetBinaryMode h True
+        r <- restore (action h) `onException` hClose h
+        when close_finally $ hClose h
         pure r
 
--- | Open a file and return the 'Handle'.
-openFile :: Path -> IOMode -> IO Handle
-openFile osfp iomode =
-    augmentError "openFile" osfp $ withOpenFile' osfp iomode False False False pure False
+addFilePathToIOError :: String -> Path -> IOException -> IOException
+addFilePathToIOError fun fp ioe =
+  let !str = Path.toString fp
+   in ioe
+        { ioe_location = fun
+        , ioe_filename = Just str
+        }
 
--- | Run an action on a file.
---
--- The 'Handle' is automatically closed afther the action.
-withFile :: Path -> IOMode -> (Handle -> IO r) -> IO r
-withFile osfp iomode act = (augmentError "withFile" osfp
-    $ withOpenFile' osfp iomode False False False (try . act) True)
-  >>= either ioError pure
+{-# INLINE catchWith #-}
+catchWith :: String -> Path -> IO a -> IO a
+catchWith str path io =
+    catchException io (ioError . addFilePathToIOError str path)
+
+{-# INLINE withFile #-}
+withFile ::
+    Bool
+    -> (Path -> IOMode -> IO Handle)
+    -> Path
+    -> IOMode
+    -> (Handle -> IO r)
+    -> IO r
+withFile binary f path iomode act =
+     catchWith "withFile" path
+        (withOpenFile binary True f path iomode (try . act))
+      >>= either ioError pure
+
+{-# INLINE openFile #-}
+openFile ::
+    Bool -> (Path -> IOMode -> IO Handle) -> Path -> IOMode -> IO Handle
+openFile binary f path iomode =
+    catchWith "openFile" path
+        $ withOpenFile binary False f path iomode pure
