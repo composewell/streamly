43edcfe9793bbd5b5411189a530517261f4b47da
Harendra Kumar <harendra@composewell.com>
2026-06-12 01:01:23 +0530

    Use per benchmark IO action in Data.MutArray benchmarks

--- benchmark/Streamly/Benchmark/Data/MutArray.hs
+++ benchmark/Streamly/Benchmark/Data/MutArray.hs
@@ -21,7 +21,6 @@
 
 import Control.DeepSeq (NFData(..))
 import Control.Monad.IO.Class (MonadIO)
-import Data.Functor ((<&>))
 #if __GLASGOW_HASKELL__ >= 810
 import Data.Kind (Type)
 #endif
@@ -41,11 +40,9 @@ import Prelude
     , (||)
     , (++)
     , concat
-    , const
     , filter
     , fmap
     , fst
-    , id
     , snd
     , undefined
     )
@@ -73,28 +70,21 @@ instance NFData (MutArray a) where
 -- Benchmark helpers
 -------------------------------------------------------------------------------
 
+{-# INLINE withRandomIntIO #-}
+withRandomIntIO :: (Int -> IO b) -> IO b
+withRandomIntIO f = randomRIO (1, 1 :: Int) >>= f
+
 {-# INLINE benchIO #-}
-benchIO :: NFData b => String -> (Int -> IO a) -> (a -> b) -> Benchmark
-benchIO name src f = bench name $ nfIO $
-    (randomRIO (1,1) >>= src) <&> f
+benchIO :: NFData b => String -> IO b -> Benchmark
+benchIO name = bench name . nfIO
 
-{-# INLINE benchPureSink #-}
-benchPureSink :: NFData b => Int -> String -> (Stream Int -> b) -> Benchmark
-benchPureSink value name = benchIO name (sourceIntFromTo value)
+{-# INLINE withArray #-}
+withArray :: Int -> (Stream Int -> IO b) -> IO b
+withArray value f = sourceIntFromTo value >>= f
 
-{-# INLINE benchIO' #-}
-benchIO' :: NFData b => String -> (Int -> IO a) -> (a -> IO b) -> Benchmark
-benchIO' name src f = bench name $ nfIO $
-    randomRIO (1,1) >>= src >>= f
-
-{-# INLINE benchIOSink #-}
-benchIOSink :: NFData b => Int -> String -> (Stream Int -> IO b) -> Benchmark
-benchIOSink value name = benchIO' name (sourceIntFromTo value)
-
--- Drain a source that generates an array in the IO monad
-{-# INLINE benchIOSrc #-}
-benchIOSrc :: String -> (Int -> IO (Stream a)) -> Benchmark
-benchIOSrc name src = benchIO name src id
+{-# INLINE withStream #-}
+withStream :: Int -> (Stream.Stream IO Int -> IO b) -> IO b
+withStream value f = withRandomIntIO $ \n -> f $ sourceUnfoldrM value n
 
 drain :: Monad m => Stream.Stream m a -> m ()
 drain = Stream.fold Fold.drain
@@ -104,34 +94,32 @@ drain = Stream.fold Fold.drain
 -------------------------------------------------------------------------------
 
 {-# INLINE sourceUnfoldr #-}
-sourceUnfoldr :: MonadIO m => Int -> Int -> m (Stream Int)
-sourceUnfoldr value n =
-    Stream.fold (MArray.createOf value) $ Stream.unfoldr step n
-
-    where
-
-    step cnt =
-        if cnt > n + value
-        then Nothing
-        else Just (cnt, cnt + 1)
+sourceUnfoldr :: Int -> IO (Stream Int)
+sourceUnfoldr value = withRandomIntIO $ \n ->
+    let step cnt =
+            if cnt > n + value
+            then Nothing
+            else Just (cnt, cnt + 1)
+    in Stream.fold (MArray.createOf value) $ Stream.unfoldr step n
 
 {-# INLINE sourceIntFromTo #-}
-sourceIntFromTo :: MonadIO m => Int -> Int -> m (Stream Int)
-sourceIntFromTo value n =
+sourceIntFromTo :: Int -> IO (Stream Int)
+sourceIntFromTo value = withRandomIntIO $ \n ->
     Stream.fold (MArray.createOf value) $ Stream.enumerateFromTo n (n + value)
 
 {-# INLINE sourceFromList #-}
-sourceFromList :: MonadIO m => Int -> Int -> m (Stream Int)
-sourceFromList value n =
+sourceFromList :: Int -> IO (Stream Int)
+sourceFromList value = withRandomIntIO $ \n ->
     Stream.fold (MArray.createOf value) $ Stream.fromList [n .. n + value]
 
 {-# INLINE sourceIntFromToFromList #-}
-sourceIntFromToFromList :: MonadIO m => Int -> Int -> m (Stream Int)
-sourceIntFromToFromList value n = MArray.fromListN value [n..n + value]
+sourceIntFromToFromList :: Int -> IO (Stream Int)
+sourceIntFromToFromList value = withRandomIntIO $ \n ->
+    MArray.fromListN value [n..n + value]
 
 {-# INLINE sourceIntFromToFromStream #-}
-sourceIntFromToFromStream :: MonadIO m => Int -> Int -> m (Stream Int)
-sourceIntFromToFromStream value n =
+sourceIntFromToFromStream :: Int -> IO (Stream Int)
+sourceIntFromToFromStream value = withRandomIntIO $ \n ->
     Stream.fold MArray.create $ Stream.enumerateFromTo n (n + value)
 
 {-# INLINE sourceUnfoldrM #-}
@@ -158,18 +146,6 @@ composeN n f x =
         4 -> f x >>= f >>= f >>= f
         _ -> undefined
 
-{-# INLINE scanl' #-}
-scanl' :: MonadIO m => Int -> Int -> Stream Int -> m (Stream Int)
-scanl' value n = composeN n $ onArray value $ Stream.scanl (Scanl.scanl' (+) 0)
-
-{-# INLINE scanl1' #-}
-scanl1' :: MonadIO m => Int -> Int -> Stream Int -> m (Stream Int)
-scanl1' value n = composeN n $ onArray value $ Stream.scanl1' (+)
-
-{-# INLINE map #-}
-map :: MonadIO m => Int -> Int -> Stream Int -> m (Stream Int)
-map value n = composeN n $ onArray value $ fmap (+ 1)
-
 {-# INLINE onArray #-}
 onArray
     :: MonadIO m => Int -> (Stream.Stream m Int -> Stream.Stream m Int)
@@ -178,29 +154,61 @@ onArray
 onArray value f arr =
     Stream.fold (MArray.createOf value) $ f $ Stream.unfold MArray.reader arr
 
+{-# INLINE scanl' #-}
+scanl' :: Int -> IO (Stream Int)
+scanl' value = withArray value $ composeN 1 $ onArray value $ Stream.scanl (Scanl.scanl' (+) 0)
+
+{-# INLINE scanl'X4 #-}
+scanl'X4 :: Int -> IO (Stream Int)
+scanl'X4 value = withArray value $ composeN 4 $ onArray value $ Stream.scanl (Scanl.scanl' (+) 0)
+
+{-# INLINE scanl1' #-}
+scanl1' :: Int -> IO (Stream Int)
+scanl1' value = withArray value $ composeN 1 $ onArray value $ Stream.scanl1' (+)
+
+{-# INLINE scanl1'X4 #-}
+scanl1'X4 :: Int -> IO (Stream Int)
+scanl1'X4 value = withArray value $ composeN 4 $ onArray value $ Stream.scanl1' (+)
+
+{-# INLINE map #-}
+map :: Int -> IO (Stream Int)
+map value = withArray value $ composeN 1 $ onArray value $ fmap (+ 1)
+
+{-# INLINE mapX4 #-}
+mapX4 :: Int -> IO (Stream Int)
+mapX4 value = withArray value $ composeN 4 $ onArray value $ fmap (+ 1)
+
+{-# INLINE idArr #-}
+idArr :: Int -> IO (Stream Int)
+idArr value = withArray value return
+
 -------------------------------------------------------------------------------
 -- Elimination
 -------------------------------------------------------------------------------
 
 {-# INLINE unfoldReadDrain #-}
-unfoldReadDrain :: MonadIO m => Stream Int -> m ()
-unfoldReadDrain = drain . Stream.unfold MArray.reader
+unfoldReadDrain :: Int -> IO ()
+unfoldReadDrain value = withArray value $ drain . Stream.unfold MArray.reader
 
 {-# INLINE unfoldReadRevDrain #-}
-unfoldReadRevDrain :: MonadIO m => Stream Int -> m ()
-unfoldReadRevDrain = drain . Stream.unfold MArray.readerRev
+unfoldReadRevDrain :: Int -> IO ()
+unfoldReadRevDrain value = withArray value $ drain . Stream.unfold MArray.readerRev
 
 {-# INLINE toStreamDRevDrain #-}
-toStreamDRevDrain :: MonadIO m => Stream Int -> m ()
-toStreamDRevDrain = drain . MArray.readRev
+toStreamDRevDrain :: Int -> IO ()
+toStreamDRevDrain value = withArray value $ drain . MArray.readRev
 
 {-# INLINE toStreamDDrain #-}
-toStreamDDrain :: MonadIO m => Stream Int -> m ()
-toStreamDDrain = drain . MArray.read
+toStreamDDrain :: Int -> IO ()
+toStreamDDrain value = withArray value $ drain . MArray.read
 
 {-# INLINE unfoldFold #-}
-unfoldFold :: MonadIO m => Stream Int -> m Int
-unfoldFold = Stream.fold (Fold.foldl' (+) 0) . Stream.unfold MArray.reader
+unfoldFold :: Int -> IO Int
+unfoldFold value = withArray value $ Stream.fold (Fold.foldl' (+) 0) . Stream.unfold MArray.reader
+
+{-# INLINE writeN #-}
+writeN :: Int -> IO (Stream Int)
+writeN value = withStream value (Stream.fold (MArray.createOf value))
 
 -------------------------------------------------------------------------------
 -- Bench groups
@@ -210,74 +218,66 @@ o_1_space_generation :: Int -> [Benchmark]
 o_1_space_generation value =
     [ bgroup
         "generation"
-        [ benchIOSrc "createOf . intFromTo" (sourceIntFromTo value)
-        , benchIOSrc
-              "fromList . intFromTo"
-              (sourceIntFromToFromList value)
-        , benchIOSrc "createOf . unfoldr" (sourceUnfoldr value)
-        , benchIOSrc "createOf . fromList" (sourceFromList value)
-        , benchIOSrc "write . intFromTo" (sourceIntFromToFromStream value)
+        [ benchIO "createOf . intFromTo" $ sourceIntFromTo value
+        , benchIO "fromList . intFromTo" $ sourceIntFromToFromList value
+        , benchIO "createOf . unfoldr" $ sourceUnfoldr value
+        , benchIO "createOf . fromList" $ sourceFromList value
+        , benchIO "write . intFromTo" $ sourceIntFromToFromStream value
         ]
     ]
 
 o_1_space_elimination :: Int -> [Benchmark]
 o_1_space_elimination value =
     [ bgroup "elimination"
-        [ benchPureSink value "id" id
-        , benchIOSink value "foldl'" unfoldFold
-        , benchIOSink value "read" unfoldReadDrain
-        , benchIOSink value "readRev" unfoldReadRevDrain
-        , benchIOSink value "toStream" toStreamDDrain
-        , benchIOSink value "toStreamRev" toStreamDRevDrain
+        [ benchIO "id" $ idArr value
+        , benchIO "foldl'" $ unfoldFold value
+        , benchIO "read" $ unfoldReadDrain value
+        , benchIO "readRev" $ unfoldReadRevDrain value
+        , benchIO "toStream" $ toStreamDDrain value
+        , benchIO "toStreamRev" $ toStreamDRevDrain value
         ]
       ]
 
 o_n_heap_serial :: Int -> [Benchmark]
 o_n_heap_serial value =
     [ bgroup "elimination"
-        [
-        -- Converting the stream to an array
-            benchFold "createOf" (Stream.fold (MArray.createOf value))
-                (sourceUnfoldrM value)
-         ]
+        [ benchIO "createOf" $ writeN value
+        ]
     ]
 
 o_1_space_transformation :: Int -> [Benchmark]
 o_1_space_transformation value =
    [ bgroup "transformation"
-        [ benchIOSink value "scanl'" (scanl' value 1)
-        , benchIOSink value "scanl1'" (scanl1' value 1)
-        , benchIOSink value "map" (map value 1)
+        [ benchIO "scanl'" $ scanl' value
+        , benchIO "scanl1'" $ scanl1' value
+        , benchIO "map" $ map value
         ]
    ]
 
 o_1_space_transformationX4 :: Int -> [Benchmark]
 o_1_space_transformationX4 value =
     [ bgroup "transformationX4"
-        [ benchIOSink value "scanl'" (scanl' value 4)
-        , benchIOSink value "scanl1'" (scanl1' value 4)
-        , benchIOSink value "map" (map value 4)
+        [ benchIO "scanl'" $ scanl'X4 value
+        , benchIO "scanl1'" $ scanl1'X4 value
+        , benchIO "map" $ mapX4 value
         ]
       ]
 
 o_1_space_serial_marray ::
     Int -> (MutArray Int, Array.Array Int) -> [Benchmark]
 o_1_space_serial_marray value ~(array, indices) =
-    [ benchIO' "partitionBy (< 0)" (const (return array))
-        $ MArray.partitionBy (< 0)
-    , benchIO' "partitionBy (> 0)" (const (return array))
-        $ MArray.partitionBy (> 0)
-    , benchIO' "partitionBy (< value/2)" (const (return array))
-        $ MArray.partitionBy (< (value `div` 2))
-    , benchIO' "partitionBy (> value/2)" (const (return array))
-        $ MArray.partitionBy (> (value `div` 2))
-    , benchIO' "strip (< value/2 || > value/2)" (const (return array))
-        $ MArray.dropAround (\x -> x < value `div` 2 || x > value `div` 2)
-    , benchIO' "strip (> 0)" (const (return array))
-        $ MArray.dropAround (> 0)
-    , benchIO' "modifyIndices (+ 1)" (const (return indices))
-        $ Stream.fold (MArray.modifyIndices array (\_idx val -> val + 1))
-        . Stream.unfold Array.reader
+    [ benchIO "partitionBy (< 0)" $ MArray.partitionBy (< 0) array
+    , benchIO "partitionBy (> 0)" $ MArray.partitionBy (> 0) array
+    , benchIO "partitionBy (< value/2)" $
+        MArray.partitionBy (< (value `div` 2)) array
+    , benchIO "partitionBy (> value/2)" $
+        MArray.partitionBy (> (value `div` 2)) array
+    , benchIO "strip (< value/2 || > value/2)" $
+        MArray.dropAround (\x -> x < value `div` 2 || x > value `div` 2) array
+    , benchIO "strip (> 0)" $ MArray.dropAround (> 0) array
+    , benchIO "modifyIndices (+ 1)" $
+        Stream.fold (MArray.modifyIndices array (\_idx val -> val + 1))
+        $ Stream.unfold Array.reader indices
     ]
 
 -------------------------------------------------------------------------------
