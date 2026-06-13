243a104c8ff139bb8deae396e3bd17f5a5312797
Harendra Kumar <harendra@composewell.com>
2026-06-14 03:31:22 +0530

    Push common array tests in the common module

--- test/Streamly/Test/Data/Array/Generic.hs
+++ test/Streamly/Test/Data/Array/Generic.hs
@@ -30,26 +30,6 @@ moduleName = "Data.Array"
 
 #include "Streamly/Test/Data/Array/Common.hs"
 
-testFromStreamToStream :: Property
-testFromStreamToStream =
-    genericTestFromTo (const A.fromStream) A.read (==)
-
-testFoldUnfold :: Property
-testFoldUnfold =
-    genericTestFromTo (const (S.fold A.create)) (S.unfold A.reader) (==)
-
-testFromList :: Property
-testFromList =
-    forAll (choose (0, maxArrLen)) $ \len ->
-            forAll (vectorOf len (arbitrary :: Gen Int)) $ \list ->
-                monadicIO $ do
-                    let arr = A.fromList list
-                    xs <- run $ S.fold Fold.toList $ S.unfold A.reader arr
-                    assert (xs == list)
-
-testLengthFromStream :: Property
-testLengthFromStream = genericTestFrom (const A.fromStream)
-
 testReadShowInstance :: Property
 testReadShowInstance =
     forAll (choose (0, maxArrLen)) $ \len ->
@@ -65,22 +45,6 @@ main =
     H.parallel $
     modifyMaxSuccess (const maxTestCount) $ do
       describe moduleName $ do
-        -- Construction
-        describe "createOf" $ do
-            prop "length . createOf n === n" testLength
-            prop "reader . createOf === id" testFoldNUnfold
-            prop "read . createOf === id" testFoldNToStream
-            prop "readRev . createOf === reverse" testFoldNToStreamRev
-            prop "foldMany concats to original" (foldManyWith A.createOf)
-        prop "reader . create === id" testFoldUnfold
-        describe "fromStreamN" $ do
-            prop "length . fromStreamN n === n" testLengthFromStreamN
-            prop "reader . fromStreamN === id" testFromStreamNUnfold
-            prop "read . fromStreamN === id" testFromStreamNToStream
-        describe "fromStream" $ do
-            prop "length . fromStream === n" testLengthFromStream
-            prop "read . fromStream === id" testFromStreamToStream
-        prop "fromListN" testFromListN
-        prop "fromList" testFromList
+        commonMain
         -- Show/Read instances
         prop "show/read roundtrip" testReadShowInstance
