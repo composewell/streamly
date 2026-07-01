de5ed84846dfc0c0cabfe2b008f5f3028f3d873f
Harendra Kumar <harendra@composewell.com>
2026-07-01 07:37:03 +0530

    Change Scanl.Window benchmarks to use isolated IO actions

--- benchmark/Streamly/Benchmark/Data/Scanl/Window.hs
+++ benchmark/Streamly/Benchmark/Data/Scanl/Window.hs
@@ -5,12 +5,6 @@
 -- License     : MIT
 -- Maintainer  : streamly@composewell.com
 
-#undef FUSION_CHECK
-#ifdef FUSION_CHECK
-{-# OPTIONS_GHC -fplugin-opt=Fusion.Plugin:verbose=2 #-}
-{-# OPTIONS_GHC -ddump-simpl -ddump-to-file -dsuppress-all #-}
-#endif
-
 module Scanl.Window (benchmarks) where
 
 import Streamly.Internal.Data.Scanl (Scanl)
@@ -39,87 +33,128 @@ sourceDescending len from =
         (from + fromIntegral (len - 1))
         from
 
-{-# INLINE sourceDescendingInt #-}
-sourceDescendingInt :: Monad m => Int -> Int -> Stream m Int
-sourceDescendingInt = sourceDescending
+{-# INLINE benchIO #-}
+benchIO :: String -> IO () -> Benchmark
+benchIO name f = bench name $ nfIO f
 
-{-# INLINE benchScanWith #-}
-benchScanWith :: Num a =>
-    (Int -> a -> Stream IO a) -> Int -> String -> Scanl IO a b -> Benchmark
-benchScanWith src len name f =
-    bench name
-        $ nfIO
-        $ randomRIO (1, 1 :: Int)
-            >>= Stream.fold Fold.drain
-                . Stream.postscanl f
-                . src len
-                . fromIntegral
+{-# INLINE withStream #-}
+withStream :: (Num a, Stream.Enumerable a) => Int -> (Stream IO a -> IO b) -> IO b
+withStream n f = randomRIO (1, 1 :: Int) >>= f . source n . fromIntegral
 
-{-# INLINE benchWithPostscan #-}
-benchWithPostscan :: Int -> String -> Scanl IO Double a -> Benchmark
-benchWithPostscan = benchScanWith source
+{-# INLINE withDescStream #-}
+withDescStream :: (Num a, Stream.Enumerable a) => Int -> (Stream IO a -> IO b) -> IO b
+withDescStream n f = randomRIO (1, 1 :: Int) >>= f . sourceDescending n . fromIntegral
 
-{-# INLINE benchWithPostscanInt #-}
-benchWithPostscanInt :: Int -> String -> Scanl IO Int a -> Benchmark
-benchWithPostscanInt = benchScanWith source
+-- | Run a scan over the stream as a postscan and drain the result.
+{-# INLINE withPostscanl #-}
+withPostscanl :: Int -> Scanl IO Double b -> IO ()
+withPostscanl n s = withStream n $ Stream.fold Fold.drain . Stream.postscanl s
+
+{-# INLINE withPostscanlInt #-}
+withPostscanlInt :: Int -> Scanl IO Int b -> IO ()
+withPostscanlInt n s = withStream n $ Stream.fold Fold.drain . Stream.postscanl s
+
+{-# INLINE withPostscanlDescInt #-}
+withPostscanlDescInt :: Int -> Scanl IO Int b -> IO ()
+withPostscanlDescInt n s = withDescStream n $ Stream.fold Fold.drain . Stream.postscanl s
+
+-------------------------------------------------------------------------------
+-- benchmarks
+-------------------------------------------------------------------------------
+
+{-# NOINLINE windowMinimum #-}
+windowMinimum :: Int -> Int -> IO ()
+windowMinimum win n = withPostscanl n (Scanl.windowMinimum win)
+
+{-# NOINLINE windowMinimumInt #-}
+windowMinimumInt :: Int -> Int -> IO ()
+windowMinimumInt win n = withPostscanlInt n (Scanl.windowMinimum win)
+
+{-# NOINLINE windowMinimumDesc #-}
+windowMinimumDesc :: Int -> Int -> IO ()
+windowMinimumDesc win n = withPostscanlDescInt n (Scanl.windowMinimum win)
+
+{-# NOINLINE windowMaximum #-}
+windowMaximum :: Int -> Int -> IO ()
+windowMaximum win n = withPostscanl n (Scanl.windowMaximum win)
+
+{-# NOINLINE windowMaximumDesc #-}
+windowMaximumDesc :: Int -> Int -> IO ()
+windowMaximumDesc win n = withPostscanlDescInt n (Scanl.windowMaximum win)
+
+{-# NOINLINE windowRange #-}
+windowRange :: Int -> Int -> IO ()
+windowRange win n = withPostscanl n (Scanl.windowRange win)
+
+{-# NOINLINE windowRangeDesc #-}
+windowRangeDesc :: Int -> Int -> IO ()
+windowRangeDesc win n = withPostscanlDescInt n (Scanl.windowRange win)
+
+{-# NOINLINE incrSum #-}
+incrSum :: Int -> Int -> IO ()
+incrSum win n = withPostscanl n (Scanl.incrScan win Scanl.incrSum)
+
+{-# NOINLINE incrSumCumulative #-}
+incrSumCumulative :: Int -> IO ()
+incrSumCumulative n = withPostscanl n (Scanl.cumulativeScan Scanl.incrSum)
+
+{-# NOINLINE incrSumInt #-}
+incrSumInt :: Int -> Int -> IO ()
+incrSumInt win n = withPostscanlInt n (Scanl.incrScan win Scanl.incrSumInt)
+
+{-# NOINLINE incrMean #-}
+incrMean :: Int -> Int -> IO ()
+incrMean win n = withPostscanl n (Scanl.incrScan win Scanl.incrMean)
+
+{-# NOINLINE incrMeanCumulative #-}
+incrMeanCumulative :: Int -> IO ()
+incrMeanCumulative n = withPostscanl n (Scanl.cumulativeScan Scanl.incrMean)
+
+{-# NOINLINE incrPowerSum #-}
+incrPowerSum :: Int -> Int -> IO ()
+incrPowerSum win n = withPostscanl n (Scanl.incrScan win (Scanl.incrPowerSum 2))
+
+{-# NOINLINE incrPowerSumCumulative #-}
+incrPowerSumCumulative :: Int -> IO ()
+incrPowerSumCumulative n =
+    withPostscanl n (Scanl.cumulativeScan (Scanl.incrPowerSum 2))
 
 benchmarks :: Int -> [(SpaceComplexity, Benchmark)]
 benchmarks numElements =
-    [ (SpaceO_1, benchWithPostscan numElements "windowMinimum 10"
-        (Scanl.windowMinimum 10))
+    map (SpaceO_1,)
+    [ benchIO "windowMinimum 10" (windowMinimum 10 numElements)
+    , benchIO "windowMinimumInt 10" (windowMinimumInt 10 numElements)
     -- Below window size 30 the linear search based impl performs better
     -- than the dequeue based implementation.
-    , (SpaceO_1, benchWithPostscan numElements "windowMinimum 30"
-        (Scanl.windowMinimum 30))
-    , (SpaceO_1, benchWithPostscan numElements "windowMinimum 1000"
-        (Scanl.windowMinimum 1000))
-    , (SpaceO_1, benchScanWith sourceDescendingInt numElements
-        "windowMinimum 1000 descending"
-        (Scanl.windowMinimum 1000))
+    , benchIO "windowMinimum 30" (windowMinimum 30 numElements)
+    , benchIO "windowMinimum 1000" (windowMinimum 1000 numElements)
+    , benchIO "windowMinimum 1000 descending"
+        (windowMinimumDesc 1000 numElements)
 
-    , (SpaceO_1, benchWithPostscan numElements "windowMaximum 10"
-        (Scanl.windowMaximum 10))
-    , (SpaceO_1, benchWithPostscan numElements "windowMaximum 30"
-        (Scanl.windowMaximum 30))
-    , (SpaceO_1, benchWithPostscan numElements "windowMaximum 1000"
-        (Scanl.windowMaximum 1000))
-    , (SpaceO_1, benchScanWith sourceDescendingInt numElements
-        "windowMaximum 1000 descending"
-        (Scanl.windowMaximum 1000))
+    , benchIO "windowMaximum 10" (windowMaximum 10 numElements)
+    , benchIO "windowMaximum 30" (windowMaximum 30 numElements)
+    , benchIO "windowMaximum 1000" (windowMaximum 1000 numElements)
+    , benchIO "windowMaximum 1000 descending"
+        (windowMaximumDesc 1000 numElements)
 
-    , (SpaceO_1, benchWithPostscan numElements "windowRange 10"
-        (Scanl.windowRange 10))
-    , (SpaceO_1, benchWithPostscan numElements "windowRange 30"
-        (Scanl.windowRange 30))
-    , (SpaceO_1, benchWithPostscan numElements "windowRange 1000"
-        (Scanl.windowRange 1000))
-    , (SpaceO_1, benchScanWith sourceDescendingInt numElements
-        "windowRange 1000 descending"
-        (Scanl.windowRange 1000))
+    , benchIO "windowRange 10" (windowRange 10 numElements)
+    , benchIO "windowRange 30" (windowRange 30 numElements)
+    , benchIO "windowRange 1000" (windowRange 1000 numElements)
+    , benchIO "windowRange 1000 descending"
+        (windowRangeDesc 1000 numElements)
 
-    , (SpaceO_1, benchWithPostscan numElements "incrSum 100"
-        (Scanl.incrScan 100 Scanl.incrSum))
-    , (SpaceO_1, benchWithPostscan numElements "incrSum 1000"
-        (Scanl.incrScan 1000 Scanl.incrSum))
-    , (SpaceO_1, benchWithPostscan numElements "incrSum cumulative"
-        (Scanl.cumulativeScan Scanl.incrSum))
+    , benchIO "incrSum 100" (incrSum 100 numElements)
+    , benchIO "incrSum 1000" (incrSum 1000 numElements)
+    , benchIO "incrSum cumulative" (incrSumCumulative numElements)
 
-    , (SpaceO_1, benchWithPostscanInt numElements "incrSumInt 100"
-        (Scanl.incrScan 100 Scanl.incrSumInt))
-    , (SpaceO_1, benchWithPostscanInt numElements "incrSumInt 1000"
-        (Scanl.incrScan 1000 Scanl.incrSumInt))
+    , benchIO "incrSumInt 100" (incrSumInt 100 numElements)
+    , benchIO "incrSumInt 1000" (incrSumInt 1000 numElements)
 
-    , (SpaceO_1, benchWithPostscan numElements "incrMean 100"
-        (Scanl.incrScan 100 Scanl.incrMean))
-    , (SpaceO_1, benchWithPostscan numElements "incrMean 1000"
-        (Scanl.incrScan 1000 Scanl.incrMean))
-    , (SpaceO_1, benchWithPostscan numElements "incrMean cumulative"
-        (Scanl.cumulativeScan Scanl.incrMean))
+    , benchIO "incrMean 100" (incrMean 100 numElements)
+    , benchIO "incrMean 1000" (incrMean 1000 numElements)
+    , benchIO "incrMean cumulative" (incrMeanCumulative numElements)
 
-    , (SpaceO_1, benchWithPostscan numElements "incrPowerSum 2 100"
-        (Scanl.incrScan 100 (Scanl.incrPowerSum 2)))
-    , (SpaceO_1, benchWithPostscan numElements "incrPowerSum 2 1000"
-        (Scanl.incrScan 1000 (Scanl.incrPowerSum 2)))
-    , (SpaceO_1, benchWithPostscan numElements "incrPowerSum 2"
-        (Scanl.cumulativeScan (Scanl.incrPowerSum 2)))
+    , benchIO "incrPowerSum 2 100" (incrPowerSum 100 numElements)
+    , benchIO "incrPowerSum 2 1000" (incrPowerSum 1000 numElements)
+    , benchIO "incrPowerSum 2" (incrPowerSumCumulative numElements)
     ]
